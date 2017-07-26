7aaee62af21041ddcb7eba2c5a079cd93b2e4d1e
Rikki Gibson <rigibson@microsoft.com>
2017-07-26 16:51:34 -0700

    Add csproj template. Temporarily ignore RegenerateManager flag.

--- src/azurefluent/CodeGeneratorCsaf.cs
+++ src/azurefluent/CodeGeneratorCsaf.cs
@@ -117,12 +117,15 @@ namespace AutoRest.CSharp.Azure.Fluent
                 await Write(xmlSerializationTemplate, Path.Combine(Settings.Instance.ModelsName, $"{XmlSerialization.XmlDeserializationClass}{ImplementationFileExtension}"));
             }
 
-            if (true == Settings.Instance.Host?.GetValue<bool?>("regenerate-manager").Result)
+            //if (Settings.Instance.RegenerateManager)
             {
                 await Write(
                     new AzureServiceManagerTemplate { Model = codeModel },
                     codeModel.ServiceName + "Manager" + ImplementationFileExtension);
                 
+                await Write(
+                    new AzureCsprojTemplate { Model = codeModel },
+                    $"Microsoft.Azure.Management.{codeModel.ServiceName}.Fluent.csproj");
             }
         }
     }
