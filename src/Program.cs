827e99fd0d684a9a2f4158be781ee06f05710330
Johannes Bader <jobader@microsoft.com>
2017-08-23 14:10:45 -0700

    wip

--- src/Program.cs
+++ src/Program.cs
@@ -34,8 +34,8 @@ namespace AutoRest.CSharp
         {
             if(args != null && args.Length > 0 && args[0] == "--server") {
                 var connection = new Connection(Console.Out, Console.OpenStandardInput());
-                connection.Dispatch<IEnumerable<string>>("GetPluginNames", async () => new []{ "csharp" });
-                connection.Dispatch<string, string, bool>("Process", (plugin, sessionId) => new Program(connection, sessionId).Process());
+                connection.Dispatch<IEnumerable<string>>("GetPluginNames", async () => new []{ "csharp", "csharp-simplifier" });
+                connection.Dispatch<string, string, bool>("Process", (plugin, sessionId) => new Program(connection, plugin, sessionId).Process());
                 connection.DispatchNotification("Shutdown", connection.Stop);
 
                 // wait for something to do.
@@ -49,8 +49,11 @@ namespace AutoRest.CSharp
             return 1;
         }
 
-        public Program(Connection connection, string sessionId) : base(connection, sessionId)
+        private string plugin;
+
+        public Program(Connection connection, string plugin, string sessionId) : base(connection, sessionId)
         {
+            this.plugin = plugin;
         }
 
         private T GetXmsCodeGenSetting<T>(CodeModel codeModel, string name)
@@ -70,110 +73,135 @@ namespace AutoRest.CSharp
 
         protected override async Task<bool> ProcessInternal()
         {
-            var codeGenerator = "csharp";
-
-            var files = await ListInputs();
-            if (files.Length != 1)
+            if (this.plugin == "csharp-simplifier")
             {
-                throw new Exception($"Generator received incorrect number of inputs: {files.Length} : {string.Join(",", files)}");
-            }
+                var fs = new MemoryFileSystem();
 
-            // get internal name
-            var language = new[] {
-                "CSharp",
-                "Ruby",
-                "NodeJS",
-                "Python",
-                "Go",
-                "Php",
-                "Java",
-                "AzureResourceSchema",
-                "JsonRpcClient" }
-                .Where(x => x.ToLowerInvariant() == codeGenerator)
-                .FirstOrDefault();
-
-            if (language == null)
-            {
-                throw new Exception($"Language '{codeGenerator}' unknown.");
-            }
-
-            // process
-            var plugin = ExtensionsLoader.GetPlugin(
-                (await GetValue<bool?>("azure-arm") ?? false ? "Azure." : "") +
-                language +
-                (await GetValue<bool?>("fluent") ?? false ? ".Fluent" : "") +
-                (await GetValue<bool?>("testgen") ?? false ? ".TestGen" : ""));
-            var modelAsJson = (await ReadFile(files[0])).EnsureYamlIsJson();
-            
-            // settings
-            var codeModelT = new ModelSerializer<CodeModel>().Load(modelAsJson);
-            new Settings
-            {
-                Namespace = await GetValue("namespace"),
-                ClientName = GetXmsCodeGenSetting<string>(codeModelT, "name") ?? await GetValue("override-client-name"),
-                PayloadFlatteningThreshold = GetXmsCodeGenSetting<int?>(codeModelT, "ft") ?? await GetValue<int?>("payload-flattening-threshold") ?? 0,
-                AddCredentials = await GetValue<bool?>("add-credentials") ?? false,
-                Host = this
-            };
-            // build settings
-            var altNamespace = (await GetValue<string[]>("input-file") ?? new[] { "" }).FirstOrDefault()?.Split('/').Last().Split('\\').Last().Split('.').First();
-
-            // TODO: might have to come after Settings were created. In that case, load model without plugin first to get settings...
-            Settings.Instance.Namespace = Settings.Instance.Namespace ?? CodeNamer.Instance.GetNamespaceName(altNamespace);
-
-            var header = await GetValue("license-header");
-            if (header != null)
-            {
-                Settings.Instance.Header = header;
-            }
-            Settings.Instance.CustomSettings.Add("InternalConstructors", GetXmsCodeGenSetting<bool?>(codeModelT, "internalConstructors") ?? await GetValue<bool?>("use-internal-constructors") ?? false);
-            Settings.Instance.CustomSettings.Add("SyncMethods", GetXmsCodeGenSetting<string>(codeModelT, "syncMethods") ?? await GetValue("sync-methods") ?? "essential");
-            Settings.Instance.CustomSettings.Add("UseDateTimeOffset", GetXmsCodeGenSetting<bool?>(codeModelT, "useDateTimeOffset") ?? await GetValue<bool?>("use-datetimeoffset") ?? false);
-            Settings.Instance.CustomSettings["ClientSideValidation"] = await GetValue<bool?>("client-side-validation") ?? false;
-            int defaultMaximumCommentColumns = codeGenerator == "go" ? 120 : Settings.DefaultMaximumCommentColumns;
-            Settings.Instance.MaximumCommentColumns = await GetValue<int?>("max-comment-columns") ?? defaultMaximumCommentColumns;
-            Settings.Instance.OutputFileName = await GetValue<string>("output-file");
-            if (codeGenerator == "csharp")
-            {
-                Settings.Instance.Header = $"<auto-generated>\n{Settings.Instance.Header}\n</auto-generated>";
-            }
-            if (codeGenerator == "ruby" || codeGenerator == "python")
-            {
-                // TODO: sort out matters here entirely instead of relying on Input being read somewhere...
-                var inputFile = await GetValue<string[]>("input-file");
-                Settings.Instance.Input = inputFile.FirstOrDefault();
-                Settings.Instance.PackageName = await GetValue("package-name");
-                Settings.Instance.PackageVersion = await GetValue("package-version");
-            }
-            if (codeGenerator == "go")
-            {
-                Settings.Instance.PackageVersion = await GetValue("package-version");
-            }
-
-            using (plugin.Activate())
-            {
-                var codeModel = plugin.Serializer.Load(modelAsJson);
-
-                codeModel = plugin.Transformer.TransformCodeModel(codeModel);
-                if (await GetValue<bool?>("sample-generation") ?? false)
+                // setup filesystem
+                var files = await ListInputs();
+                foreach (var file in files)
                 {
-                    plugin.CodeGenerator.GenerateSamples(codeModel).GetAwaiter().GetResult();
+                    fs.WriteAllText(file, await ReadFile(file));
                 }
-                else
+
+                // simplify
+                new AutoRest.Simplify.CSharpSimplifier().Run(fs).ConfigureAwait(false).GetAwaiter().GetResult();
+
+                // write out files
+                var outFiles = fs.GetFiles("", "*", System.IO.SearchOption.AllDirectories);
+                foreach (var outFile in outFiles)
                 {
-                    plugin.CodeGenerator.Generate(codeModel).GetAwaiter().GetResult();
+                    WriteFile(outFile, fs.ReadAllText(outFile), null);
                 }
-            }
 
-            // write out files
-            var outFS = Settings.Instance.FileSystemOutput;
-            var outFiles = outFS.GetFiles("", "*", System.IO.SearchOption.AllDirectories);
-            foreach (var outFile in outFiles)
+                return true;
+            }
+            else
             {
-                WriteFile(outFile, outFS.ReadAllText(outFile), null);
-            }
+                var codeGenerator = this.plugin;
 
-            return true;
+                var files = await ListInputs();
+                if (files.Length != 1)
+                {
+                    throw new Exception($"Generator received incorrect number of inputs: {files.Length} : {string.Join(",", files)}");
+                }
+                var modelAsJson = (await ReadFile(files[0])).EnsureYamlIsJson();
+                var codeModelT = new ModelSerializer<CodeModel>().Load(modelAsJson);
+
+                // get internal name
+                var language = new[] {
+                    "CSharp",
+                    "Ruby",
+                    "NodeJS",
+                    "Python",
+                    "Go",
+                    "Php",
+                    "Java",
+                    "AzureResourceSchema",
+                    "JsonRpcClient" }
+                    .Where(x => x.ToLowerInvariant() == codeGenerator)
+                    .First();
+
+                // build settings
+                var altNamespace = (await GetValue<string[]>("input-file") ?? new[] { "" }).FirstOrDefault()?.Split('/').Last().Split('\\').Last().Split('.').First();
+                
+                new Settings
+                {
+                    Namespace = await GetValue("namespace"),
+                    ClientName = GetXmsCodeGenSetting<string>(codeModelT, "name") ?? await GetValue("override-client-name"),
+                    PayloadFlatteningThreshold = GetXmsCodeGenSetting<int?>(codeModelT, "ft") ?? await GetValue<int?>("payload-flattening-threshold") ?? 0,
+                    AddCredentials = await GetValue<bool?>("add-credentials") ?? false,
+                    Host = this
+                };
+                var header = await GetValue("license-header");
+                if (header != null)
+                {
+                    Settings.Instance.Header = header;
+                }
+                Settings.Instance.CustomSettings.Add("InternalConstructors", GetXmsCodeGenSetting<bool?>(codeModelT, "internalConstructors") ?? await GetValue<bool?>("use-internal-constructors") ?? false);
+                Settings.Instance.CustomSettings.Add("SyncMethods", GetXmsCodeGenSetting<string>(codeModelT, "syncMethods") ?? await GetValue("sync-methods") ?? "essential");
+                Settings.Instance.CustomSettings.Add("UseDateTimeOffset", GetXmsCodeGenSetting<bool?>(codeModelT, "useDateTimeOffset") ?? await GetValue<bool?>("use-datetimeoffset") ?? false);
+                Settings.Instance.CustomSettings["ClientSideValidation"] = await GetValue<bool?>("client-side-validation") ?? false;
+                int defaultMaximumCommentColumns = codeGenerator == "go" ? 120 : Settings.DefaultMaximumCommentColumns;
+                Settings.Instance.MaximumCommentColumns = await GetValue<int?>("max-comment-columns") ?? defaultMaximumCommentColumns;
+                Settings.Instance.OutputFileName = await GetValue<string>("output-file");
+                if (codeGenerator == "csharp")
+                {
+                    Settings.Instance.Header = $"<auto-generated>\n{Settings.Instance.Header}\n</auto-generated>";
+                }
+                if (codeGenerator == "ruby" || codeGenerator == "python")
+                {
+                    // TODO: sort out matters here entirely instead of relying on Input being read somewhere...
+                    var inputFile = await GetValue<string[]>("input-file");
+                    Settings.Instance.Input = inputFile.FirstOrDefault();
+                    Settings.Instance.PackageName = await GetValue("package-name");
+                    Settings.Instance.PackageVersion = await GetValue("package-version");
+                }
+                if (codeGenerator == "go")
+                {
+                    Settings.Instance.PackageVersion = await GetValue("package-version");
+                }
+
+                // process
+                var plugin = ExtensionsLoader.GetPlugin(
+                    (await GetValue<bool?>("azure-arm") ?? false ? "Azure." : "") +
+                    language +
+                    (await GetValue<bool?>("fluent") ?? false ? ".Fluent" : "") +
+                    (await GetValue<bool?>("testgen") ?? false ? ".TestGen" : ""));
+                Settings.PopulateSettings(plugin.Settings, Settings.Instance.CustomSettings);
+                
+                using (plugin.Activate())
+                {
+                    System.Console.Error.WriteLine(DependencyInjection.Singleton<GeneratorSettingsCs>.Instance.ClientSideValidation);
+                    Settings.Instance.Namespace = Settings.Instance.Namespace ?? CodeNamer.Instance.GetNamespaceName(altNamespace);
+                    var codeModel = plugin.Serializer.Load(modelAsJson);
+                    codeModel = plugin.Transformer.TransformCodeModel(codeModel);
+                    if (await GetValue<bool?>("sample-generation") ?? false)
+                    {
+                        plugin.CodeGenerator.GenerateSamples(codeModel).GetAwaiter().GetResult();
+                    }
+                    else
+                    {
+                        plugin.CodeGenerator.Generate(codeModel).GetAwaiter().GetResult();
+                    }
+                }
+
+                // write out files
+                var outFS = Settings.Instance.FileSystemOutput;
+                var outFiles = outFS.GetFiles("", "*", System.IO.SearchOption.AllDirectories);
+                foreach (var outFile in outFiles)
+                {
+                    if (outFile == "SwaggerPetstoreExtensions.cs")
+                        continue;
+                    if (outFile == "Models/Order.cs")
+                        continue;
+                    if (outFile == "Models/XmlSerialization.cs")
+                        continue;
+                    WriteFile(outFile, outFS.ReadAllText(outFile), null);
+                }
+
+                return true;
+            }
         }
     }
 }
