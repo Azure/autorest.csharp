18428eeb85f11b04d43013c0d023584767448876
Chris Suich <chsuich@microsoft.com>
2017-10-03 12:02:29 -0400

    Added 'ExcludeFromInterface' flag to stop methods from appearing in interfaces and extensions

--- test/vanilla/AcceptanceTests.cs
+++ test/vanilla/AcceptanceTests.cs
@@ -2291,6 +2291,22 @@ namespace AutoRest.CSharp.Tests
             Assert.Null(petstoreWithEssentialSyncMethods.GetMethod("AddPetWithHttpMessages"));
         }
 
+        [Fact]
+        public async Task EnsureHiddenMethodsMissingTest()
+        {
+            // Confirm the method exists in body-complex, which hidden methods is based on
+            var knownMethod = typeof(Fixtures.AcceptanceTestsBodyComplex.IBasicOperations).GetMethod("PutValidWithHttpMessagesAsync");
+            // Make sure it is missing for hidden methods
+            var missingMethod = typeof(Fixtures.AcceptanceTestsHiddenMethods.IBasicOperations).GetMethod(knownMethod.Name);
+            Assert.Null(missingMethod);
+
+            // Use the facade method
+            using (var client = new Fixtures.AcceptanceTestsHiddenMethods.AutoRestComplexTestService(Fixture.Uri))
+            {
+                await client.Basic.PutValidAsync("abc", CMYKColors.Magenta, 2);
+            }
+        }
+
         public void EnsureTestCoverage()
         {
             using (var client =
