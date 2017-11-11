91f8d670e24e522cbf77b586894eeb3443c8da3f
Johannes Bader <jogus@live.de>
2017-11-10 16:40:05 -0800

    refen

--- src/azurefluent/CodeGeneratorCsaf.cs
+++ src/azurefluent/CodeGeneratorCsaf.cs
@@ -16,7 +16,6 @@ using AutoRest.CSharp.vanilla.Templates.Rest.Client;
 using AutoRest.CSharp.vanilla.Templates.Rest.Common;
 using AutoRest.Extensions.Azure;
 using AutoRest.Core.Utilities;
-using AutoRest.CSharp.azurefluent.Templates;
 
 namespace AutoRest.CSharp.Azure.Fluent
 {
@@ -31,106 +30,17 @@ namespace AutoRest.CSharp.Azure.Fluent
         /// <returns></returns>
         public override async Task Generate(CodeModel cm)
         {
-            var codeModel = cm as CodeModelCsa;
-            if (codeModel == null)
-            {
-                throw new InvalidCastException("CodeModel is not a Azure c# CodeModel");
-            }
+            var codeModel = cm as CodeModelCsa ?? throw new InvalidCastException("CodeModel is not a Azure c# CodeModel");
 
-            // Service client
-            var serviceClientTemplate = new AzureServiceClientTemplate { Model = codeModel };
-            await Write(serviceClientTemplate, $"Generated/{codeModel.Name}{ImplementationFileExtension}");
-
-            // Service client interface
-            var serviceClientInterfaceTemplate = new ServiceClientInterfaceTemplate { Model = codeModel };
-            await Write(serviceClientInterfaceTemplate, $"Generated/I{codeModel.Name}{ImplementationFileExtension}");
-
-            // Operations
-            foreach (MethodGroupCs group in codeModel.Operations)
-            {
-                if (!group.IsCodeModelMethodGroup)
-                {
-                    // Operation
-                    var operationsTemplate = new AzureMethodGroupTemplate { Model = group };
-                    await Write(operationsTemplate, $"Generated/{operationsTemplate.Model.TypeName}{ImplementationFileExtension}");
-
-                    // Operation interface
-                    var operationsInterfaceTemplate = new MethodGroupInterfaceTemplate { Model = group };
-                    await Write(operationsInterfaceTemplate,
-                        $"Generated/I{operationsInterfaceTemplate.Model.TypeName}{ImplementationFileExtension}");
-                }
-                var operationExtensionsTemplate = new ExtensionsTemplate { Model = group };
-                await Write(operationExtensionsTemplate,
-                    $"Generated/{group.ExtensionTypeName}Extensions{ImplementationFileExtension}");
-            }
-
-            // Models
-            foreach (CompositeTypeCs model in codeModel.ModelTypes.Concat(codeModel.HeaderTypes))
-            {
-                if (true == model.Extensions.Get<bool>(AzureExtensions.ExternalExtension))
-                {
-                    continue;
-                }
-                if (model.IsResource())
-                {
-                    continue;
-                }
-                var modelTemplate = new ModelTemplate { Model = model };
-                await Write(modelTemplate, Path.Combine("Generated", Settings.Instance.ModelsName,
-                    $"{model.Name}{ImplementationFileExtension}"));
-            }
-
-            // Enums
-            foreach (EnumTypeCs enumType in codeModel.EnumTypes)
-            {
-                var enumTemplate = new EnumTemplate { Model = enumType };
-                await Write(enumTemplate, Path.Combine("Generated", Settings.Instance.ModelsName,
-                    $"{enumTemplate.Model.Name}{ImplementationFileExtension}"));
-            }
-
-            // Page class
-            foreach (var pageClass in codeModel.pageClasses)
-            {
-                var pageTemplate = new PageTemplate
-                {
-                    Model = new Page(pageClass.Value, pageClass.Key.Key, pageClass.Key.Value)
-                };
-                await Write(pageTemplate, Path.Combine("Generated", Settings.Instance.ModelsName,
-                    $"{pageTemplate.Model.TypeDefinitionName}{ImplementationFileExtension}"));
-            }
-            // Exceptions
-            foreach (CompositeTypeCs exceptionType in codeModel.ErrorTypes)
-            {
-                if (exceptionType.Name == "CloudError")
-                {
-                    continue;
-                }
-
-                var exceptionTemplate = new ExceptionTemplate { Model = exceptionType };
-                await Write(exceptionTemplate, Path.Combine("Generated", Settings.Instance.ModelsName,
-                     $"{exceptionTemplate.Model.ExceptionTypeDefinitionName}{ImplementationFileExtension}"));
-            }
-
-            // Xml Serialization
+            await GenerateServiceClient<AzureServiceClientTemplate>(codeModel);
+            await GenerateOperations<AzureMethodGroupTemplate>(codeModel.Operations);
+            await GenerateModels(codeModel.ModelTypes.Union(codeModel.HeaderTypes).Where(m => !m.IsResource()));
+            await GenerateEnums(codeModel.EnumTypes);
+            await GeneratePageClasses(codeModel.pageClasses);
+            await GenerateExceptions(codeModel.ErrorTypes);
             if (codeModel.ShouldGenerateXmlSerialization)
             {
-                var xmlSerializationTemplate = new XmlSerializationTemplate { Model = null };
-                await Write(xmlSerializationTemplate, Path.Combine("Generated", Settings.Instance.ModelsName, $"{XmlSerialization.XmlDeserializationClass}{ImplementationFileExtension}"));
-            }
-
-            if (Settings.Instance.Host?.GetValue<bool?>("regenerate-manager").Result == true)
-            {
-                await Write(
-                    new AzureServiceManagerTemplate { Model = codeModel },
-                    codeModel.ServiceName + "Manager" + ImplementationFileExtension);
-                
-                await Write(
-                    new AzureCsprojTemplate { Model = codeModel },
-                    $"Microsoft.Azure.Management.{codeModel.ServiceName}.Fluent.csproj");
-
-                await Write(
-                    new AzureAssemblyInfoTemplate { Model = codeModel },
-                    "Properties/AssemblyInfo.cs");
+                await GenerateXmlSerialization();
             }
         }
     }
--- src/vanilla/CodeNamerCs.cs
+++ src/vanilla/CodeNamerCs.cs
@@ -100,10 +100,6 @@ namespace AutoRest.CSharp
                 }
                 if (primaryType != null)
                 {
-                    if (primaryType.KnownPrimaryType == KnownPrimaryType.Decimal)
-                    {
-                        return decimal.Parse(defaultValue).ToString() + "m";
-                    }
                     if (primaryType.KnownPrimaryType == KnownPrimaryType.Double)
                     {
                         return double.Parse(defaultValue).ToString();
