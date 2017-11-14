b513ce248a6f2fa01e46f78bf982092cd923b758
Johannes Bader <jogus@live.de>
2017-11-14 15:32:50 -0800

    decimal

--- test/vanilla/AcceptanceTests.cs
+++ test/vanilla/AcceptanceTests.cs
@@ -2355,7 +2355,7 @@ namespace AutoRest.CSharp.Tests
                     logger.LogInformation(string.Format(CultureInfo.CurrentCulture, "SKIPPED {0}.", item));
                 }
                 // TODO: This is fudging some numbers. Fixing the actual problem is a priority.
-                int totalTests = report.Count - 15;
+                int totalTests = report.Count - 7;
                 int executedTests = report.Values.Count(v => v > 0);
 
                 var nullValued = report.Where(p => p.Value == null).Select(p => p.Key);
