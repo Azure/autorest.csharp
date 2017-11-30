c8c9d65cbd9907a7c5dff305a8266c79e3f3dfbc
Johannes Bader <jogus@live.de>
2017-11-30 15:20:33 -0800

    test case

---
+++ test/unit/Deprecated.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace AutoRest.CSharp.Unit.Tests
+{
+    public class Deprecated : BugTest
+    {
+        public Deprecated(ITestOutputHelper output) : base(output)
+        {
+        }
+
+        /// <summary>
+        ///     https://github.com/Azure/autorest/issues/1285
+        ///     Deprecated on: methods, model classes, properties
+        /// </summary>
+        [Fact]
+        public async Task DeprecatedOnLotsOfStuff()
+        {
+            // simplified test pattern for unit testing aspects of code generation
+            using (var fileSystem = GenerateCodeForTestFromSpec())
+            {
+                // check for the expected class.
+                Assert.True(fileSystem.FileExists(@"PathExtensions.cs"));
+                Assert.True(fileSystem.FileExists(@"Models/PetNo.cs"));
+                Assert.True(fileSystem.FileExists(@"Models/PetYes.cs"));
+                Assert.True(fileSystem.FileExists(@"Models/Pet.cs"));
+                Assert.True(fileSystem.FileExists(@"Models/ChildPet.cs"));
+
+                var result = await Compile(fileSystem);
+
+                // filter the warnings
+                var warnings = result.Messages.Where(
+                    each => each.Severity == DiagnosticSeverity.Warning
+                            && !SuppressWarnings.Contains(each.Id)).ToArray();
+
+                // filter the errors
+                var errors = result.Messages.Where(each => each.Severity == DiagnosticSeverity.Error).ToArray();
+
+                Write(warnings, fileSystem);
+                Write(errors, fileSystem);
+
+                // Don't proceed unless we have zero Warnings.
+                Assert.Empty(warnings);
+
+                // Don't proceed unless we have zero Errors.
+                Assert.Empty(errors);
+
+                // Should also succeed.
+                Assert.True(result.Succeeded);
+
+                
+            }
+        }
+    }
+}
