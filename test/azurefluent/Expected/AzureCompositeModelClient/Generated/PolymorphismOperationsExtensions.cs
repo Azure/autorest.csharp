9466e702a9f208b9425ceae9b159e28d277af9ba
Johannes Bader <olydis@users.noreply.github.com>
2017-12-18 11:58:37 -0800

    `components` test case (#62)
    * component test case
    
    * regen

--- test/azurefluent/Expected/AzureCompositeModelClient/Generated/PolymorphismOperationsExtensions.cs
+++ test/azurefluent/Expected/AzureCompositeModelClient/Generated/PolymorphismOperationsExtensions.cs
@@ -206,6 +206,38 @@ namespace Fixtures.Azure.Fluent.AzureCompositeModelClient
             }
 
             /// <summary>
+            /// Put complex types that are polymorphic, omitting the discriminator
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='complexBody'>
+            /// </param>
+            public static SalmonInner PutMissingDiscriminator(this IPolymorphismOperations operations, SalmonInner complexBody)
+            {
+                return operations.PutMissingDiscriminatorAsync(complexBody).GetAwaiter().GetResult();
+            }
+
+            /// <summary>
+            /// Put complex types that are polymorphic, omitting the discriminator
+            /// </summary>
+            /// <param name='operations'>
+            /// The operations group for this extension method.
+            /// </param>
+            /// <param name='complexBody'>
+            /// </param>
+            /// <param name='cancellationToken'>
+            /// The cancellation token.
+            /// </param>
+            public static async Task<SalmonInner> PutMissingDiscriminatorAsync(this IPolymorphismOperations operations, SalmonInner complexBody, CancellationToken cancellationToken = default(CancellationToken))
+            {
+                using (var _result = await operations.PutMissingDiscriminatorWithHttpMessagesAsync(complexBody, null, cancellationToken).ConfigureAwait(false))
+                {
+                    return _result.Body;
+                }
+            }
+
+            /// <summary>
             /// Put complex types that are polymorphic, attempting to omit required
             /// 'birthday' field - the request should not be allowed from the client
             /// </summary>
