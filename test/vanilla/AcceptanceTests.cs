82e89fdf703570e88bec8dc9190bf4a04235f843
Deepak Shankargouda <deshank@microsoft.com>
2018-01-05 15:50:02 -0800

    Disabling ensuretestcoverage for regular test runs

--- test/vanilla/AcceptanceTests.cs
+++ test/vanilla/AcceptanceTests.cs
@@ -2368,7 +2368,6 @@ namespace AutoRest.CSharp.Tests
                 {
                     logger.LogInformation(string.Format(CultureInfo.CurrentCulture, "MISSING: {0}", item));
                 }
-                // Assert.Equal(report.Count, report.Values.Count(v => v > 0));
             }
         }
 
