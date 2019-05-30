728d864e995da1dc30727eb462532c11d2fe9ee3
Ting-Yu Lin <ting-yu.lin@agoda.com>
2019-05-30 19:46:52 +0700

    Fix tests

--- agoda.csharp.client.test/EndToEndTeset.cs
+++ agoda.csharp.client.test/EndToEndTeset.cs
@@ -1,9 +1,9 @@
 using System;
 using System.Collections.Generic;
-using Agoda.RoundRobin;
 using NUnit.Framework;
 using System.Linq;
 using Agoda.Csharp.Client.Test.Models;
+using System.Threading.Tasks;
 
 namespace Agoda.Csharp.Client.Test
 {
@@ -15,20 +15,20 @@ namespace Agoda.Csharp.Client.Test
         }
 
         [Test]
-        public void TestShouldSerializeBasicDataTypes()
+        public async Task TestShouldSerializeBasicDataTypes()
         {
             var config = new ApiBaseConfig()
             {
-                settings = new List<ServerSettings>()
+                settings = new List<string>()
                 {
-                    new ServerSettings("http://localhost:8443", 100)
+                    "http://localhost:8443"
                 },
                 name = "petstore",
                 retryCount = 0,
                 timeout = TimeSpan.FromMinutes(1)
             };
             var client = new SwaggerPetstore(config);
-            var result = client.GetPetByIdAsync(1).Result;
+            var result = await client.GetPetByIdAsync(1);
             List<Tag> expectedTag = new List<Tag>();
             expectedTag.Add(new Tag(2, "Beagle"));
             Assert.AreEqual(result.Id, 1);
@@ -37,33 +37,33 @@ namespace Agoda.Csharp.Client.Test
         }
 
         [Test]
-        public void TestShouldSerializeDateTime()
+        public async Task TestShouldSerializeDateTime()
         {
             var config = new ApiBaseConfig()
             {
-                settings = new List<ServerSettings>()
+                settings = new List<string>()
                 {
-                    new ServerSettings("http://localhost:8443", 100)
+                    "http://localhost:8443"
                 },
                 name = "petstore",
                 retryCount = 0,
                 timeout = TimeSpan.FromMinutes(1)
             };
             var client = new SwaggerPetstore(config);
-            var result = client.GetOrderByIdAsync(1).Result;
+            var result = await client.GetOrderByIdAsync(1);
             // 2019-04-30T04:36:59.348Z
             const long timeInTicks = 636921958193480000L;
             Assert.AreEqual(new DateTime(timeInTicks), result.ShipDate);
         }
         
         [Test]
-        public void TestShouldBeAbleToMakePostCalls()
+        public async Task TestShouldBeAbleToMakePostCalls()
         {
             var config = new ApiBaseConfig()
             {
-                settings = new List<ServerSettings>()
+                settings = new List<string>()
                 {
-                    new ServerSettings("http://localhost:8443", 100)
+                    "http://localhost:8443"
                 },
                 name = "petstore",
                 retryCount = 0,
@@ -71,20 +71,20 @@ namespace Agoda.Csharp.Client.Test
             };
             var pet = new Pet("dog", new List<string>(), 2, null, new List<Tag>(), "good");
             var client = new SwaggerPetstore(config);
-            var result = client.AddPetAsync(pet).Result;
+            var result = await client.AddPetAsync(pet);
             
             Assert.AreEqual(200, result.HttpCode);
         }
 
                 
         [Test]
-        public void TestShouldBeAbleToMakePostCallsWithNullValues()
+        public async Task TestShouldBeAbleToMakePostCallsWithNullValues()
         {
             var config = new ApiBaseConfig()
             {
-                settings = new List<ServerSettings>()
+                settings = new List<string>()
                 {
-                    new ServerSettings("http://localhost:8443", 100)
+                    "http://localhost:8443"
                 },
                 name = "petstore",
                 retryCount = 0,
@@ -92,20 +92,20 @@ namespace Agoda.Csharp.Client.Test
             };
             var pet = new Pet("dog", new List<string>(), null, null, new List<Tag>(), "good");
             var client = new SwaggerPetstore(config);
-            var result = client.AddPetAsync(pet).Result;
-            
+            var result = await client.AddPetAsync(pet);
+
             Assert.AreEqual(200, result.HttpCode);
         }
         
                
         [Test]
-        public void TestShouldGetCorrectHttpCode()
+        public async Task TestShouldGetCorrectHttpCode()
         {
             var config = new ApiBaseConfig()
             {
-                settings = new List<ServerSettings>()
+                settings = new List<string>()
                 {
-                    new ServerSettings("http://localhost:8443", 100)
+                    "http://localhost:8443"
                 },
                 name = "petstore",
                 retryCount = 0,
@@ -113,7 +113,7 @@ namespace Agoda.Csharp.Client.Test
             };
             var pet = new Pet("dog", new List<string>(), null, null, new List<Tag>(), "good");
             var client = new SwaggerPetstore(config);
-            var result = client.AddPetAsync(pet).Result;
+            var result = await client.AddPetAsync(pet);
             
             Assert.AreEqual(200, result.HttpCode);
             Assert.AreEqual(true, result.IsSuccessful);
