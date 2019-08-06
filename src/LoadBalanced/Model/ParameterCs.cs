f8b284cf9db3993c0daf2e06589d96d34a35fca7
Raj Kumar <Raj.Kumar@agoda.com>
2019-08-06 19:32:41 +0700

    Update the interface template which was failing when there were comments in the swagger

--- src/LoadBalanced/Model/ParameterCs.cs
+++ src/LoadBalanced/Model/ParameterCs.cs
@@ -39,5 +39,11 @@ namespace AutoRest.CSharp.LoadBalanced.Model
             }
             set => base.ModelType = value;
         }
+        
+        public string DocumentationString => (
+            this.DeprecationMessage != null
+                ? base.Documentation + "\nCAUTION: " + this.DeprecationMessage
+                : (string)base.Documentation
+        ).Trim();
     }
 }
