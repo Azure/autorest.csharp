601df841b5a946d7e2645c17fd37b99ac8d01e4b
Michael Yanni <miyanni@microsoft.com>
2019-09-13 18:00:54 -0700

    Initial project.

---
+++ src/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoRest.CSharp.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
