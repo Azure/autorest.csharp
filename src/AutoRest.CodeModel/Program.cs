ad406980ec7be9cdebfce6cccc619133e66691c0
Michael Yanni <miyanni@microsoft.com>
2019-09-25 14:37:08 -0700

    Moved projects 1 folder down. Added project for reading the code model and turning into C# code.

---
+++ src/AutoRest.CodeModel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace AutoRest.CodeModel
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            //var codeModelJson = File.ReadAllText("code-model.json");
+            //var schema = JsonSchema.FromJsonAsync(codeModelJson).Result;
+            var schema = JsonSchema.FromFileAsync("code-model.json").Result;
+            var settings = new CSharpGeneratorSettings
+            {
+                HandleReferences = true,
+                GenerateOptionalPropertiesAsNullable = true
+            };
+            var generator = new CSharpGenerator(schema, settings);
+            var file = generator.GenerateFile();
+            File.WriteAllText("CodeModel.cs", file);
+            //var types = generator.GenerateTypes();
+        }
+    }
+}
