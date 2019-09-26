963fe15e09cfb11b7085fc0777863bdfdc1231c4
Michael Yanni <miyanni@microsoft.com>
2019-09-25 17:36:06 -0700

    Created usable CodeModel classes for the generator plugins to use.

---
+++ src/AutoRest.CSharp.V3/PipelineModels/CSharpLanguage.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoRest.CSharp.V3.PipelineModels
+{
+    public class CSharpLanguage
+    {
+    }
+}
--- src/AutoRest.CodeModel/Program.cs
+++ src/AutoRest.CodeModel/Program.cs
@@ -1,4 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using NJsonSchema;
 using NJsonSchema.CodeGeneration.CSharp;
 
@@ -8,18 +12,42 @@ namespace AutoRest.CodeModel
     {
         private static void Main()
         {
-            //var codeModelJson = File.ReadAllText("code-model.json");
-            //var schema = JsonSchema.FromJsonAsync(codeModelJson).Result;
-            var schema = JsonSchema.FromFileAsync("code-model.json").Result;
+            using var webClient = new WebClient();
+            webClient.DownloadFile(@"https://raw.githubusercontent.com/Azure/perks/master/codemodel/.resources/all-in-one/json/code-model.json", "../code-model.json");
+
+            var codeModelJson = File.ReadAllText("../code-model.json")
+                .Replace("\"+\"", "\"plus\"").Replace("\"-\"", "\"minus\"");
+            var schema = JsonSchema.FromJsonAsync(codeModelJson).Result;
+
             var settings = new CSharpGeneratorSettings
             {
+                Namespace = "AutoRest.CSharp.V3.PipelineModels",
                 HandleReferences = true,
                 GenerateOptionalPropertiesAsNullable = true
             };
-            var generator = new CSharpGenerator(schema, settings);
-            var file = generator.GenerateFile();
-            File.WriteAllText("CodeModel.cs", file);
-            //var types = generator.GenerateTypes();
+            var rawFile = new CSharpGenerator(schema, settings).GenerateFile();
+            var cleanFile = String.Join(Environment.NewLine, rawFile.ToLines().Where(l => !l.Contains("[Newtonsoft.Json")))
+                .Replace($"    {Environment.NewLine}    {Environment.NewLine}", String.Empty)
+                .Replace($"    {Environment.NewLine}    }}", "    }")
+                .Replace($"    {Environment.NewLine}", Environment.NewLine)
+                .Replace("\"plus\"", "\"+\"")
+                .Replace("\"minus\"", "\"-\"")
+                .Replace("Language Csharp", "CSharpLanguage Csharp")
+                .Replace("SchemaMetadata Csharp", "CSharpSchemaMetadata Csharp")
+                .Replace("AutoRest.CSharp.V3.PipelineModels.bool.True", "true");
+            File.WriteAllText("../../AutoRest.CSharp.V3/PipelineModels/CodeModel.cs", cleanFile);
+        }
+
+        //https://stackoverflow.com/a/41176852/294804
+        private static IEnumerable<string> ToLines(this string value, bool removeEmptyLines = false)
+        {
+            using var sr = new StringReader(value);
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (removeEmptyLines && String.IsNullOrWhiteSpace(line)) continue;
+                yield return line;
+            }
         }
     }
 }
