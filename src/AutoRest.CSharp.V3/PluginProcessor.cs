7f92c7520fc428f8f51741f6c130a1e715fa92d0
Michael Yanni <miyanni@microsoft.com>
2019-10-07 15:46:33 -0700

    Remove commented code.

--- src/AutoRest.CSharp.V3/PluginProcessor.cs
+++ src/AutoRest.CSharp.V3/PluginProcessor.cs
@@ -26,17 +26,6 @@ namespace AutoRest.CSharp.V3
                 if (codeModelFile.IsNullOrEmpty()) throw new Exception("Generator did not receive the code model file.");
 
                 var codeModelYaml = await autoRest.ReadFile(codeModelFile);
-                //codeModelYaml = String.Join(Environment.NewLine, codeModelYaml.ToLines().Where(l => !l.Contains("uid:")));
-                //codeModelYaml = Regex.Replace(codeModelYaml, @"(.*)!<!.*>(.*)", "$1$2", RegexOptions.Multiline);
-                //codeModelYaml = codeModelYaml.Replace("<!CodeModel>", "<CodeModel>");
-                //codeModelYaml = codeModelYaml.Replace("!<!CodeModel>", "");
-                //codeModelYaml = codeModelYaml.Replace("primitives:", "primitives: !<!Primitives>");
-                //codeModelYaml = codeModelYaml.Replace("https: ", "https:");
-                //codeModelYaml = codeModelYaml.Replace("!<!Metadata>", "!<!OperationGroup>");
-                //codeModelYaml = String.Join(Environment.NewLine, codeModelYaml.ToLines());
-                //codeModelYaml = codeModelYaml.Replace($"{Environment.NewLine}    x-ms-metadata:{Environment.NewLine}      - url: 'https: //xkcd.com/json.html'", String.Empty);
-                //codeModelYaml = codeModelYaml.Replace("          internal: true", $"          internal: true{Environment.NewLine}          coolCat: 'make me some bacon'");
-
                 var inputFile = (await autoRest.GetValue<string[]>("input-file")).FirstOrDefault();
                 await autoRest.Message(new Message { Channel = Channel.Fatal, Text = inputFile });
                 var fileName = $"CodeModel-{Path.GetFileNameWithoutExtension(inputFile)}.yaml";
