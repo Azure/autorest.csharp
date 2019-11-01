19470d400c6408b8883984fe9e2e5a601a5d0d20
Michael Yanni <miyanni@microsoft.com>
2019-11-01 16:13:22 -0700

    Updated folder outputs and started grouping them more appropriately.

--- src/AutoRest.CSharp.V3/Plugins/ModelCreator.cs
+++ src/AutoRest.CSharp.V3/Plugins/ModelCreator.cs
@@ -13,24 +13,26 @@ namespace AutoRest.CSharp.V3.Plugins
     {
         public async Task<bool> Execute(AutoRestInterface autoRest, CodeModel codeModel, Configuration configuration)
         {
-            foreach (var schema in codeModel.Schemas.Objects ?? Enumerable.Empty<ObjectSchema>())
-            {
-                var writer = new SchemaWriter();
-                writer.WriteSchema(schema);
-                await autoRest.WriteFile($"models-cs/{schema.Language.CSharp?.Name}.cs", writer.GetFormattedCode(), "source-file-csharp");
-            }
-
+            // Every schema for debugging
             foreach (var schema in codeModel.Schemas.GetAllSchemaNodes())
             {
                 var writer = new SchemaWriter();
                 writer.WriteSchema(schema);
-                await autoRest.WriteFile($"all-cs/{schema.Language.CSharp?.Name}.cs", writer.GetFormattedCode(), "source-file-csharp");
+                await autoRest.WriteFile($"All/{schema.Language.CSharp?.Name}.cs", writer.GetFormattedCode(), "source-file-csharp");
             }
 
-            var inputFile = (await autoRest.GetValue<string[]>("input-file")).FirstOrDefault();
-            var fileName = $"CodeModel-{Path.GetFileNameWithoutExtension(inputFile)}.yaml";
-            var codeModelYaml = codeModel.Serialize();
-            await autoRest.WriteFile(fileName, codeModelYaml, "source-file-csharp");
+            var schemas = (codeModel.Schemas.Choices ?? Enumerable.Empty<ChoiceSchema>()).Cast<Schema>()
+                .Concat(codeModel.Schemas.SealedChoices ?? Enumerable.Empty<SealedChoiceSchema>())
+                .Concat(codeModel.Schemas.Objects ?? Enumerable.Empty<ObjectSchema>());
+            foreach (var schema in schemas)
+            {
+                var writer = new SchemaWriter();
+                writer.WriteSchema(schema);
+                await autoRest.WriteFile($"Models/{schema.Language.CSharp?.Name}.cs", writer.GetFormattedCode(), "source-file-csharp");
+            }
+
+            // CodeModel for debugging
+            await autoRest.WriteFile($"CodeModel-{await autoRest.GetValue<string>("title")}.yaml", codeModel.Serialize(), "source-file-csharp");
 
             return true;
         }
