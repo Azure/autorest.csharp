423aaff70ac2ab9d9167dce19efa632b97bbfa55
Michael Yanni <miyanni@microsoft.com>
2019-11-21 15:54:38 -0800

    Removed example test projects.

--- src/examples/BodyComplex/BodyComplex/Generated/Models/ComponentsSchemasSmartSalmonAdditionalproperties.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-namespace BodyComplex.Models.V20160229
-{
-    public partial class ComponentsSchemasSmartSalmonAdditionalproperties
-    {
-    }
-}
--- src/examples/Dns/Generated/Models/ZoneUpdateTags.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-namespace Azure.Dns.Models.V20180501
-{
-    public partial class ZoneUpdateTags
-    {
-    }
-}
