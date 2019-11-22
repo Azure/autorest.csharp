96c050bafbd0582190eadfde92239b979290dc6a
Pavel Krymets <pavel@krymets.com>
2019-11-22 14:57:12 -0800

    Add couple of tests

--- test/AutoRest.TestServer.Tests/BodyStringTest.cs
+++ test/AutoRest.TestServer.Tests/BodyStringTest.cs
@@ -1,32 +1,66 @@
-﻿using System.Threading.Tasks;
-using Azure.Core.Pipeline;
-using Azure.Identity;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Threading.Tasks;
 using body_string.Operations.V100;
 using NUnit.Framework;
 
 namespace AutoRest.TestServer.Tests
 {
-    public class BodyStringTest
+    public class BodyStringTest: TestServerTestBase
     {
         [Test]
         public async Task GetMbcs()
         {
             await using var server = TestServerSession.Start("string_mbcs");
 
-            var clientDiagnostics = new ClientDiagnostics(new DefaultAzureCredentialOptions());
-            var pipeline = HttpPipelineBuilder.Build(new DefaultAzureCredentialOptions());
-            var result = StringOperations.GetMbcsAsync(clientDiagnostics, pipeline, server.Host).GetAwaiter().GetResult();
+            var result = StringOperations.GetMbcsAsync(ClientDiagnostics, Pipeline, server.Host).GetAwaiter().GetResult();
             Assert.AreEqual("啊齄丂狛狜隣郎隣兀﨩ˊ〞〡￤℡㈱‐ー﹡﹢﹫、〓ⅰⅹ⒈€㈠㈩ⅠⅫ！￣ぁんァヶΑ︴АЯаяāɡㄅㄩ─╋︵﹄︻︱︳︴ⅰⅹɑɡ〇〾⿻⺁䜣€", result.Value);
         }
 
         [Test]
         public async Task PutMbcs()
         {
-            await using var server = TestServerSession.Start(true);
+            await using var server = TestServerSession.Start("string_mbcs");
 
-            var clientDiagnostics = new ClientDiagnostics(new DefaultAzureCredentialOptions());
-            var pipeline = HttpPipelineBuilder.Build(new DefaultAzureCredentialOptions());
-            var result = StringOperations.PutMbcsAsync(clientDiagnostics, pipeline, server.Host).GetAwaiter().GetResult();
+            var result = StringOperations.PutMbcsAsync(ClientDiagnostics, Pipeline, server.Host).GetAwaiter().GetResult();
+            Assert.AreEqual(200, result.Status);
+        }
+
+        [Test]
+        [Ignore("Deserializer fails")]
+        public async Task GetNull()
+        {
+            await using var server = TestServerSession.Start("string_null");
+
+            var result = StringOperations.GetNullAsync(ClientDiagnostics, Pipeline, server.Host).GetAwaiter().GetResult();
+            Assert.AreEqual(null, result.Value);
+        }
+
+        [Test]
+        public async Task PutNull()
+        {
+            await using var server = TestServerSession.Start("string_null");
+
+            var result = StringOperations.PutNullAsync(ClientDiagnostics, Pipeline, server.Host).GetAwaiter().GetResult();
+            Assert.AreEqual(200, result.Status);
+        }
+
+        [Test]
+        public async Task GetEmpty()
+        {
+            await using var server = TestServerSession.Start("string_empty");
+
+            var result = StringOperations.GetEmptyAsync(ClientDiagnostics, Pipeline, server.Host).GetAwaiter().GetResult();
+            Assert.AreEqual("", result.Value);
+        }
+
+        [Test]
+        public async Task PutEmpty()
+        {
+            await using var server = TestServerSession.Start("string_empty");
+
+            var result = StringOperations.PutEmptyAsync(ClientDiagnostics, Pipeline, server.Host).GetAwaiter().GetResult();
             Assert.AreEqual(200, result.Status);
         }
     }
