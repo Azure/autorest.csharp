94e3dc60e73d245ea10d88ce725c65db1464f04b
Michael Yanni <miyanni@microsoft.com>
2019-11-22 17:09:15 -0800

    Added test for custom-baseURL.

---
+++ test/AutoRest.TestServer.Tests/custom-baseURL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using custom_baseUrl.Operations.V100;
+using NUnit.Framework;
+
+namespace AutoRest.TestServer.Tests
+{
+    public class custom_baseURL : TestServerTestBase
+    {
+        [Test]
+        public async Task GetEmpty()
+        {
+            await using var server = TestServerSession.Start("customuri");
+
+            var result = PathsOperations.GetEmptyAsync(ClientDiagnostics, Pipeline, string.Empty, server.Host.Replace("http://", string.Empty)).GetAwaiter().GetResult();
+            Assert.AreEqual(200, result.Status);
+        }
+    }
+}
