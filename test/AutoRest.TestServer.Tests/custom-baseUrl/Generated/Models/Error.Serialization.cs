dabc504a97cbec36c605212479154ee71e85711d
Michael Yanni <miyanni@microsoft.com>
2019-11-22 16:51:42 -0800

    Updated script to allow no debug information. Added custom url generated files.

---
+++ test/AutoRest.TestServer.Tests/custom-baseUrl/Generated/Models/Error.Serialization.cs
@@ -0,0 +1,49 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+
+namespace custom_baseUrl.Models.V100
+{
+    public partial class Error
+    {
+        internal void Serialize(Utf8JsonWriter writer, bool includeName = true)
+        {
+            if (includeName)
+            {
+                writer.WriteStartObject("Error");
+            }
+            else
+            {
+                writer.WriteStartObject();
+            }
+            if (Status != null)
+            {
+                writer.WriteNumber("status", Status.Value);
+            }
+            if (Message != null)
+            {
+                writer.WriteString("message", Message);
+            }
+            writer.WriteEndObject();
+        }
+        internal static Error Deserialize(JsonElement element)
+        {
+            var result = new Error();
+            foreach (var property in element.EnumerateObject())
+            {
+                if (property.NameEquals("status"))
+                {
+                    result.Status = property.Value.GetInt32();
+                    continue;
+                }
+                if (property.NameEquals("message"))
+                {
+                    result.Message = property.Value.GetString();
+                    continue;
+                }
+            }
+            return result;
+        }
+    }
+}
