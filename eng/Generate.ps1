d3521253445c600cc4546f6d8fd394290f0db66e
Pavel Krymets <pavel@krymets.com>
2019-12-05 13:10:55 -0800

    Try to suppress

--- eng/Generate.ps1
+++ eng/Generate.ps1
@@ -39,6 +39,6 @@ foreach ($path in $paths)
     Write-Host ">" $command
 
     Invoke-Block { 
-        npx autorest-beta @debugFlags $testConfiguration --output-folder=$outputFolder --input-file=$inputFile --title=$path --namespace=$namespace
+        npx autorest-beta @debugFlags $testConfiguration --output-folder=$outputFolder --input-file=$inputFile --title=$path --namespace=$namespace 2> $null
     }
 }
