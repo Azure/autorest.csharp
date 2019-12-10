7f7cb975297d30c82411a3d0bd7d9d48d76bfe41
Michael Yanni <miyanni@microsoft.com>
2019-12-10 10:46:27 -0800

    Nested try/catch caused failures to be ignored. Inverted noReset to reset, as resetting locally causes npx to not acquire the modelerfour extension.

--- eng/CodeChecks.ps1
+++ eng/CodeChecks.ps1
@@ -1,26 +1,18 @@
 #Requires -Version 6.0
-param($name, [switch]$noDebug, [switch]$noReset)
+param($name, [switch]$noDebug, [switch]$reset)
 
 $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 1
 
-try
-{
-    Write-Host 'Downloading shared source files...'
-    & (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
+Write-Host 'Downloading shared source files...'
+& (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
 
-    Write-Host 'Generating test clients...'
-    & (Join-Path $PSScriptRoot 'Generate.ps1') @PSBoundParameters
+Write-Host 'Generating test clients...'
+& (Join-Path $PSScriptRoot 'Generate.ps1') @PSBoundParameters
 
-    Write-Host 'Checking generated file differences...'
-    git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code
-    if ($LastExitCode -ne 0) {
-        $status = (git status -s | Out-String) -replace '`n','`n    '
-        throw 'Generated code is not up to date. Please run: eng/Generate.ps1'
-    }
-}
-catch
-{
-    Write-Host -ForegroundColor Red $_
-    exit 1
+Write-Host 'Checking generated file differences...'
+git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code
+if ($LastExitCode -ne 0) {
+    $status = (git status -s | Out-String) -replace '`n','`n    '
+    throw 'Generated code is not up to date. Please run: eng/Generate.ps1'
 }
--- eng/Generate.ps1
+++ eng/Generate.ps1
@@ -1,5 +1,5 @@
 #Requires -Version 6.0
-param($name, [switch]$noDebug, [switch]$noReset)
+param($name, [switch]$noDebug, [switch]$reset)
 
 $ErrorActionPreference = 'Stop'
 
@@ -13,45 +13,37 @@ function Invoke-AutoRest($autoRestArguments) {
     }
 }
 
-try
+if ($reset)
 {
-    if (-not $noReset)
-    {
-        Invoke-AutoRest '--reset'
-    }
-
-    # General configuration
-    $repoRoot = Resolve-Path (Join-Path $PSScriptRoot '..')
-    $debugFlags = if (-not $noDebug) { '--debug', '--verbose' }
-
-    # Test server test configuration
-    $testServerDirectory = Join-Path $repoRoot 'test' 'TestServerProjects'
-    $configurationPath = Join-Path $testServerDirectory 'readme.tests.md'
-    $testServerSwaggerPath = Join-Path $repoRoot 'node_modules' '@microsoft.azure' 'autorest.testserver' 'swagger'
-    $testNames = if ($name) { $name } else { 'url', 'body-string', 'body-complex', 'custom-baseUrl', 'custom-baseUrl-more-options', 'header' }
-
-    foreach ($testName in $testNames)
-    {
-        $inputFile = Join-Path $testServerSwaggerPath "$testName.json"
-        $namespace = $testName.Replace('-', '_')
-        $autoRestArguments = "$debugFlags --require=$configurationPath --input-file=$inputFile --title=$testName --namespace=$namespace"
-        Invoke-AutoRest $autoRestArguments
-    }
-
-    # Sample configuration
-    $sampleDirectory = Join-Path $repoRoot 'samples'
-    $projectNames = 'AppConfiguration'
-
-    foreach ($projectName in $projectNames)
-    {
-        $projectDirectory = Join-Path $sampleDirectory $projectName
-        $configurationPath = Join-Path $projectDirectory 'readme.md'
-        $autoRestArguments = "$debugFlags --require=$configurationPath"
-        Invoke-AutoRest $autoRestArguments
-    }
+    Invoke-AutoRest '--reset'
 }
-catch
+
+# General configuration
+$repoRoot = Resolve-Path (Join-Path $PSScriptRoot '..')
+$debugFlags = if (-not $noDebug) { '--debug', '--verbose' }
+
+# Test server test configuration
+$testServerDirectory = Join-Path $repoRoot 'test' 'TestServerProjects'
+$configurationPath = Join-Path $testServerDirectory 'readme.tests.md'
+$testServerSwaggerPath = Join-Path $repoRoot 'node_modules' '@microsoft.azure' 'autorest.testserver' 'swagger'
+$testNames = if ($name) { $name } else { 'url', 'body-string', 'body-complex', 'custom-baseUrl', 'custom-baseUrl-more-options', 'header' }
+
+foreach ($testName in $testNames)
 {
-    Write-Host -ForegroundColor Red $_
-    exit 1
+    $inputFile = Join-Path $testServerSwaggerPath "$testName.json"
+    $namespace = $testName.Replace('-', '_')
+    $autoRestArguments = "$debugFlags --require=$configurationPath --input-file=$inputFile --title=$testName --namespace=$namespace"
+    Invoke-AutoRest $autoRestArguments
+}
+
+# Sample configuration
+$sampleDirectory = Join-Path $repoRoot 'samples'
+$projectNames = 'AppConfiguration'
+
+foreach ($projectName in $projectNames)
+{
+    $projectDirectory = Join-Path $sampleDirectory $projectName
+    $configurationPath = Join-Path $projectDirectory 'readme.md'
+    $autoRestArguments = "$debugFlags --require=$configurationPath"
+    Invoke-AutoRest $autoRestArguments
 }
