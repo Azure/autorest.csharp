cbc593260f12c6cb72ac74bb7cbe8dc755226b1d
Michael Yanni <miyanni@microsoft.com>
2019-12-10 15:31:22 -0800

    This should now work as intended.

--- eng/CodeChecks.ps1
+++ eng/CodeChecks.ps1
@@ -5,14 +5,13 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 1
 
 Write-Host 'Downloading shared source files...'
-& (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
+. (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
 
 Write-Host 'Generating test clients...'
-& (Join-Path $PSScriptRoot 'Generate.ps1') @PSBoundParameters
+. (Join-Path $PSScriptRoot 'Generate.ps1') @PSBoundParameters
 
 Write-Host 'Checking generated file differences...'
 git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code
 if ($LastExitCode -ne 0) {
-    $status = (git status -s | Out-String) -replace '`n','`n    '
     Write-Error 'Generated code is not up to date. Please run: eng/Generate.ps1'
 }
