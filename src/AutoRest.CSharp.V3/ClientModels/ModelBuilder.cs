c7193f7f8e9a32657536403a196ed3e1fe3f6584
Pavel Krymets <pavel@krymets.com>
2019-12-11 14:56:27 -0800

    Banner

--- src/AutoRest.CSharp.V3/ClientModels/ModelBuilder.cs
+++ src/AutoRest.CSharp.V3/ClientModels/ModelBuilder.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
 using System.Linq;
 using AutoRest.CSharp.V3.Pipeline.Generated;
 using AutoRest.CSharp.V3.Plugins;
