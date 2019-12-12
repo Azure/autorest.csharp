5a0030b4a93dbbbd014766fca800f0baa97357ef
Pavel Krymets <pavel@krymets.com>
2019-12-12 12:24:47 -0800

    Fixup

--- src/AutoRest.CSharp.V3/ClientModels/ClientBuilder.cs
+++ src/AutoRest.CSharp.V3/ClientModels/ClientBuilder.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
 using System.Text;
 using AutoRest.CSharp.V3.Pipeline;
@@ -21,7 +20,7 @@ namespace AutoRest.CSharp.V3.ClientModels
 
         private static ClientConstant? CreateDefaultValueConstant(Parameter requestParameter) =>
             requestParameter.ClientDefaultValue != null ?
-                ParseClientConstant(requestParameter.ClientDefaultValue, (FrameworkTypeReference)CreateType(requestParameter.Schema, requestParameter.IsNullable())) :
+                ClientModelBuilderHelpers.ParseClientConstant(requestParameter.ClientDefaultValue, (FrameworkTypeReference)ClientModelBuilderHelpers.CreateType(requestParameter.Schema, requestParameter.IsNullable())) :
                 (ClientConstant?)null;
 
         private static ClientMethod? BuildMethod(Operation operation)
@@ -53,7 +52,7 @@ namespace AutoRest.CSharp.V3.ClientModels
                 switch (requestParameter.Schema)
                 {
                     case ConstantSchema constant:
-                        constantOrParameter = ParseClientConstant(constant.Value.Value, CreateType(constant.ValueType, constant.Value.Value == null));
+                        constantOrParameter = ClientModelBuilderHelpers.ParseClientConstant(constant.Value.Value, ClientModelBuilderHelpers.CreateType(constant.ValueType, constant.Value.Value == null));
                         valueSchema = constant.ValueType;
                         break;
                     case BinarySchema _:
@@ -62,25 +61,25 @@ namespace AutoRest.CSharp.V3.ClientModels
                     //TODO: Workaround for https://github.com/Azure/autorest.csharp/pull/275
                     case ArraySchema arraySchema when arraySchema.ElementType is ConstantSchema constantInnerType:
                         constantOrParameter = new ServiceClientMethodParameter(requestParameter.CSharpName(),
-                            new CollectionTypeReference(CreateType(constantInnerType.ValueType, false), false),
+                            new CollectionTypeReference(ClientModelBuilderHelpers.CreateType(constantInnerType.ValueType, false), false),
                             CreateDefaultValueConstant(requestParameter), false);
                         break;
                     //TODO: Workaround for https://github.com/Azure/autorest.csharp/pull/275
                     case DictionarySchema dictionarySchema when dictionarySchema.ElementType is ConstantSchema constantInnerType:
                         constantOrParameter = new ServiceClientMethodParameter(requestParameter.CSharpName(),
-                            new CollectionTypeReference(CreateType(constantInnerType.ValueType, false), false),
+                            new CollectionTypeReference(ClientModelBuilderHelpers.CreateType(constantInnerType.ValueType, false), false),
                             CreateDefaultValueConstant(requestParameter), false);
                         break;
                     default:
                         constantOrParameter = new ServiceClientMethodParameter(requestParameter.CSharpName(),
-                            CreateType(requestParameter.Schema, requestParameter.IsNullable()),
+                            ClientModelBuilderHelpers.CreateType(requestParameter.Schema, requestParameter.IsNullable()),
                             CreateDefaultValueConstant(requestParameter), requestParameter.Required == true);
                         break;
                 }
 
                 if (requestParameter.Protocol.Http is HttpParameter httpParameter)
                 {
-                    SerializationFormat serializationFormat = GetSerializationFormat(valueSchema);
+                    SerializationFormat serializationFormat = ClientModelBuilderHelpers.GetSerializationFormat(valueSchema);
                     switch (httpParameter.In)
                     {
                         case ParameterLocation.Header:
@@ -126,7 +125,7 @@ namespace AutoRest.CSharp.V3.ClientModels
             if (response is SchemaResponse schemaResponse)
             {
                 var schema = schemaResponse.Schema is ConstantSchema constantSchema ? constantSchema.ValueType : schemaResponse.Schema;
-                responseType = CreateType(schema, isNullable: false);
+                responseType = ClientModelBuilderHelpers.CreateType(schema, isNullable: false);
             }
 
             return new ClientMethod(
@@ -156,15 +155,6 @@ namespace AutoRest.CSharp.V3.ClientModels
                     throw new ArgumentOutOfRangeException();
             }
         }
-
-        private static SerializationFormat GetSerializationFormat(Schema schema) => schema switch
-        {
-            UnixTimeSchema _ => SerializationFormat.DateTimeUnix,
-            DateTimeSchema dateTimeSchema when dateTimeSchema.Format == DateTimeSchemaFormat.DateTime => SerializationFormat.DateTimeISO8601,
-            DateTimeSchema dateTimeSchema when dateTimeSchema.Format == DateTimeSchemaFormat.DateTimeRfc1123 => SerializationFormat.DateTimeRFC1123,
-            DateSchema _ => SerializationFormat.Date,
-            _ => SerializationFormat.Default,
-        };
 
         private static ConstantOrParameter[] ToParts(string httpRequestUri, Dictionary<string, ConstantOrParameter> parameters)
         {
@@ -208,69 +198,8 @@ namespace AutoRest.CSharp.V3.ClientModels
 
         private static int ToStatusCode(StatusCodes arg) => int.Parse(arg.ToString().Trim('_'));
 
-        private static ClientConstant StringConstant(string s) => ParseClientConstant(s, new FrameworkTypeReference(typeof(string)));
+        private static ClientConstant StringConstant(string s) => ClientModelBuilderHelpers.ParseClientConstant(s, new FrameworkTypeReference(typeof(string)));
 
-        private static ClientModel BuildClientEnum(SealedChoiceSchema sealedChoiceSchema) => new ClientEnum(
-            sealedChoiceSchema,
-            sealedChoiceSchema.CSharpName(),
-            sealedChoiceSchema.Choices.Select(c => new ClientEnumValue(c.CSharpName(), StringConstant(c.Value))));
-
-        private static ClientModel BuildClientEnum(ChoiceSchema choiceSchema) => new ClientEnum(
-            choiceSchema,
-            choiceSchema.CSharpName(),
-            choiceSchema.Choices.Select(c => new ClientEnumValue(c.CSharpName(), StringConstant(c.Value))),
-            true);
-
-        private static ClientModel BuildClientObject(ObjectSchema objectSchema) => new ClientObject(
-            objectSchema, objectSchema.CSharpName(),
-            objectSchema.Properties.Where(property => !(property.Schema is ConstantSchema)).Select(CreateProperty),
-            objectSchema.Properties.Where(property => property.Schema is ConstantSchema).Select(CreateConstant));
-
-        private static ClientModel BuildModel(Schema schema) => schema switch
-        {
-            SealedChoiceSchema sealedChoiceSchema => BuildClientEnum(sealedChoiceSchema),
-            ChoiceSchema choiceSchema => BuildClientEnum(choiceSchema),
-            ObjectSchema objectSchema => BuildClientObject(objectSchema),
-            _ => throw new NotImplementedException()
-        };
-
-        private static ClientObjectConstant CreateConstant(Property property)
-        {
-            var constantSchema = (ConstantSchema)property.Schema;
-            FrameworkTypeReference type = (FrameworkTypeReference)CreateType(constantSchema.ValueType, false);
-            return new ClientObjectConstant(property.CSharpName(), type, ParseClientConstant(constantSchema.Value.Value, type));
-        }
-
-        private static ClientObjectProperty CreateProperty(Property property) =>
-            new ClientObjectProperty(property.CSharpName(), CreateType(property.Schema, property.IsNullable()), property.Schema.IsLazy(), property.SerializedName, GetSerializationFormat(property.Schema));
-
-        //TODO: Handle nullability properly
-        private static ClientTypeReference CreateType(Schema schema, bool isNullable) => schema switch
-        {
-            BinarySchema _ => (ClientTypeReference)new BinaryTypeReference(isNullable),
-            ByteArraySchema _ => new BinaryTypeReference(isNullable),
-            //https://devblogs.microsoft.com/dotnet/do-more-with-patterns-in-c-8-0/
-            { Type: AllSchemaTypes.Binary } => new BinaryTypeReference(false),
-            ArraySchema array => new CollectionTypeReference(CreateType(array.ElementType, false), isNullable),
-            DictionarySchema dictionary => new DictionaryTypeReference(new FrameworkTypeReference(typeof(string)), CreateType(dictionary.ElementType, isNullable)),
-            NumberSchema number => new FrameworkTypeReference(number.ToFrameworkType(), isNullable),
-            _ when schema.Type.ToFrameworkCSharpType() is Type type => new FrameworkTypeReference(type, isNullable),
-            _ => new SchemaTypeReference(schema, isNullable)
-        };
-
-        private static ClientConstant ParseClientConstant(object? value, ClientTypeReference type)
-        {
-            var normalizedValue = type switch
-            {
-                BinaryTypeReference _ when value is string base64String => Convert.FromBase64String(base64String),
-                FrameworkTypeReference frameworkType when
-                    frameworkType.Type == typeof(DateTimeOffset) &&
-                    value is string dateTimeString => DateTimeOffset.Parse(dateTimeString, styles: DateTimeStyles.AssumeUniversal),
-                FrameworkTypeReference frameworkType => Convert.ChangeType(value, frameworkType.Type),
-                _ => null
-            };
-            return new ClientConstant(normalizedValue, type);
-        }
 
         //TODO: Refactor as this is written quite... ugly.
         private static IEnumerable<(string Text, bool IsLiteral)> GetPathParts(string? path)
