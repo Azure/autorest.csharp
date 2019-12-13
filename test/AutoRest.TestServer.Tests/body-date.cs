558de5c054692da592c36f3e517109aab607919d
Michael Yanni <miyanni@microsoft.com>
2019-12-13 13:56:46 -0800

    Added tests. Only 1 ignored.

--- test/AutoRest.TestServer.Tests/body-date.cs
+++ test/AutoRest.TestServer.Tests/body-date.cs
@@ -2,8 +2,10 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Text.Json;
 using System.Threading.Tasks;
 using AutoRest.TestServer.Tests.Infrastructure;
+using body_date;
 using NUnit.Framework;
 
 namespace AutoRest.TestServer.Tests
@@ -13,27 +15,60 @@ namespace AutoRest.TestServer.Tests
         public BodyDateTest(TestServerVersion version) : base(version, "date") { }
 
         [Test]
-        public Task GetDateInvalid() => Test(async (host, pipeline) => { await Task.FromException(new Exception()); });
+        public Task GetDateInvalid() => Test((host, pipeline) =>
+        {
+            Assert.ThrowsAsync<FormatException>(async () =>
+                await DateOperations.GetInvalidDateAsync(ClientDiagnostics, pipeline, host));
+        });
 
         [Test]
-        public Task GetDateMax() => Test(async (host, pipeline) => { await Task.FromException(new Exception()); });
+        public Task GetDateMax() => Test(async (host, pipeline) =>
+        {
+            var result = await DateOperations.GetMaxDateAsync(ClientDiagnostics, pipeline, host);
+            Assert.AreEqual(DateTimeOffset.Parse("9999-12-31"), result.Value);
+        });
 
         [Test]
-        public Task GetDateMin() => Test(async (host, pipeline) => { await Task.FromException(new Exception()); });
+        public Task GetDateMin() => Test(async (host, pipeline) =>
+        {
+            var result = await DateOperations.GetMinDateAsync(ClientDiagnostics, pipeline, host);
+            Assert.AreEqual(DateTimeOffset.Parse("0001-01-01"), result.Value);
+        });
 
         [Test]
-        public Task GetDateNull() => Test(async (host, pipeline) => { await Task.FromException(new Exception()); });
+        [Ignore("https://github.com/Azure/autorest.csharp/issues/300")]
+        public Task GetDateNull() => Test(async (host, pipeline) =>
+        {
+            var result = await DateOperations.GetNullAsync(ClientDiagnostics, pipeline, host);
+            Assert.AreEqual(null, result.Value);
+        });
 
         [Test]
-        public Task GetDateOverflow() => Test(async (host, pipeline) => { await Task.FromException(new Exception()); });
+        public Task GetDateOverflow() => Test((host, pipeline) =>
+        {
+            Assert.ThrowsAsync<FormatException>(async () =>
+                await DateOperations.GetOverflowDateAsync(ClientDiagnostics, pipeline, host));
+        });
 
         [Test]
-        public Task GetDateUnderflow() => Test(async (host, pipeline) => { await Task.FromException(new Exception()); });
+        public Task GetDateUnderflow() => Test((host, pipeline) =>
+        {
+            Assert.ThrowsAsync<FormatException>(async () =>
+                await DateOperations.GetUnderflowDateAsync(ClientDiagnostics, pipeline, host));
+        });
 
         [Test]
-        public Task PutDateMax() => TestStatus(async (host, pipeline) => { await Task.FromException(new Exception()); return null; });
+        public Task PutDateMax() => TestStatus(async (host, pipeline) =>
+        {
+            var value = DateTimeOffset.Parse("9999-12-31");
+            return await DateOperations.PutMaxDateAsync(ClientDiagnostics, pipeline, value, host);
+        });
 
         [Test]
-        public Task PutDateMin() => TestStatus(async (host, pipeline) => { await Task.FromException(new Exception()); return null; });
+        public Task PutDateMin() => TestStatus(async (host, pipeline) =>
+        {
+            var value = DateTimeOffset.Parse("0001-01-01");
+            return await DateOperations.PutMinDateAsync(ClientDiagnostics, pipeline, value, host);
+        });
     }
 }
