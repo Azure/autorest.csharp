007800ebf4c62ef3bebfa9fab81932b6af400a54
Michael Yanni <miyanni@microsoft.com>
2019-12-13 16:58:23 -0800

    Added tests for body-duration.

---
+++ test/TestServerProjects/body-duration/Generated/Operations/DurationOperations.cs
@@ -0,0 +1,154 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace body_duration
+{
+    internal static class DurationOperations
+    {
+        public static async ValueTask<Response<TimeSpan>> GetNullAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_duration.GetNull");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Get;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/duration/null", false);
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        {
+                            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                            var value = Azure.Core.TypeFormatters.GetTimeSpan(document.RootElement, "P");
+                            return Response.FromValue(value, response);
+                        }
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response> PutPositiveDurationAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, TimeSpan durationBody, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_duration.PutPositiveDuration");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Put;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/duration/positiveduration", false);
+                request.Headers.Add("Content-Type", "application/json");
+                using var content = new Utf8JsonRequestContent();
+                var writer = content.JsonWriter;
+                Azure.Core.Utf8JsonWriterExtensions.WriteStringValue(writer, durationBody, "P");
+                request.Content = content;
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        return response;
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response<TimeSpan>> GetPositiveDurationAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_duration.GetPositiveDuration");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Get;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/duration/positiveduration", false);
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        {
+                            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                            var value = Azure.Core.TypeFormatters.GetTimeSpan(document.RootElement, "P");
+                            return Response.FromValue(value, response);
+                        }
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response<TimeSpan>> GetInvalidAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_duration.GetInvalid");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Get;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/duration/invalid", false);
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        {
+                            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                            var value = Azure.Core.TypeFormatters.GetTimeSpan(document.RootElement, "P");
+                            return Response.FromValue(value, response);
+                        }
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+    }
+}
