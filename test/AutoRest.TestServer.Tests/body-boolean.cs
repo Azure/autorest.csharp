23128ef13068cbaf2abfbef6dbb1be04451a35c8
Pavel Krymets <pavel@krymets.com>
2019-12-17 10:28:07 -0800

    Add boolean tests (#355)

---
+++ test/AutoRest.TestServer.Tests/body-boolean.cs
@@ -0,0 +1,51 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Text.Json;
+using System.Threading.Tasks;
+using AutoRest.TestServer.Tests.Infrastructure;
+using body_boolean;
+using NUnit.Framework;
+
+namespace AutoRest.TestServer.Tests
+{
+    public class BooleanTests : TestServerTestBase
+    {
+        public BooleanTests(TestServerVersion version) : base(version, "bool") { }
+
+        [Test]
+        public Task GetBoolFalse() => Test(async (host, pipeline) =>
+        {
+            var result = await BoolOperations.GetFalseAsync(ClientDiagnostics, pipeline, host);
+            Assert.False(result.Value);
+        });
+
+        [Test]
+        public Task GetBoolInvalid() => Test((host, pipeline) =>
+        {
+            Assert.ThrowsAsync(Is.InstanceOf<JsonException>(), async () => await BoolOperations.GetInvalidAsync(ClientDiagnostics, pipeline, host));
+        });
+
+        [Test]
+        [Ignore("Nullable return types are not implemented")]
+        public Task GetBoolNull() => Test(async (host, pipeline) =>
+        {
+            var result = await BoolOperations.GetNullAsync(ClientDiagnostics, pipeline, host);
+            Assert.False(result.Value);
+        });
+
+        [Test]
+        public Task GetBoolTrue() => Test(async (host, pipeline) =>
+        {
+            var result = await BoolOperations.GetTrueAsync(ClientDiagnostics, pipeline, host);
+            Assert.True(result.Value);
+        });
+
+        [Test]
+        public Task PutBoolFalse() => TestStatus(async (host, pipeline) => await BoolOperations.PutFalseAsync(ClientDiagnostics, pipeline, host));
+
+        [Test]
+        public Task PutBoolTrue() => TestStatus(async (host, pipeline) => await BoolOperations.PutTrueAsync(ClientDiagnostics, pipeline, host));
+
+    }
+}
---
+++ test/TestServerProjects/body-boolean/Generated/Operations/BoolOperations.cs
@@ -0,0 +1,223 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
+using Azure.Core;
+using Azure.Core.Pipeline;
+
+namespace body_boolean
+{
+    internal static class BoolOperations
+    {
+        public static async ValueTask<Response<bool>> GetTrueAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_boolean.GetTrue");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Get;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/bool/true", false);
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        {
+                            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                            var value = document.RootElement.GetBoolean();
+                            return Response.FromValue(value, response);
+                        }
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response> PutTrueAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_boolean.PutTrue");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Put;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/bool/true", false);
+                request.Headers.Add("Content-Type", "application/json");
+                using var content = new Utf8JsonRequestContent();
+                var writer = content.JsonWriter;
+                writer.WriteBooleanValue(true);
+                request.Content = content;
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        return response;
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response<bool>> GetFalseAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_boolean.GetFalse");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Get;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/bool/false", false);
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        {
+                            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                            var value = document.RootElement.GetBoolean();
+                            return Response.FromValue(value, response);
+                        }
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response> PutFalseAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_boolean.PutFalse");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Put;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/bool/false", false);
+                request.Headers.Add("Content-Type", "application/json");
+                using var content = new Utf8JsonRequestContent();
+                var writer = content.JsonWriter;
+                writer.WriteBooleanValue(false);
+                request.Content = content;
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        return response;
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response<bool>> GetNullAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_boolean.GetNull");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Get;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/bool/null", false);
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        {
+                            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                            var value = document.RootElement.GetBoolean();
+                            return Response.FromValue(value, response);
+                        }
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+        public static async ValueTask<Response<bool>> GetInvalidAsync(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000", CancellationToken cancellationToken = default)
+        {
+            if (host == null)
+            {
+                throw new ArgumentNullException(nameof(host));
+            }
+
+            using var scope = clientDiagnostics.CreateScope("body_boolean.GetInvalid");
+            scope.Start();
+            try
+            {
+                var request = pipeline.CreateRequest();
+                request.Method = RequestMethod.Get;
+                request.Uri.Reset(new Uri($"{host}"));
+                request.Uri.AppendPath("/bool/invalid", false);
+                var response = await pipeline.SendRequestAsync(request, cancellationToken).ConfigureAwait(false);
+                switch (response.Status)
+                {
+                    case 200:
+                        {
+                            using var document = await JsonDocument.ParseAsync(response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                            var value = document.RootElement.GetBoolean();
+                            return Response.FromValue(value, response);
+                        }
+                    default:
+                        throw new Exception();
+                }
+            }
+            catch (Exception e)
+            {
+                scope.Failed(e);
+                throw;
+            }
+        }
+    }
+}
