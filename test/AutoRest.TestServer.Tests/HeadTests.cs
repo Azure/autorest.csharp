29279cd0a4ebc0ef91a2951abe60532176682e85
Pavel Krymets <pavel@krymets.com>
2020-01-14 15:23:59 -0800

    Update code model

---
+++ test/AutoRest.TestServer.Tests/HeadTests.cs
@@ -0,0 +1,9 @@
+using AutoRest.TestServer.Tests.Infrastructure;
+
+namespace AutoRest.TestServer.Tests
+{
+    public class HeadTests: TestServerTestBase
+    {
+        public HeadTests(TestServerVersion version, "head") : base(version) { }
+    }
+}
