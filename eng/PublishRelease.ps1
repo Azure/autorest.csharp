711ab8f22553c68a1bdc756fab274e95f5278c8d
Pavel Krymets <pavel@krymets.com>
2020-01-24 16:57:43 -0800

    name

--- eng/PublishRelease.ps1
+++ eng/PublishRelease.ps1
@@ -1,6 +1,6 @@
 param($Artifacts, $Token, $BuildNumber, $Sha)
 
 $file = Get-ChildItem $Artifacts -Filter *.tgz
-$name = $file.Name
+$name = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
 
 npx publish-release --token $Token --repo autorest.csharp --owner azure --name $name --tag $name --notes='prerelease build' --prerelease --editRelease false --assets $file.FullName --target_commitish $Sha
