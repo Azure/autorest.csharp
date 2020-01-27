7768c6fce34ca53245d78dc0f49bd0dfa6676bcd
Pavel Krymets <pavel@krymets.com>
2020-01-27 12:32:48 -0800

    Publish Github release (#432)

---
+++ eng/PublishRelease.ps1
@@ -0,0 +1,27 @@
+param($Token, $BuildNumber, $Sha, $WorkingDirectory, $PackageJson)
+
+$WorkingDirectory = Resolve-Path $WorkingDirectory
+
+cp $PackageJson $WorkingDirectory -Force
+
+pushd $WorkingDirectory
+try
+{
+   $currentVersion = node -p -e "require('./package.json').version";
+   $devVersion="$currentVersion-dev.$BuildNumber"
+
+   Write-Host "Setting version to $devVersion"
+
+   npm version --no-git-tag-version $devVersion > Out-Null;
+   
+   $file = npm pack -q;
+   $name = [System.IO.Path]::GetFileNameWithoutExtension($file)
+
+   Write-Host "Publishing $file"
+
+cmd /c "npx -q publish-release --token $Token --repo autorest.csharp --owner azure --name $name --tag $devVersion --notes=prerelease-build --prerelease --editRelease false --assets $file --target_commitish $Sha 2>&1"
+}
+finally
+{
+    popd
+}
