0c553a3d4d730120e7fa28931ecc7651d8042b6e
Michael Yanni <miyanni@microsoft.com>
2020-02-04 13:53:28 -0800

    Replaced string replacements with custom name generators.

---
+++ src/AutoRest.CodeModel/CustomEnumNameGenerator.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using NJsonSchema;
+using NJsonSchema.CodeGeneration;
+
+namespace AutoRest.CodeModel
+{
+    internal class CustomEnumNameGenerator : IEnumNameGenerator
+    {
+        private readonly DefaultEnumNameGenerator _defaultEnumNameGenerator = new DefaultEnumNameGenerator();
+
+        public string Generate(int index, string name, object value, JsonSchema schema) =>
+            _defaultEnumNameGenerator.Generate(index, name.Replace("+", "plus").Replace("-", "minus"), value, schema);
+    }
+}
---
+++ src/AutoRest.CodeModel/CustomPropertyNameGenerator.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using NJsonSchema;
+using NJsonSchema.CodeGeneration.CSharp;
+
+namespace AutoRest.CodeModel
+{
+    internal class CustomPropertyNameGenerator : CSharpPropertyNameGenerator
+    {
+        public override string Generate(JsonSchemaProperty property)
+        {
+            if (property.IsArray)
+            {
+                property.IsRequired = true;
+            }
+
+            if (property.Name == "csharp")
+            {
+                return "CSharp";
+            }
+            return base.Generate(property);
+        }
+    }
+}
