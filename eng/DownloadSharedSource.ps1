957814c0bfa8e06c7d1e2cd9b32c5bff95b61fef
Pavel Krymets <pavel@krymets.com>
2020-02-07 15:17:20 -0800

    Update

--- eng/DownloadSharedSource.ps1
+++ eng/DownloadSharedSource.ps1
@@ -3,16 +3,11 @@ $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
 $downloadPath = Resolve-Path (Join-Path $PSScriptRoot '..' 'src' 'assets' 'Azure.Core.Shared')
-$files = 'ClientDiagnostics.cs', 'ArrayBufferWriter.cs', 'DiagnosticScope.cs', 'ResponseExceptionExtensions.cs', 'ContentTypeUtilities.cs'
+$files = 'ClientDiagnostics.cs', 'ArrayBufferWriter.cs', 'DiagnosticScope.cs', 'DiagnosticScopeFactory.cs', 'ContentTypeUtilities.cs', 'HttpMessageSanitizer.cs'
 $baseUrl = 'https://raw.githubusercontent.com/Azure/azure-sdk-for-net/master/sdk/core/Azure.Core/src/Shared/'
 
 foreach ($file in $files)
 {
     $text = (Invoke-WebRequest -Uri "$baseUrl/$file").Content
-    $text = $text.Replace('#nullable enable', '#pragma warning disable CS8600, CS8604, CS8605')
-    if ($file -eq 'ResponseExceptionExtensions.cs')
-    {
-        $text = "#nullable disable`n"  + $text
-    }
     $text.Trim() | Out-File (Join-Path $downloadPath $file)
 }
