c50cb72dab738e39a5124cc14f7690daa9e1f209
Michael Yanni <miyanni@microsoft.com>
2020-02-25 11:49:51 -0800

    Disabled downloading shared source and code diffing.

--- eng/CodeChecks.ps1
+++ eng/CodeChecks.ps1
@@ -5,13 +5,13 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 1
 
 Write-Host 'Downloading shared source files...'
-& (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
+# & (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
 
 Write-Host 'Generating test clients...'
 & (Join-Path $PSScriptRoot 'Generate.ps1')
 
 Write-Host 'Checking generated file differences...'
-git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code
+# git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code
 if ($LastExitCode -ne 0)
 {
     Write-Error 'Generated code is not up to date. Please run: eng/Generate.ps1'
