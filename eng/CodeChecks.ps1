ad57fda344f9101e9cdac0d5f0acaa8e051e170a
Pavel Krymets <pavel@krymets.com>
2020-02-26 10:47:10 -0800

    Pause the shared source downloading

--- eng/CodeChecks.ps1
+++ eng/CodeChecks.ps1
@@ -5,7 +5,7 @@ $ErrorActionPreference = 'Stop'
 Set-StrictMode -Version 1
 
 Write-Host 'Downloading shared source files...'
-& (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
+# & (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
 
 Write-Host 'Generating test clients...'
 & (Join-Path $PSScriptRoot 'Generate.ps1')
