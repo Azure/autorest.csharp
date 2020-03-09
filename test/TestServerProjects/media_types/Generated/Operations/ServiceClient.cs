d50504085d952f75179b9c3423640957cccf323e
Michael Yanni <miyanni@microsoft.com>
2020-03-09 15:08:14 -0700

    Regen after feature/v3 changes.

--- test/TestServerProjects/media_types/Generated/Operations/ServiceClient.cs
+++ test/TestServerProjects/media_types/Generated/Operations/ServiceClient.cs
@@ -18,6 +18,10 @@ namespace media_types
         private readonly ClientDiagnostics clientDiagnostics;
         private readonly HttpPipeline pipeline;
         internal ServiceRestClient RestClient { get; }
+        /// <summary> Initializes a new instance of ServiceClient for mocking. </summary>
+        protected ServiceClient()
+        {
+        }
         /// <summary> Initializes a new instance of ServiceClient. </summary>
         internal ServiceClient(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, string host = "http://localhost:3000")
         {
