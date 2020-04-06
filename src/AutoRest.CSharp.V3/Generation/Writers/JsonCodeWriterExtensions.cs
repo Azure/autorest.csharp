d9930a84b4ecdeb48c6e4737d49de289bc620089
Pavel Krymets <pavel@krymets.com>
2020-04-06 15:52:08 -0700

    rev

--- src/AutoRest.CSharp.V3/Generation/Writers/JsonCodeWriterExtensions.cs
+++ src/AutoRest.CSharp.V3/Generation/Writers/JsonCodeWriterExtensions.cs
@@ -3,7 +3,6 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using AutoRest.CSharp.V3.Generation.Types;
 using AutoRest.CSharp.V3.Output.Models.Requests;
