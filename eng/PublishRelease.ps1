72c8c370e893d1fd3776479b140fe2fa9465f50f
Pavel Krymets <pavel@krymets.com>
2020-05-13 15:01:49 -0700

    .

--- eng/PublishRelease.ps1
+++ eng/PublishRelease.ps1
@@ -22,7 +22,7 @@ try
 
    cmd /c "npx -q publish-release --token $Token --repo autorest.csharp --owner azure --name $name --tag $devVersion --notes=prerelease-build --prerelease --editRelease false --assets $file --target_commitish $Sha 2>&1"
 
-   Write-Host "##vso[task.setvariable variable=AutorestCSharpVersion;isSecret=false;isOutput=true;]https://github.com/Azure/autorest.csharp/releases/download/$devVersion/autorest-csharp-v3-$devVersion.tgz"
+   Write-Host "##vso[task.setvariable variable=AutorestCSharpVersion;isSecret=false]https://github.com/Azure/autorest.csharp/releases/download/$devVersion/autorest-csharp-v3-$devVersion.tgz"
 }
 finally
 {
