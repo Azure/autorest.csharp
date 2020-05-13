4e879d9cfca470f65e4deb3cb9734f5bdb4c55a7
Pavel Krymets <pavel@krymets.com>
2020-05-13 16:53:23 -0700

    Cover built-in type name conflicts

--- test/TestProjects/NameConflicts/Generated/ServiceClient.cs
+++ test/TestProjects/NameConflicts/Generated/ServiceClient.cs
@@ -79,6 +79,108 @@ namespace NameConflicts
             }
         }
 
+        /// <param name="httpMessage"> The HttpMessage to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual async Task<Response<HttpMessage>> HttpMessageAsync(HttpMessage httpMessage = null, CancellationToken cancellationToken = default)
+        {
+            using var scope0 = _clientDiagnostics.CreateScope("ServiceClient.HttpMessage");
+            scope0.Start();
+            try
+            {
+                return await RestClient.HttpMessageAsync(httpMessage, cancellationToken).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                scope0.Failed(e);
+                throw;
+            }
+        }
+
+        /// <param name="httpMessage"> The HttpMessage to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual Response<HttpMessage> HttpMessage(HttpMessage httpMessage = null, CancellationToken cancellationToken = default)
+        {
+            using var scope0 = _clientDiagnostics.CreateScope("ServiceClient.HttpMessage");
+            scope0.Start();
+            try
+            {
+                return RestClient.HttpMessage(httpMessage, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                scope0.Failed(e);
+                throw;
+            }
+        }
+
+        /// <param name="request"> The Request to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual async Task<Response<Request>> RequestAsync(Request request = null, CancellationToken cancellationToken = default)
+        {
+            using var scope0 = _clientDiagnostics.CreateScope("ServiceClient.Request");
+            scope0.Start();
+            try
+            {
+                return await RestClient.RequestAsync(request, cancellationToken).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                scope0.Failed(e);
+                throw;
+            }
+        }
+
+        /// <param name="request"> The Request to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual Response<Request> Request(Request request = null, CancellationToken cancellationToken = default)
+        {
+            using var scope0 = _clientDiagnostics.CreateScope("ServiceClient.Request");
+            scope0.Start();
+            try
+            {
+                return RestClient.Request(request, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                scope0.Failed(e);
+                throw;
+            }
+        }
+
+        /// <param name="response"> The Response to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual async Task<Response<Models.Response>> ResponseAsync(Models.Response response = null, CancellationToken cancellationToken = default)
+        {
+            using var scope0 = _clientDiagnostics.CreateScope("ServiceClient.Response");
+            scope0.Start();
+            try
+            {
+                return await RestClient.ResponseAsync(response, cancellationToken).ConfigureAwait(false);
+            }
+            catch (Exception e)
+            {
+                scope0.Failed(e);
+                throw;
+            }
+        }
+
+        /// <param name="response"> The Response to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        public virtual Response<Models.Response> Response(Models.Response response = null, CancellationToken cancellationToken = default)
+        {
+            using var scope0 = _clientDiagnostics.CreateScope("ServiceClient.Response");
+            scope0.Start();
+            try
+            {
+                return RestClient.Response(response, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                scope0.Failed(e);
+                throw;
+            }
+        }
+
         /// <summary> Analyze body, that could be different media types. </summary>
         /// <param name="stringBody"> The binary to use. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
