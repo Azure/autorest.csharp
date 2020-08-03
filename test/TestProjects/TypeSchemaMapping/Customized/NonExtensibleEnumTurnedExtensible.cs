b01051b067a40585e0a74259d2cd88722f31b7e4
Pavel Krymets <pavel@krymets.com>
2020-08-03 11:41:38 -0700

    Add a test for turning normal enum into extensible

---
+++ test/TestProjects/TypeSchemaMapping/Customized/NonExtensibleEnumTurnedExtensible.cs
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace NamespaceForEnums
+{
+    [CodeGenModel("NonExtensibleEnum2")]
+    internal partial struct NonExtensibleEnumTurnedExtensible
+    {
+    }
+}
