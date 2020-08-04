f569dd3d8d05667dc1fd7f7ce78136dc75692194
Shivangi Reja <shreja@microsoft.com>
2020-08-03 18:46:29 -0700

    Fix test

--- test/AutoRest.TestServer.Tests/NameConflictTests.cs
+++ test/AutoRest.TestServer.Tests/NameConflictTests.cs
@@ -12,9 +12,12 @@ namespace AutoRest.TestServer.Tests
             var modelType = typeof(MyEnumValue);
             Assert.True(modelType.IsValueType);
             Assert.False(modelType.IsEnum);
+
             Assert.AreEqual(2, modelType.GetProperties().Length);
-            TypeAsserts.HasProperty(modelType, "MyEnum", BindingFlags.Static | BindingFlags.Public);
-            TypeAsserts.HasProperty(modelType, "SecondEnumValue", BindingFlags.Static | BindingFlags.Public);
+            var propInfo1 = TypeAsserts.HasProperty(modelType, "MyEnum", BindingFlags.Static | BindingFlags.Public);
+            Assert.AreNotEqual(modelType.Name, propInfo1.Name);
+            var propInfo2 = TypeAsserts.HasProperty(modelType, "SecondEnumValue", BindingFlags.Static | BindingFlags.Public);
+            Assert.AreNotEqual(modelType.Name, propInfo2.Name);
         }
     }
 }
