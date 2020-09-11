04a812d8ff0b2d43d47e21bca4b54001fb54b67c
Pavel Krymets <pavel@krymets.com>
2020-09-11 14:54:36 -0700

    Pipelines and all

--- eng/UpdateAzureSdkForNet.ps1
+++ eng/UpdateAzureSdkForNet.ps1
@@ -8,7 +8,7 @@ $RepoRoot = Resolve-Path "$PSScriptRoot/.."
 
 $PackagesProps = "$SdkRepoRoot\eng\Packages.Data.props"
 
-(Get-Content -Raw $PackagesProps) -replace '<PackageReference Update="AutoRest.CSharp.V3" Version="*.?" />', "<PackageReference Update=`"AutoRest.CSharp.V3`" Version=`"$(Version)`" />" | `
-    Set-Content $PackagesProps
+(Get-Content -Raw $PackagesProps) -replace '<PackageReference Update="AutoRest.CSharp.V3" Version=".*?" />', "<PackageReference Update=`"AutoRest.CSharp.V3`" Version=`"$Version`" />" | `
+    Set-Content $PackagesProps -NoNewline
 
 dotnet msbuild /t:GenerateCode "$SdkRepoRoot\eng\service.proj"
