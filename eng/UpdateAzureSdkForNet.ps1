6fa76e90aec57d22b6f642366b463a6f106c1b71
Pavel Krymets <pavel@krymets.com>
2020-09-11 13:19:12 -0700

    Package generator into a nuget pakcage

--- eng/UpdateAzureSdkForNet.ps1
+++ eng/UpdateAzureSdkForNet.ps1
@@ -4,40 +4,11 @@ $SdkRepoRoot = Resolve-Path $SdkRepoRoot
 
 Write-Host "Running $Version, $SdkRepoRoot"
 
-function UpdateValue([Parameter(ValueFromPipeline=$true)]$content, $name, $value)
-{
-    Write-Host "Updating $name to $value"
-    return $content -replace "<$name>.*?</$name>", "<$name>$value</$name>";
-}
-
 $RepoRoot = Resolve-Path "$PSScriptRoot/.."
 
-if (!$Version)
-{
-    $Version = "$RepoRoot\artifacts\bin\AutoRest.CSharp.V3\Debug\netcoreapp3.0"
-}
+$PackagesProps = "$SdkRepoRoot\eng\Packages.Data.props"
 
-if ((Get-Content -Raw "$RepoRoot\readme.md") -match "version: ([\.\d]+)")
-{
-    $CoreVersion = $Matches[1];
-}
-
-if ((Get-Content -Raw "$RepoRoot\package.json") -match "`"autorest`": `"(.*?)`"")
-{
-    $AutoRestVersion = $Matches[1];
-}
-
-$CodeGenerationTargetsPath = "$SdkRepoRoot\eng\CodeGeneration.targets"
-
-Get-Content -Raw $CodeGenerationTargetsPath | `
-    UpdateValue -Name "_AutoRestCSharpVersion" -Value $Version | `
-    UpdateValue -Name "_AutoRestCoreVersion" -Value $CoreVersion | `
-    UpdateValue -Name "_AutoRestVersion" -Value $AutoRestVersion | `
-    Set-Content $CodeGenerationTargetsPath
-
-$CodeGenerationTargets | Write-Host;
-
-Remove-Item -v -Recurse -Force "$SdkRepoRoot\sdk\core\Azure.Core\src\Shared\AutoRest\"
-Copy-Item -v  -Recurse -Force "$RepoRoot\src\assets\Generator.Shared\" "$SdkRepoRoot\sdk\core\Azure.Core\src\Shared\AutoRest\"
+(Get-Content -Raw $PackagesProps) -replace '<PackageReference Update="AutoRest.CSharp.V3" Version="*.?" />', "<PackageReference Update=`"AutoRest.CSharp.V3`" Version=`"$(Version)`" />" | `
+    Set-Content $PackagesProps
 
 dotnet msbuild /t:GenerateCode "$SdkRepoRoot\eng\service.proj"
