0360ce5e22df40701552bece6d0d1bbfc6354b57
Shivangi Reja <shreja@microsoft.com>
2020-12-14 17:02:03 -0800

    Publish package on npm

--- eng/PublishRelease.ps1
+++ eng/PublishRelease.ps1
@@ -7,19 +7,27 @@ Copy-Item $PackageJson $WorkingDirectory -Force
 
 Push-Location $WorkingDirectory
 try {
-    $currentVersion = node -p -e "require('./package.json').version";
-    $devVersion = "$currentVersion-dev.$BuildNumber"
+    # $currentVersion = node -p -e "require('./package.json').version";
+    # $devVersion = "$currentVersion.$BuildNumber"
+    $devVersion = $BuildNumber
 
     Write-Host "Setting version to $devVersion"
 
     npm version --no-git-tag-version $devVersion | Out-Null;
    
     $file = npm pack -q;
-    $name = [System.IO.Path]::GetFileNameWithoutExtension($file)
+   # $name = [System.IO.Path]::GetFileNameWithoutExtension($file)
 
     Write-Host "Publishing $file"
+    
+    # git rev-list --parents HEAD --count --full-history
 
-    cmd /c "npx -q publish-release --token $Token --repo autorest.csharp --owner azure --name $name --tag $devVersion --notes=prerelease-build --prerelease --editRelease false --assets $file --target_commitish $Sha 2>&1"
+    # cmd /c "//registry.npmjs.org/:_authToken=$(Token)" > ./.npmrc
+
+    cmd /c "npm publish --registry=https://registry.npmjs.org/:_authToken=$Token --access public"
+    # npm publish --access public
+    
+    # cmd /c "npx -q publish-release --token $Token --repo autorest.csharp --owner azure --name $name --tag $devVersion --notes=prerelease-build --prerelease --editRelease false --assets $file --target_commitish $Sha 2>&1"
 
     Write-Host "##vso[task.setvariable variable=AutorestCSharpVersion;isSecret=false]https://github.com/Azure/autorest.csharp/releases/download/$devVersion/autorest-csharp-v3-$devVersion.tgz"
 }
