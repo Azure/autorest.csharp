624d38c1c67e5205b972445dcfb061064e89ecff
Pavel Krymets <pavel@krymets.com>
2021-01-05 14:26:46 -0800

    Update the UpdateAzureSdkForNet script (#969)
    To include PrivateAssets="All" see https://github.com/Azure/azure-sdk-for-net/pull/17777/files/6166e6c7d3b8534f58544d770767c7e9e44c903b#diff-93a28d9569550c68624a8ad2209a6fae1d4f88237e4b5414eed2ecac4ef8c98f

--- eng/UpdateAzureSdkForNet.ps1
+++ eng/UpdateAzureSdkForNet.ps1
@@ -8,7 +8,9 @@ $RepoRoot = Resolve-Path "$PSScriptRoot/.."
 
 $PackagesProps = "$SdkRepoRoot\eng\Packages.Data.props"
 
-(Get-Content -Raw $PackagesProps) -replace '<PackageReference Update="AutoRest.CSharp" Version=".*?" />', "<PackageReference Update=`"AutoRest.CSharp`" Version=`"$Version`" />" | `
+(Get-Content -Raw $PackagesProps) -replace`
+    '<PackageReference Update="AutoRest.CSharp" Version=".*?" />',
+    "<PackageReference Update=`"AutoRest.CSharp`" Version=`"$Version`" PrivateAssets=`"All`" />" | `
     Set-Content $PackagesProps -NoNewline
 
 dotnet msbuild /restore /t:GenerateCode "$SdkRepoRoot\eng\service.proj"
