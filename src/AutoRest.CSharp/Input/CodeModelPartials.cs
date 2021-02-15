69f11972fd79533906d499da26afedd832daa753
bquantump <53361486+bquantump@users.noreply.github.com>
2021-02-14 18:49:28 -0800

    WIP: Update code for provider name

--- src/AutoRest.CSharp/Input/CodeModelPartials.cs
+++ src/AutoRest.CSharp/Input/CodeModelPartials.cs
@@ -64,7 +64,7 @@ namespace AutoRest.CSharp.Input
 
     internal partial class DictionaryOfAny
     {
-        private static char[] _formatSplitChar = new[] {',', ' '};
+        private static char[] _formatSplitChar = new[] { ',', ' ' };
 
         public string? Accessibility => TryGetValue("x-accessibility", out object? value) ? value?.ToString() : null;
         public string? Namespace => TryGetValue("x-namespace", out object? value) ? value?.ToString() : null;
@@ -85,7 +85,10 @@ namespace AutoRest.CSharp.Input
 
         public string? HeaderCollectionPrefix => TryGetValue("x-ms-header-collection-prefix", out object? value) ? value?.ToString() : null;
     }
-
+    internal partial class OperationGroup
+    {
+        public string? ProviderName { get; set; }
+    }
     internal partial class ServiceResponse
     {
         public HttpResponse HttpResponse => Protocol.Http as HttpResponse ?? throw new InvalidOperationException($"Expected an HTTP response");
@@ -167,6 +170,7 @@ namespace AutoRest.CSharp.Input
         [YamlMember(Alias = "nextLinkOperation")]
         public Operation? NextLinkOperation { get; set; }
     }
+
 
     /// <summary>language metadata specific to schema instances</summary>
     internal partial class Language : IDictionary<string, object>
