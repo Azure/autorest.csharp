d63d4d35f0b5218af5abedfc70c2f4b22e1691e4
bquantump <53361486+bquantump@users.noreply.github.com>
2021-02-17 14:34:43 -0800

    Test commit for CI failure

--- src/AutoRest.CSharp/Output/Models/Types/OutputLibrary.cs
+++ src/AutoRest.CSharp/Output/Models/Types/OutputLibrary.cs
@@ -210,6 +210,11 @@ namespace AutoRest.CSharp.Output.Models.Types
                     }
                 }
             }
+            if (request == null || providerName == "")
+            {
+                throw new ArgumentException("Could not set ResourceType for operations group " + operationsGroup.Key +
+                "\nPlease try setting this value for this operations in the readme.md for this swagger in the operation-group-mapping section");
+            }
             return resourceName.TrimEnd('/');
         }
 
