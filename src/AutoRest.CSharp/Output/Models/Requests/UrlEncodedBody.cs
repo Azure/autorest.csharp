c32a7a493975d60ebcd64b6677b200ed33ba9830
Chris Hamons <chris.hamons@microsoft.com>
2021-02-19 15:44:47 -0600

    Remove record usage

--- src/AutoRest.CSharp/Output/Models/Requests/UrlEncodedBody.cs
+++ src/AutoRest.CSharp/Output/Models/Requests/UrlEncodedBody.cs
@@ -7,13 +7,29 @@ namespace AutoRest.CSharp.Output.Models.Requests
 {
     internal class UrlEncodedBody : RequestBody
     {
-        public record NamedReferenceOrConstant (string Name, ReferenceOrConstant Value);
+        public struct NamedReferenceOrConstant
+        {
+            public string Name { get; }
+            public ReferenceOrConstant Value { get; }
+
+            public NamedReferenceOrConstant (string name, ReferenceOrConstant value)
+            {
+                Name = name;
+                Value = value;
+            }
+
+            public void Deconstruct (out string name, out ReferenceOrConstant value)
+            {
+                name = Name;
+                value = Value;
+            }
+        }
 
         public List<NamedReferenceOrConstant> Values { get; set; }= new List<NamedReferenceOrConstant>();
 
         public void Add (string parameter, ReferenceOrConstant value)
         {
-            Values.Add(new NamedReferenceOrConstant (parameter, value));
+            Values.Add(new NamedReferenceOrConstant(parameter, value));
         }
     }
 }
