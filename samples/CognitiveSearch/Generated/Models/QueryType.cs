51d403dfe2c784add74610a982e34e72dace96b2
Chris Hamons <chris.hamons@microsoft.com>
2021-02-23 11:05:35 -0600

    Code generation

--- samples/CognitiveSearch/Generated/Models/QueryType.cs
+++ samples/CognitiveSearch/Generated/Models/QueryType.cs
@@ -7,7 +7,7 @@
 
 namespace CognitiveSearch.Models
 {
-    /// <summary> The QueryType. </summary>
+    /// <summary> Specifies the syntax of the search query. The default is &apos;simple&apos;. Use &apos;full&apos; if your query uses the Lucene query syntax. </summary>
     public enum QueryType
     {
         /// <summary> simple. </summary>
