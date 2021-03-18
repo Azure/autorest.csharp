cb8d7bb8aff204836c7d6d145c2bf9d47f4918e1
Chris Hamons <chris.hamons@microsoft.com>
2021-03-18 12:36:41 -0500

    Split CSharpGen logic into {high,low} level client

--- src/AutoRest.CSharp/AutoRest/Plugins/CSharpGen.cs
+++ src/AutoRest.CSharp/AutoRest/Plugins/CSharpGen.cs
@@ -39,99 +39,14 @@ namespace AutoRest.CSharp.AutoRest.Plugins
 
             var context = new BuildContext(await codeModelTask, configuration, sourceInputModel);
 
-            var modelWriter = new ModelWriter();
-            var clientWriter = new ClientWriter();
-            var restClientWriter = new RestClientWriter();
-            var serializeWriter = new SerializationWriter();
-            var headerModelModelWriter = new ResponseHeaderGroupWriter();
-
-            if (context.Configuration.LowLevelClient)
+            if (configuration.LowLevelClient)
             {
-                foreach (var client in context.Library.Clients)
-                {
-                    var codeWriter = new CodeWriter();
-                    var lowLevelClientWriter = new LowLevelClientWriter ();
-                    lowLevelClientWriter.WriteClient(codeWriter, client, context);
-                    project.AddGeneratedFile($"{client.Type.Name}.cs", codeWriter.ToString());
-                }
-                foreach (var model in context.Library.Models.Where(x => x.Type.Implementation is EnumType))
-                {
-                    var codeWriter = new CodeWriter();
-                    modelWriter.WriteModel(codeWriter, model);
-
-                    var name = model.Type.Name;
-                    project.AddGeneratedFile($"Models/{name}.cs", codeWriter.ToString());
-                }
-                return project;
+                LowLevelClient.ExecuteAsync (project, context, configuration);
             }
-
-            foreach (var model in context.Library.Models)
+            else
             {
-                var codeWriter = new CodeWriter();
-                modelWriter.WriteModel(codeWriter, model);
-
-                var serializerCodeWriter = new CodeWriter();
-                serializeWriter.WriteSerialization(serializerCodeWriter, model);
-
-                var name = model.Type.Name;
-                project.AddGeneratedFile($"Models/{name}.cs", codeWriter.ToString());
-                project.AddGeneratedFile($"Models/{name}.Serialization.cs", serializerCodeWriter.ToString());
+                HighLevelClient.ExecuteAsync (project, context, configuration);
             }
-
-            foreach (var client in context.Library.RestClients)
-            {
-                var restCodeWriter = new CodeWriter();
-                restClientWriter.WriteClient(restCodeWriter, client);
-
-                project.AddGeneratedFile($"{client.Type.Name}.cs", restCodeWriter.ToString());
-            }
-
-            foreach (ResponseHeaderGroupType responseHeaderModel in context.Library.HeaderModels)
-            {
-                var headerModelCodeWriter = new CodeWriter();
-                headerModelModelWriter.WriteHeaderModel(headerModelCodeWriter, responseHeaderModel);
-
-                project.AddGeneratedFile($"{responseHeaderModel.Type.Name}.cs", headerModelCodeWriter.ToString());
-            }
-
-            if (configuration.PublicClients && !configuration.AzureArm && context.Library.Clients.Count() > 0)
-            {
-                var codeWriter = new CodeWriter();
-                ClientOptionsWriter.WriteClientOptions(codeWriter, context);
-
-                var clientOptionsName = ClientBase.GetClientPrefix(context.DefaultLibraryName, context);
-                project.AddGeneratedFile($"{clientOptionsName}ClientOptions.cs", codeWriter.ToString());
-            }
-
-            foreach (var client in context.Library.Clients)
-            {
-                var codeWriter = new CodeWriter();
-                clientWriter.WriteClient(codeWriter, client, context);
-
-                project.AddGeneratedFile($"{client.Type.Name}.cs", codeWriter.ToString());
-            }
-
-            foreach (var operation in context.Library.LongRunningOperations)
-            {
-                var codeWriter = new CodeWriter();
-                LongRunningOperationWriter.Write(codeWriter, operation);
-
-                project.AddGeneratedFile($"{operation.Type.Name}.cs", codeWriter.ToString());
-            }
-
-            if (context.Configuration.AzureArm)
-            {
-                var codeWriter = new CodeWriter();
-                ManagementClientWriter.WriteClientOptions(codeWriter, context);
-                var libraryName = ManagementClientWriter.GetManagementClientPrefix(context.DefaultLibraryName);
-                project.AddGeneratedFile($"{libraryName}ManagementClientOptions.cs", codeWriter.ToString());
-
-                var clientCodeWriter = new CodeWriter();
-                ManagementClientWriter.WriteAggregateClient(clientCodeWriter, context);
-                project.AddGeneratedFile($"{libraryName}ManagementClient.cs", clientCodeWriter.ToString());
-
-            }
-
             return project;
         }
 
