736a9960280ce2dba2ae41250136d02ffcbd6875
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-03-19 16:27:05 -0700

    Create ResourceOperationsTests.cs

---
+++ test/AutoRest.TestServer.Tests/Management/TenantOnly/ResourceOperationsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using TenantOnly;
+using Azure.ResourceManager.Core;
+
+namespace AutoRest.TestServer.Tests
+{
+    public class ResourceOperationsTests
+    {
+        [TestCase (typeof(AgreementsOperations))]
+        public void ValidateBaseClass(Type type)
+        {
+            Assert.AreEqual(typeof(ResourceOperationsBase), type.BaseType);
+        }
+
+        [TestCase(typeof(AgreementsOperations))]
+        public void ValiateListAvailableLocations(Type type)
+        {
+            var method = type.GetMethod("ListAvailableLocations");
+            Assert.NotNull(method);
+        }
+    }
+}
