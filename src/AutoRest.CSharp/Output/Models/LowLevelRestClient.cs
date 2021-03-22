375a64b6f3d48aa4b5b0926691a17f926114a0c0
Chris Hamons <chris.hamons@microsoft.com>
2021-03-22 13:45:16 -0500

    More cleanup

--- src/AutoRest.CSharp/Output/Models/LowLevelRestClient.cs
+++ src/AutoRest.CSharp/Output/Models/LowLevelRestClient.cs
@@ -1,36 +1,25 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using AutoRest.CSharp.Generation.Types;
 using AutoRest.CSharp.Input;
-using AutoRest.CSharp.Output.Builders;
 using AutoRest.CSharp.Output.Models.Requests;
-using AutoRest.CSharp.Output.Models.Responses;
-using AutoRest.CSharp.Output.Models.Serialization;
 using AutoRest.CSharp.Output.Models.Shared;
 using AutoRest.CSharp.Output.Models.Types;
-using AutoRest.CSharp.Utilities;
-using Azure.Core;
-using Request = AutoRest.CSharp.Output.Models.Requests.Request;
-using StatusCodes = AutoRest.CSharp.Output.Models.Responses.StatusCodes;
 
 namespace AutoRest.CSharp.Output.Models
 {
     internal class LowLevelRestClient : ClientBase
     {
-        private RestClientBuilder<LowLevelOutputLibrary> _builder;
         private readonly OperationGroup _operationGroup;
-        // Diff
         private readonly BuildContext<LowLevelOutputLibrary> _context;
+        private RestClientBuilder<LowLevelOutputLibrary> _builder;
+
         private Dictionary<ServiceRequest, RestClientMethod>? _requestMethods;
         private RestClientMethod[]? _allMethods;
 
-        // DIFF
         protected override string DefaultAccessibility { get; } = "public";
 
         public LowLevelRestClient(OperationGroup operationGroup, BuildContext<LowLevelOutputLibrary> context) : base(context)
@@ -81,6 +70,7 @@ namespace AutoRest.CSharp.Output.Models
             {
                 foreach (var serviceRequest in operation.Requests)
                 {
+                    // See also RestClient::EnsureNormalMethods if changing 
                     if (!(serviceRequest.Protocol.Http is HttpRequest httpRequest))
                     {
                         continue;
--- src/AutoRest.CSharp/Output/Models/RestClient.cs
+++ src/AutoRest.CSharp/Output/Models/RestClient.cs
@@ -3,18 +3,14 @@
 
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
 using System.Linq;
 using AutoRest.CSharp.Generation.Types;
 using AutoRest.CSharp.Input;
-using AutoRest.CSharp.Output.Builders;
 using AutoRest.CSharp.Output.Models.Requests;
 using AutoRest.CSharp.Output.Models.Responses;
 using AutoRest.CSharp.Output.Models.Serialization;
 using AutoRest.CSharp.Output.Models.Shared;
 using AutoRest.CSharp.Output.Models.Types;
-using AutoRest.CSharp.Utilities;
 using Azure.Core;
 using Request = AutoRest.CSharp.Output.Models.Requests.Request;
 using StatusCodes = AutoRest.CSharp.Output.Models.Responses.StatusCodes;
@@ -104,11 +100,11 @@ namespace AutoRest.CSharp.Output.Models
             {
                 foreach (var serviceRequest in operation.Requests)
                 {
+                    // See also LowLevelRestClient::EnsureNormalMethods if changing 
                     if (!(serviceRequest.Protocol.Http is HttpRequest httpRequest))
                     {
                         continue;
                     }
-
                     var headerModel = _context.Library.FindHeaderModel(operation);
                     _requestMethods.Add(serviceRequest, _builder.BuildMethod(operation, httpRequest, serviceRequest.Parameters, headerModel));
                 }
