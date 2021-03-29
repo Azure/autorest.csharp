882e8b194396c74a7e7f5ab1d1274cbc5a51aacd
bquantump <53361486+bquantump@users.noreply.github.com>
2021-03-29 12:26:05 -0700

    Updates test

--- test/AutoRest.TestServer.Tests/TestMgmtParent.cs
+++ test/AutoRest.TestServer.Tests/TestMgmtParent.cs
@@ -23,7 +23,6 @@ namespace AutoRest.TestServer.Tests
         [Test]
         public async Task TestParentComputer()
         {
-            string[] args = {"--standalone $(SolutionDir)\\samples\\Azure.ResourceManager.Sample\\Generated"};
             var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             basePath = Path.Combine(basePath.Substring(0, basePath.IndexOf("autorest.csharp")), "autorest.csharp", "samples", "Azure.ResourceManager.Sample", "Generated");
             var configuration = StandaloneGeneratorRunner.LoadConfiguration(basePath, File.ReadAllText(Path.Combine(basePath, "Configuration.json")));
@@ -38,17 +37,36 @@ namespace AutoRest.TestServer.Tests
             {
                 Assert.IsNotNull(operations.Parent);
                 if (operations.Key.Equals("VirtualMachineExtensionImages"))
-                {
                     Assert.IsTrue(operations.Parent.Equals("Microsoft.Compute/locations/publishers"));
-                }
                 else if (operations.Key.Equals("AvailabilitySets"))
-                {
-                   Assert.IsTrue(operations.Parent.Equals("resourceGroups"));
-                }
+                    Assert.IsTrue(operations.Parent.Equals("resourceGroups"));
                 else if (operations.Key.Equals("DedicatedHosts"))
-                {
                     Assert.IsTrue(operations.Parent.Equals("Microsoft.Compute/hostGroups"));
-                }
+            }
+        }
+
+        [Test]
+        public async Task TestTenant()
+        {
+            var basePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            basePath = Path.Combine(basePath.Substring(0, basePath.IndexOf("autorest.csharp")), "autorest.csharp", "test", "TestProjects", "TenantOnly", "Generated");
+            var configuration = StandaloneGeneratorRunner.LoadConfiguration(basePath, File.ReadAllText(Path.Combine(basePath, "Configuration.json")));
+            var codeModelTask = Task.Run(() => CodeModelSerialization.DeserializeCodeModel(File.ReadAllText(Path.Combine(basePath, "CodeModel.yaml"))));
+            var projectDirectory = Path.Combine(configuration.OutputFolder, Configuration.ProjectRelativeDirectory);
+            var project = await GeneratedCodeWorkspace.Create(projectDirectory, configuration.OutputFolder, configuration.SharedSourceFolders);
+            var sourceInputModel = new SourceInputModel(await project.GetCompilationAsync());
+            var model = await codeModelTask;
+            var context = new BuildContext(model, configuration, sourceInputModel);
+            _ = context.Library;
+            foreach (var operations in model.OperationGroups)
+            {
+                Assert.IsNotNull(operations.Parent);
+                if (operations.Key.Equals("AvailableBalances") || operations.Key.Equals("Instructions"))
+                    Assert.IsTrue(operations.Parent.Equals("Microsoft.Billing/billingAccounts/billingProfiles"));
+                else if (operations.Key.Equals("Agreements"))
+                    Assert.IsTrue(operations.Parent.Equals("Microsoft.Billing/billingAccounts"));
+                else
+                    Assert.IsTrue(operations.Parent.Equals("tenant"));
             }
         }
     }
