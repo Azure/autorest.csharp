ab9d5dd76ec8806c22de95042a232d31e6633580
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-03-29 21:17:13 -0700

    Update Azure.ResourceManager.Core version

--- src/AutoRest.CSharp/AutoRest/Plugins/CSharpProj.cs
+++ src/AutoRest.CSharp/AutoRest/Plugins/CSharpProj.cs
@@ -27,7 +27,7 @@ namespace AutoRest.CSharp.AutoRest.Plugins
 {0}
   <ItemGroup>
     <PackageReference Include=""Azure.Core"" Version=""1.10.0"" />
-    <PackageReference Include=""Azure.ResourceManager.Core"" Version=""1.0.0-alpha.20210323.1"" />
+    <PackageReference Include=""Azure.ResourceManager.Core"" Version=""1.0.0-alpha.20210329.1"" />
   </ItemGroup>
 
 </Project>
