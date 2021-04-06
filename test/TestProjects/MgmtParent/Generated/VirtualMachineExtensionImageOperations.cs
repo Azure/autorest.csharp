9b9fb2f050e087e7e65e52c2a354b405f6493756
m-nash <prognash@microsoft.com>
2021-04-05 21:20:41 -0700

    regen after merge

--- test/TestProjects/MgmtParent/Generated/VirtualMachineExtensionImageOperations.cs
+++ test/TestProjects/MgmtParent/Generated/VirtualMachineExtensionImageOperations.cs
@@ -5,14 +5,51 @@
 
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.ResourceManager.Core;
+
 namespace MgmtParent
 {
     /// <summary> A class representing the operations that can be performed over a specific VirtualMachineExtensionImage. </summary>
-    public partial class VirtualMachineExtensionImageOperations
+    public partial class VirtualMachineExtensionImageOperations : ResourceOperationsBase<VirtualMachineExtensionImage>
     {
         /// <summary> Initializes a new instance of VirtualMachineExtensionImageOperations for mocking. </summary>
         protected VirtualMachineExtensionImageOperations()
         {
+        }
+
+        private static readonly ResourceType ResourceType = "MgmtParent/VirtualMachineExtensionImageOperations";
+        protected override ResourceType ValidResourceType => ResourceType;
+
+        /// <inheritdoc />
+        public override ArmResponse<VirtualMachineExtensionImage> Get(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public override Task<ArmResponse<VirtualMachineExtensionImage>> GetAsync(CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary> Lists all available geo-locations. </summary>
+        /// <returns> A collection of location that may take multiple service requests to iterate over. </returns>
+        public IEnumerable<LocationData> ListAvailableLocations()
+        {
+            return ListAvailableLocations(ResourceType);
+        }
+
+        /// <summary> Lists all available geo-locations. </summary>
+        /// <param name="cancellationToken"> A token to allow the caller to cancel the call to the service. The default value is <see cref="P: System.Threading.CancellationToken.None" />. </param>
+        /// <returns> An async collection of location that may take multiple service requests to iterate over. </returns>
+        /// <exception cref="InvalidOperationException"> The default subscription id is null. </exception>
+        public async Task<IEnumerable<LocationData>> ListAvailableLocationsAsync(CancellationToken cancellationToken = default)
+        {
+            return await ListAvailableLocationsAsync(ResourceType, cancellationToken);
         }
     }
 }
