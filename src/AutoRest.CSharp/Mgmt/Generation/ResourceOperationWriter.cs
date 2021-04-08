4a4fe172137b4482ca3b26dc2d3554e0478320a9
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-04-08 14:17:06 -0700

    Missing using statement

--- src/AutoRest.CSharp/Mgmt/Generation/ResourceOperationWriter.cs
+++ src/AutoRest.CSharp/Mgmt/Generation/ResourceOperationWriter.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
 using AutoRest.CSharp.Generation.Writers;
 using AutoRest.CSharp.Mgmt.Output;
 
