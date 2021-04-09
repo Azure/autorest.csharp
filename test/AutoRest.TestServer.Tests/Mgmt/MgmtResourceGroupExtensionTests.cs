2295f0b36ec056f36cb2cad5d069165c976dc2db
Harvey Chen <30464227+HarveyLink@users.noreply.github.com>
2021-04-09 09:35:29 +0800

    Update Tests

---
+++ test/AutoRest.TestServer.Tests/Mgmt/MgmtResourceGroupExtensionTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoRest.CSharp.Input;
+using NUnit.Framework;
+using MgmtResourceGroupExtension;
+
+namespace AutoRest.TestServer.Tests.Mgmt
+{
+    public class MgmtResourceGroupExtensionTests
+    {
+        [Test]
+        public void TestResourceGroupExtensionTests()
+        {
+            var errorCode = 0;
+            foreach (var method in typeof(MgmtResourceGroupExtension.ResourceGroupExtensions).GetMethods())
+            {
+                if (method.DeclaringType.Name.Equals("Object"))
+                    continue;
+                if (!method.Name.Equals("GetAvailabilitySets"))
+                    errorCode++;
+                if (!method.ReturnType.Name.Equals("AvailabilitySetContainer"))
+                    errorCode++;
+                if (method.Name.Equals("GetVirtualMachineExtensionImages"))
+                    errorCode++;
+                if (method.ReturnType.Name.Equals("VirtualMachineExtensionImageContainer"))
+                    errorCode++;
+                if (!method.IsStatic)
+                    errorCode++;
+                foreach (var parameter in method.GetParameters())
+                {
+                    if (!parameter.Name.Equals("resourceGroup"))
+                        errorCode++;
+                    if (!parameter.ParameterType.Name.Equals("ResourceGroupOperations"))
+                        errorCode++;
+                }
+            }
+            Assert.IsTrue(errorCode == 0);
+        }
+    }
+}
