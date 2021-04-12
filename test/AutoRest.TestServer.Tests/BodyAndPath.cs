d71282b57fafda43501b046767eb9b05c716a4bb
Matt Ellis <matell@microsoft.com>
2021-04-12 10:45:53 -0700

    [LLC] Make `requestBody` last required parameter (#1140)
    Upon using the generator for some services that have operations with
    both a body and required path parameters, we noticed that having the
    body parameter come first in the parameter list ends up feeling weird
    when there are other required parameters.
    
    Move the body paramter to the end of the required parameter list so
    the generated code is more plesent to use.

---
+++ test/AutoRest.TestServer.Tests/BodyAndPath.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Reflection;
+using System.Threading;
+using Azure.Core;
+using BodyAndPath;
+using NUnit.Framework;
+
+namespace AutoRest.TestServer.Tests
+{
+    public class BodayAndPath
+    {
+        [Test]
+        public void BodyParameterIsLast()
+        {
+            ParameterInfo[] parameters = typeof(BodyAndPathClient).GetMethod("Create").GetParameters();
+            Assert.AreEqual(typeof(string), parameters[0].ParameterType);
+            Assert.AreEqual(typeof(RequestContent), parameters[1].ParameterType);
+            Assert.AreEqual(typeof(CancellationToken), parameters[2].ParameterType);
+        }
+    }
+}
