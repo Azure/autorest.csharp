057fafd1e7fc4701d0229dd14839eb2bced1cce8
Chris Hamons <chris.hamons@microsoft.com>
2021-04-13 12:18:39 -0500

    Code review change

--- src/AutoRest.CSharp/LowLevel/Output/LowLevelRestClient.cs
+++ src/AutoRest.CSharp/LowLevel/Output/LowLevelRestClient.cs
@@ -75,7 +75,7 @@ namespace AutoRest.CSharp.Output.Models
                     }
 
                     Request request = new Request (method.Request.HttpMethod, method.Request.PathSegments, method.Request.Query, method.Request.Headers, body);
-                    yield return new RestClientMethod (method.Name, method.Description, method.ReturnType, request, parameters.ToArray(), method.Responses, method.HeaderModel, method.BufferResponse, accessibility);
+                    yield return new RestClientMethod (method.Name, method.Description, method.ReturnType, request, parameters.ToArray(), method.Responses, method.HeaderModel, method.BufferResponse, method.Accessibility);
                 }
             }
         }
