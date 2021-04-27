34d163ce24807a9b738f0491c4232a1a4d83481e
Chris Hamons <chris.hamons@microsoft.com>
2021-04-27 15:45:30 -0500

    Improve tests

--- test/AutoRest.Shared.Tests/WriterExtensionTests.cs
+++ test/AutoRest.Shared.Tests/WriterExtensionTests.cs
@@ -13,34 +13,39 @@ namespace Azure.Core.Tests
     {
         private static object[] ObjectCases =
         {
-            new object[] { null },
-            new object[] { new byte[] { 1, 2, 3, 4} },
-            new object[] { 42 },
-            new object[] { 42.0m, },
-            new object[] { 42.0d },
-            new object[] { 42.0f },
-            new object[] { 42L },
-            new object[] { "asdf" },
-            new object[] { false },
-            new object[] { Guid.NewGuid() },
-            new object[] { new BinaryData (new byte[] { 1, 2, 3, 4}) },
-            new object[] { new DateTimeOffset (2001, 1, 1, 1, 1, 1, 1, new TimeSpan ()) },
-            new object[] { DateTime.Now },
-            new object[] { (IEnumerable<object>)new List<object>() { 1, 2, 3, 4 } },
+            new object[] { null, "null" },
+            new object[] { new byte[] { 1, 2, 3, 4}, @"""AQIDBA==""" },
+            new object[] { 42, "42" },
+            new object[] { 42.0m, "42.0" },
+            new object[] { 42.0d, "42" },
+            new object[] { 42.0f, "42" },
+            new object[] { 42L, "42" },
+            new object[] { "asdf", @"""asdf""" },
+            new object[] { false, "false" },
+            new object[] { new Guid ("c6125705-61d7-4cd6-8d6c-f7f247a7a5fa"), @"""c6125705-61d7-4cd6-8d6c-f7f247a7a5fa""" },
+            new object[] { new BinaryData (new byte[] { 1, 2, 3, 4}), @"""AQIDBA==""" },
+            new object[] { new DateTimeOffset (2001, 1, 1, 1, 1, 1, 1, new TimeSpan ()), @"""2001-01-01T01:01:01.0010000Z""" },
+            new object[] { new DateTime (2001, 1, 1), @"""2001-01-01T00:00:00.0000000-06:00""" },
+            new object[] { (IEnumerable<object>)new List<object>() { 1, 2, 3, 4 }, "[1,2,3,4]" },
             new object[] { (IEnumerable<KeyValuePair<string, object>>)new List<KeyValuePair<string, object>>() {
                 KeyValuePair.Create<string, object> ("a", (object)1),
                 KeyValuePair.Create<string, object> ("b", (object)2),
                 KeyValuePair.Create<string, object> ("c", (object)3),
                 KeyValuePair.Create<string, object> ("d", (object)4),
-            }},
+            },
+            @"{""a"":1,""b"":2,""c"":3,""d"":4}"
+            },
         };
 
         [Test, TestCaseSource("ObjectCases")]
-        public static void WriteObjectValue (object value)
+        public static void WriteObjectValue (object value, string expectedJson)
         {
             using MemoryStream stream = new MemoryStream ();
             using Utf8JsonWriter writer = new Utf8JsonWriter (stream);
             writer.WriteObjectValue (value);
+
+            writer.Flush ();
+            Assert.AreEqual (expectedJson, System.Text.Encoding.UTF8.GetString(stream.ToArray()));
         }
 
         [Test]
@@ -53,6 +58,9 @@ namespace Azure.Core.Tests
             Dictionary<string, object> content = JsonSerializer.Deserialize<Dictionary<string, object>>(json);
             JsonElement element = (JsonElement)content["TablesToMove"];
             writer.WriteObjectValue (element);
+            writer.Flush ();
+
+            Assert.AreEqual (@"[{""TableName"":""TestTable""}]", System.Text.Encoding.UTF8.GetString(stream.ToArray()));
         }
 
         [Test]
