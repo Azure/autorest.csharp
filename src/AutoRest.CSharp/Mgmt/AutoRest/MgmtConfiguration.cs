1ead2064530dd359c46723e463676faeea07a961
Allen Zhang <allenzhang@live.com>
2021-05-18 12:25:36 -0700

    Fixing configuration to make them all optional in readme and configuration.json.

--- src/AutoRest.CSharp/Mgmt/AutoRest/MgmtConfiguration.cs
+++ src/AutoRest.CSharp/Mgmt/AutoRest/MgmtConfiguration.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json;
 using AutoRest.CSharp.AutoRest.Communication;
 
@@ -12,58 +13,70 @@ namespace AutoRest.CSharp.AutoRest.Plugins
     {
         public MgmtConfiguration(
             string[] operationGroupIsTuple,
+            string[] singletonResource,
             JsonElement? operationGroupToResourceType = default,
             JsonElement? operationGroupToResource = default,
-            JsonElement? operationGroupToParent = default,
-            JsonElement? singletonResource = default)
+            JsonElement? operationGroupToParent = default)
         {
             OperationGroupToResourceType = operationGroupToResourceType?.ValueKind == JsonValueKind.Undefined ? new Dictionary<string, string>() : JsonSerializer.Deserialize<Dictionary<string, string>>(operationGroupToResourceType.ToString());
             OperationGroupToResource = operationGroupToResource?.ValueKind == JsonValueKind.Undefined ? new Dictionary<string, string>() : JsonSerializer.Deserialize<Dictionary<string, string>>(operationGroupToResource.ToString());
             OperationGroupToParent = operationGroupToParent?.ValueKind == JsonValueKind.Undefined ? new Dictionary<string, string>() : JsonSerializer.Deserialize<Dictionary<string, string>>(operationGroupToParent.ToString());
-            SingletonResource = singletonResource?.ValueKind == JsonValueKind.Undefined ? new List<string>() : JsonSerializer.Deserialize<List<string>>(singletonResource.ToString());
+            SingletonResource = singletonResource;
             OperationGroupIsTuple = operationGroupIsTuple;
         }
 
         public IReadOnlyDictionary<string, string> OperationGroupToResourceType { get; }
         public IReadOnlyDictionary<string, string> OperationGroupToResource { get; }
         public IReadOnlyDictionary<string, string> OperationGroupToParent { get; }
-        public IReadOnlyList<string> SingletonResource { get; }
+        public string[] SingletonResource { get; }
         public string[] OperationGroupIsTuple { get; }
 
         internal static MgmtConfiguration GetConfiguration(IPluginCommunication autoRest)
         {
             return new MgmtConfiguration(
                 autoRest.GetValue<string[]?>("operation-group-is-tuple").GetAwaiter().GetResult() ?? Array.Empty<string>(),
+                autoRest.GetValue<string[]?>("singleton-resource").GetAwaiter().GetResult() ?? Array.Empty<string>(),
                 autoRest.GetValue<JsonElement?>("operation-group-to-resource-type").GetAwaiter().GetResult(),
                 autoRest.GetValue<JsonElement?>("operation-group-to-resource").GetAwaiter().GetResult(),
-                autoRest.GetValue<JsonElement?>("operation-group-to-parent").GetAwaiter().GetResult(),
-                autoRest.GetValue<JsonElement?>("singleton-resource").GetAwaiter().GetResult());
+                autoRest.GetValue<JsonElement?>("operation-group-to-parent").GetAwaiter().GetResult());
         }
 
         internal void SaveConfiguration(Utf8JsonWriter writer)
         {
-            writer.WriteStartObject(nameof(OperationGroupToResourceType));
-            foreach (var keyval in OperationGroupToResourceType)
+            if (OperationGroupToResourceType.Count > 0)
             {
-                writer.WriteString(keyval.Key, keyval.Value);
-            }
-            writer.WriteEndObject();
+                writer.WriteStartObject(nameof(OperationGroupToResourceType));
+                foreach (var keyval in OperationGroupToResourceType)
+                {
+                    writer.WriteString(keyval.Key, keyval.Value);
+                }
 
-            writer.WriteStartObject(nameof(OperationGroupToResource));
-            foreach (var keyval in OperationGroupToResource)
+                writer.WriteEndObject();
+            }
+
+            if (OperationGroupToResource.Count > 0)
             {
-                writer.WriteString(keyval.Key, keyval.Value);
-            }
-            writer.WriteEndObject();
+                writer.WriteStartObject(nameof(OperationGroupToResource));
+                foreach (var keyval in OperationGroupToResource)
+                {
+                    writer.WriteString(keyval.Key, keyval.Value);
+                }
 
-            writer.WriteStartObject(nameof(OperationGroupToParent));
-            foreach (var keyval in OperationGroupToParent)
+                writer.WriteEndObject();
+            }
+
+            if (OperationGroupToParent.Count > 0)
             {
-                writer.WriteString(keyval.Key, keyval.Value);
-            }
-            writer.WriteEndObject();
+                writer.WriteStartObject(nameof(OperationGroupToParent));
+                foreach (var keyval in OperationGroupToParent)
+                {
+                    writer.WriteString(keyval.Key, keyval.Value);
+                }
 
-            if (SingletonResource.Count > 0)
+                writer.WriteEndObject();
+            }
+
+            if (SingletonResource.Length > 0)
             {
                 writer.WriteStartArray(nameof(SingletonResource));
                 foreach (var r in SingletonResource)
@@ -74,34 +87,40 @@ namespace AutoRest.CSharp.AutoRest.Plugins
                 writer.WriteEndArray();
             }
 
-            writer.WriteStartArray(nameof(OperationGroupIsTuple));
-            foreach (var tuple in OperationGroupIsTuple)
+            if (OperationGroupIsTuple.Length > 0)
             {
-                writer.WriteStringValue(tuple);
+                writer.WriteStartArray(nameof(OperationGroupIsTuple));
+                foreach (var tuple in OperationGroupIsTuple)
+                {
+                    writer.WriteStringValue(tuple);
+                }
+
+                writer.WriteEndArray();
             }
-            writer.WriteEndArray();
         }
 
         internal static MgmtConfiguration LoadConfiguration(JsonElement root)
         {
-            var operationGroupIsTuple = new List<string>();
-
-            foreach (var tuple in root.GetProperty(nameof(OperationGroupIsTuple)).EnumerateArray())
-            {
-                operationGroupIsTuple.Add(tuple.ToString());
-            }
-
-            root.TryGetProperty(nameof(SingletonResource), out var singletonList);
+            root.TryGetProperty(nameof(OperationGroupIsTuple), out var operationGroupIsTuple);
+            root.TryGetProperty(nameof(SingletonResource), out var singletonResource);
             root.TryGetProperty(nameof(OperationGroupToResourceType), out var operationGroupToResourceType);
             root.TryGetProperty(nameof(OperationGroupToResource), out var operationGroupToResource);
             root.TryGetProperty(nameof(OperationGroupToParent), out var operationGroupToParent);
 
+            var operationGroupIsTupleList = operationGroupIsTuple.ValueKind == JsonValueKind.Array
+                ? operationGroupIsTuple.EnumerateArray().Select(t => t.ToString()).ToArray()
+                : new string[0];
+
+            var singletonList = singletonResource.ValueKind == JsonValueKind.Array
+                ? singletonResource.EnumerateArray().Select(t => t.ToString()).ToArray()
+                : new string[0];
+
             return new MgmtConfiguration(
-                operationGroupIsTuple.ToArray(),
+                operationGroupIsTupleList,
+                singletonList,
                 operationGroupToResourceType,
                 operationGroupToResource,
-                operationGroupToParent,
-                singletonList);
+                operationGroupToParent);
         }
     }
 }
