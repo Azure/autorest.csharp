79e240f6d391b516c599641edfda1fd4076d012b
Allen Zhang <allenzhang@live.com>
2021-05-20 18:55:07 -0700

    Adding singleton tests (#1259)
    * Initial Singleton Compile fine
    
    * Updated singleton detection to look into path first then override.
    
    * Remove override and fix issue with config reading missing sections.
    
    * Adding GetOperation on parent operation for singleton
    
    * Initial Singleton Compile fine
    
    * Remove override and fix issue with config reading missing sections.
    
    * Adding GetOperation on parent operation for singleton
    
    * Fixing configuration to make them all optional in readme and configuration.json.
    
    * Fix merge conflicts.
    
    * CI fix.
    
    * CI fix
    
    * CI fix protected internal
    
    * CI fix
    
    * iteration 2
    
    * Generating files.
    
    * Fixing swagger
    
    * inline parameter definition
    
    * Added test server test case
    
    * autorest tests added.
    
    * Adding a test and fixing CI
    
    * Sync latest and fix CI.
    
    Co-authored-by: Yeming Liu <Yeming.Liu@microsoft.com>

---
+++ test/AutoRest.TestServer.Tests/Mgmt/TestProjects/MgmtSingletonTests.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Linq;
+using NUnit.Framework;
+
+namespace AutoRest.TestServer.Tests.Mgmt.TestProjects
+{
+    public class MgmtSingletonTests : TestProjectTests
+    {
+        public MgmtSingletonTests() : base("MgmtSingleton") { }
+
+        [TestCase("ParentResourceOperations", true)]
+        [TestCase("SingletonResourceOperations", true)]
+        [TestCase("SingletonResource2Operations", true)]
+        public void ValidateOperations(string operation, bool isExists)
+        {
+            var operationTypeExists = FindAllOperations().Any(o => o.Name == operation);
+            Assert.AreEqual(isExists, operationTypeExists);
+        }
+
+        [TestCase("ParentResource", true)]
+        [TestCase("SingletonResource", true)]
+        [TestCase("SingletonResource2", true)]
+        public void ValidateResources(string resource, bool isExists)
+        {
+            var resourceTypeExists = FindAllResources().Any(o => o.Name == resource);
+            Assert.AreEqual(isExists, resourceTypeExists);
+        }
+
+        [TestCase("ParentResourceContainer", true)]
+        [TestCase("SingletonResourceContainer", false)]
+        [TestCase("SingletonResource2Container", false)]
+        public void ValidateContainers(string container, bool isExists)
+        {
+            var containerTypeExists = FindAllContainers().Any(o => o.Name == container);
+            Assert.AreEqual(isExists, containerTypeExists);
+        }
+    }
+}
---
+++ test/TestProjects/MgmtSingleton/readme.md
@@ -0,0 +1,21 @@
+# ExactMatchInheritance
+### AutoRest Configuration
+> see https://aka.ms/autorest
+
+``` yaml
+title: MgmtSingleton
+require: $(this-folder)/../../../readme.md
+azure-arm: true
+model-namespace: false
+input-file: $(this-folder)/MgmtSingleton.json
+namespace: MgmtSingleton
+operation-group-to-resource-type:
+  SingletonResources: Microsoft.Billing/parentResources/singletonResources
+  SingletonResources2: Microsoft.Billing/parentResources/singletonResources2
+operation-group-to-parent:
+  SingletonResources: ParentResources
+operation-group-to-resource:
+  SingletonResources: SingletonResource
+  SingletonResources2: SingletonResource2
+singleton-resource: SingletonResource2
+```
