975940d111b28d9060a6d028fb4e6421db436155
Jonathan Cardenas <JonathanCrd@users.noreply.github.com>
2021-05-27 15:47:43 -0700

    Re-run generation script after merge

--- test/TestProjects/MgmtOperations/Generated/AvailabilitySetChildContainer.cs
+++ test/TestProjects/MgmtOperations/Generated/AvailabilitySetChildContainer.cs
@@ -220,33 +220,11 @@ namespace MgmtOperations
             return new PhWrappingPageable<GenericResource, AvailabilitySetChild>(results, genericResource => new AvailabilitySetChildOperations(genericResource, genericResource.Id as ResourceGroupResourceIdentifier).Get().Value);
         }
 
-        /// <summary> Filters the list of <see cref="AvailabilitySetChild" /> for this resource group. Makes an additional network call to retrieve the full data model for each resource group. </summary>
-        /// <param name="nameFilter"> The filter used in this operation. </param>
-        /// <param name="top"> The number of results to return. </param>
-        /// <param name="cancellationToken"> A token to allow the caller to cancel the call to the service. The default value is <see cref="P:System.Threading.CancellationToken.None" />. </param>
-        /// <returns> A collection of <see cref="AvailabilitySetChild" /> that may take multiple service requests to iterate over. </returns>
-        public Pageable<AvailabilitySetChild> List(string nameFilter, int? top = null, CancellationToken cancellationToken = default)
-        {
-            var results = ListAsGenericResource(null, top, cancellationToken);
-            return new PhWrappingPageable<GenericResource, AvailabilitySetChild>(results, genericResource => new AvailabilitySetChildOperations(genericResource, genericResource.Id as ResourceGroupResourceIdentifier).Get().Value);
-        }
-
         /// <summary> Filters the list of <see cref="AvailabilitySetChild" /> for this resource group. </summary>
         /// <param name="top"> The number of results to return. </param>
         /// <param name="cancellationToken"> A token to allow the caller to cancel the call to the service. The default value is <see cref="P:System.Threading.CancellationToken.None" />. </param>
         /// <returns> An async collection of <see cref="AvailabilitySetChild" /> that may take multiple service requests to iterate over. </returns>
         public AsyncPageable<AvailabilitySetChild> ListAsync(int? top = null, CancellationToken cancellationToken = default)
-        {
-            var results = ListAsGenericResourceAsync(null, top, cancellationToken);
-            return new PhWrappingAsyncPageable<GenericResource, AvailabilitySetChild>(results, genericResource => new AvailabilitySetChildOperations(genericResource, genericResource.Id as ResourceGroupResourceIdentifier).Get().Value);
-        }
-
-        /// <summary> Filters the list of <see cref="AvailabilitySetChild" /> for this resource group. Makes an additional network call to retrieve the full data model for each resource group. </summary>
-        /// <param name="nameFilter"> The filter used in this operation. </param>
-        /// <param name="top"> The number of results to return. </param>
-        /// <param name="cancellationToken"> A token to allow the caller to cancel the call to the service. The default value is <see cref="P:System.Threading.CancellationToken.None" />. </param>
-        /// <returns> An async collection of <see cref="AvailabilitySetChild" /> that may take multiple service requests to iterate over. </returns>
-        public AsyncPageable<AvailabilitySetChild> ListAsync(string nameFilter, int? top = null, CancellationToken cancellationToken = default)
         {
             var results = ListAsGenericResourceAsync(null, top, cancellationToken);
             return new PhWrappingAsyncPageable<GenericResource, AvailabilitySetChild>(results, genericResource => new AvailabilitySetChildOperations(genericResource, genericResource.Id as ResourceGroupResourceIdentifier).Get().Value);
