f82fecddc8a825b1af44b4d008f661af4b241530
Chris Hamons <chris.hamons@microsoft.com>
2021-06-04 12:28:40 -0500

    Remove usings while i'm here like Alexander did

--- src/AutoRest.CSharp/DataPlane/Output/DataPlaneRestClient.cs
+++ src/AutoRest.CSharp/DataPlane/Output/DataPlaneRestClient.cs
@@ -1,20 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using AutoRest.CSharp.Generation.Types;
 using AutoRest.CSharp.Input;
 using AutoRest.CSharp.Output.Models.Requests;
-using AutoRest.CSharp.Output.Models.Responses;
-using AutoRest.CSharp.Output.Models.Serialization;
-using AutoRest.CSharp.Output.Models.Shared;
 using AutoRest.CSharp.Output.Models.Types;
-using AutoRest.CSharp.Utilities;
-using Azure.Core;
-using Request = AutoRest.CSharp.Output.Models.Requests.Request;
-using StatusCodes = AutoRest.CSharp.Output.Models.Responses.StatusCodes;
 
 namespace AutoRest.CSharp.Output.Models
 {
