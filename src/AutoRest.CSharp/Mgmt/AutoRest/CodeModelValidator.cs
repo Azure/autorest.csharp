4574d70ed1f8c7f0e277360c28d2be52c7204d2e
archerzz <archerzz@gmail.com>
2021-06-21 16:32:26 +0800

    feat(generator): check `operationId` without underscore for resource manager
    1. add validation against `operationId` without underscore when generating resource manager codes
    2. add corresponding unit test, and a new test project for testing generator itself
    3. some corresponding changes to project configuration
    
    resolve https://dev.azure.com/azure-mgmt-ex/DotNET%20Management%20SDK/_workitems/edit/5886

---
+++ src/AutoRest.CSharp/Mgmt/AutoRest/CodeModelValidator.cs
@@ -0,0 +1,22 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using AutoRest.CSharp.Input;
+using AutoRest.CSharp.Utilities;
+
+namespace AutoRest.CSharp.Mgmt.AutoRest
+{
+    internal class CodeModelValidator
+    {
+        public static void Validate(CodeModel model) {
+            foreach (var group in model.OperationGroups)
+            {
+                if (string.IsNullOrEmpty(group.Key)) {
+                    ErrorHelpers.ThrowError("Malformed 'operationId' detected. Check swagger definition and make sure every 'operatoinId' follows form of 'Noun_Verb'." +
+                        " For details, refer to: https://github.com/Azure/azure-rest-api-specs/blob/master/documentation/openapi-authoring-automated-guidelines.md#r1001\n" +
+                        "Or overwrite the 'operationId' by 'transform' directive in configuration. For examples, refer to: https://github.com/Azure/autorest/blob/main/Samples/openapi-v2/3b-custom-transformations/readme.md#openapi-definition-mutate-descriptions");
+                }
+            }
+        }
+    }
+}
---
+++ test/AutoRest.CSharp.Tests/Mgmt/AutoRest/CodeModelValidatorTest.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.IO;
+using AutoRest.CSharp.Input;
+using AutoRest.CSharp.Mgmt.AutoRest;
+using AutoRest.CSharp.Utilities;
+using NUnit.Framework;
+
+namespace AutoRest.CSharp.Tests
+{
+    public class CodeModelValidatorTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void EmptyOperationGroupName()
+        {
+            var model = loadModel("Mgmt/AutoRest/EmptyOperationGroupName.yaml");
+            try
+            {
+                CodeModelValidator.Validate(model);
+            } catch (ErrorHelpers.ErrorException e)
+            {
+                Assert.IsNotNull(e);
+                return;
+            }
+            Assert.Fail("ErrorHelpers.ErrorException expected");
+        }
+
+        private CodeModel loadModel(string filePath)
+        {
+            return CodeModelSerialization.DeserializeCodeModel(File.ReadAllText(filePath));
+        }
+    }
+}
