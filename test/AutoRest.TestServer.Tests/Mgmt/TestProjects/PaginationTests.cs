a0ff66673293313a1335fde170494353851727de
archerzz <archerzz@gmail.com>
2021-06-30 14:36:18 +0800

    update according to review comments
    1. add missing TestProjectTests and OutputLibraryTests for `Pagination` test project
    2. move PageSizeTests to upper level, delete unnecessary `TestProjects` folder

---
+++ test/AutoRest.TestServer.Tests/Mgmt/TestProjects/PaginationTests.cs
@@ -0,0 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace AutoRest.TestServer.Tests.Mgmt.TestProjects
+{
+    internal class PaginationTests : TestProjectTests
+    {
+        public PaginationTests() : base("Pagination") { }
+    }
+}
