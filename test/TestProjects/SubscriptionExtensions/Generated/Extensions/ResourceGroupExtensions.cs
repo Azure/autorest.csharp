abe769e5f52783e7ea3fc1fe6e421c0dfbcb3566
arcturusZhang <ufo54153@gmail.com>
2021-07-07 15:31:02 +0800

    regenerate everything

--- test/TestProjects/SubscriptionExtensions/Generated/Extensions/ResourceGroupExtensions.cs
+++ test/TestProjects/SubscriptionExtensions/Generated/Extensions/ResourceGroupExtensions.cs
@@ -12,7 +12,7 @@ namespace SubscriptionExtensions
     /// <summary> A class to add extension methods to ResourceGroup. </summary>
     public static partial class ResourceGroupExtensions
     {
-        #region Ovens
+        #region Oven
         /// <summary> Gets an object representing a OvenContainer along with the instance operations that can be performed on it. </summary>
         /// <param name="resourceGroup"> The <see cref="ResourceGroupOperations" /> instance the method will execute against. </param>
         /// <returns> Returns a <see cref="OvenContainer" /> object. </returns>
