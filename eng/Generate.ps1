3c132d5eb87d6a830bf295975173aa40bbde6e44
Feng Zhou <fenzhou@microsoft.com>
2021-07-10 21:47:52 +0800

    revert add resources sample

--- eng/Generate.ps1
+++ eng/Generate.ps1
@@ -139,7 +139,6 @@ $projectNames =
     'Azure.AI.FormRecognizer',
     'Azure.Storage.Tables',
     'Azure.ResourceManager.Sample',
-    'Azure.Resources.Sample',
     'Azure.Management.Storage',
     'Azure.Network.Management.Interface',
     'Azure.AI.DocumentTranslation'
