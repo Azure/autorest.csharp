c0b4a3d4a0e178a1be128470c335db461a2a7599
Matt Ellis <matell@microsoft.com>
2021-07-13 12:14:56 -0700

    [LLC] Fix StyleCop error in `LowLevelBinaryDataOperation` (#1400)
    An extra blank line was causing StyleCop errors when we tried to
    update some packages in azure-sdk-for-net. This fixes the stylecop
    error (and cleans up a supression we had in our code base we no longer
    need)

--- src/AutoRest.CSharp/Common/Output/Models/Shared/Parameter.cs
+++ src/AutoRest.CSharp/Common/Output/Models/Shared/Parameter.cs
@@ -1,9 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-#pragma warning disable AD0001 // Doesn't understand record decleration, and the anaylizer throws an exception.
-#pragma warning disable SA1649 // Doesn't understand record decleration, and the anaylizer throws an exception.
-
 using AutoRest.CSharp.Generation.Types;
 
 namespace AutoRest.CSharp.Output.Models.Shared
