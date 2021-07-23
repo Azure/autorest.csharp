34757838cecdf582ec5f9d61d4864335bdc99797
Alexander Sher <alsher@microsoft.com>
2021-07-23 12:23:53 -0500

    Fix #1313: CodeGenSuppress with non-existant types crashes autorest.csharp in LLC mode

---
+++ src/AutoRest.CSharp/Common/Utilities/SyntaxReferenceExtensions.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis;
+
+namespace AutoRest.CSharp.Utilities
+{
+    internal static class SyntaxReferenceExtensions
+    {
+        public static string GetText(this SyntaxReference? syntaxReference)
+            => syntaxReference?.SyntaxTree.GetText().ToString(syntaxReference.Span) ?? string.Empty;
+    }
+}
