93154785264626cbebb6cdc0b2fce59d1a294421
YalinLi0312 <yall@microsoft.com>
2021-07-26 15:35:19 -0700

    Add test file

---
+++ test/AutoRest.TestServer.Tests/Mgmt/TestProjects/OmitOperationGroupsTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace AutoRest.TestServer.Tests.Mgmt.TestProjects
+{
+    public class OmitOperationGroupTests : TestProjectTests
+    {
+        public OmitOperationGroupTests()
+            : base("OmitOperationGroups")
+        {
+        }
+
+        [TestCase(new string[] { "Model1Data", "Model1Update", "Model1ListResult" }, new string[] { "Model2Data", "ModelX", "ModelY", "Model3" })]
+        public void ValidateOperationGroupExistence(string[] notExist, string[] doesExist)
+        {
+            string @namespace = "OmitOperationGroups";
+            for (int i = 0; i < notExist.Length; i++)
+            {
+                Assert.IsNull(Type.GetType(String.Format("{0}.{1}", @namespace, notExist[i])));
+            }
+            for (int i = 0; i < doesExist.Length; i++)
+            {
+                Assert.IsNotNull(Type.GetType(String.Format("{0}.{1}", @namespace, doesExist[i])));
+            }
+        }
+    }
+}
