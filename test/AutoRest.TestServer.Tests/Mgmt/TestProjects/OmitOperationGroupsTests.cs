4032bd8573a4ef1c5da5a080780bb0e9c61c80b1
YalinLi0312 <yall@microsoft.com>
2021-07-27 17:08:15 -0700

    Update test file

--- test/AutoRest.TestServer.Tests/Mgmt/TestProjects/OmitOperationGroupsTests.cs
+++ test/AutoRest.TestServer.Tests/Mgmt/TestProjects/OmitOperationGroupsTests.cs
@@ -13,15 +13,22 @@ namespace AutoRest.TestServer.Tests.Mgmt.TestProjects
         [TestCase(new string[] { "Model1Data", "Model1Update", "Model1ListResult" }, new string[] { "Model2Data", "ModelX", "ModelY", "Model3" })]
         public void ValidateOperationGroupExistence(string[] notExist, string[] doesExist)
         {
-            string @namespace = "OmitOperationGroups";
             for (int i = 0; i < notExist.Length; i++)
             {
-                Assert.IsNull(Type.GetType(String.Format("{0}.{1}", @namespace, notExist[i])));
+                Assert.IsNull(Type.GetType(GetTypeName(notExist[i])));
             }
             for (int i = 0; i < doesExist.Length; i++)
             {
-                Assert.IsNotNull(Type.GetType(String.Format("{0}.{1}", @namespace, doesExist[i])));
+                string name = GetTypeName(doesExist[i]);
+                Assert.IsNotNull(Type.GetType(name));
             }
+        }
+
+        private string GetTypeName(string className)
+        {
+            string @namespace = "OmitOperationGroups";
+
+            return className.EndsWith("Data") ? String.Format("{0}.{1}", @namespace, className) : String.Format("{0}.{1}.{2}", @namespace, "Models", className);
         }
     }
 }
