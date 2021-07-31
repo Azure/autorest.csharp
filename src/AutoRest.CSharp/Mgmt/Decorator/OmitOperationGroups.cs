e13e5ef788dc9eb65febaca174b89be641cb76b5
YalinLi0312 <yall@microsoft.com>
2021-07-30 17:19:19 -0700

    Update all smaple projects

--- src/AutoRest.CSharp/Mgmt/Decorator/OmitOperationGroups.cs
+++ src/AutoRest.CSharp/Mgmt/Decorator/OmitOperationGroups.cs
@@ -16,7 +16,7 @@ namespace AutoRest.CSharp.Mgmt.Decorator
         public static void RemoveOperationGroups(CodeModel codeModel, BuildContext<MgmtOutputLibrary> context)
         {
             var operationGroupsToOmit = context.Configuration.MgmtConfiguration.OperationGroupsToOmit;
-            if (operationGroupsToOmit != null)
+            if (operationGroupsToOmit.Length > 0)
             {
                 var omitSet = operationGroupsToOmit.ToHashSet();
                 codeModel.OperationGroups = codeModel.OperationGroups.Where(og => !omitSet.Contains(og.Key)).ToList();
