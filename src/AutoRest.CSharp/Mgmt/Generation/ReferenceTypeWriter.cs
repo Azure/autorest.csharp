b2da317f782f4690951b6d995f3a2aa0b44853b8
Feng Zhou <fenzhou@microsoft.com>
2021-08-19 18:02:43 +0800

    add required namespace

--- src/AutoRest.CSharp/Mgmt/Generation/ReferenceTypeWriter.cs
+++ src/AutoRest.CSharp/Mgmt/Generation/ReferenceTypeWriter.cs
@@ -24,6 +24,7 @@ namespace AutoRest.CSharp.Mgmt.Generation
             var extensions = schema.ObjectSchema.Extensions;
             if (extensions != null)
             {
+                writer.UseNamespace("Azure.ResourceManager.Core");
                 if (extensions.MgmtReferenceType)
                 {
                     writer.Line($"[{ReferenceClassFinder.ReferenceTypeAttribute}]");
--- test/TestProjects/ReferenceTypes/Generated/Models/SkuReference.cs
+++ test/TestProjects/ReferenceTypes/Generated/Models/SkuReference.cs
@@ -6,6 +6,7 @@
 #nullable disable
 
 using System;
+using Azure.ResourceManager.Core;
 
 namespace Azure.ResourceManager.Resources.Models
 {
