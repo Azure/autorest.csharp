a90f4c48ba6fad6babf346d7dccc0d864c97f547
m-nash <64171366+m-nash@users.noreply.github.com>
2021-08-26 13:48:03 -0700

    bump arm version (#1490)
    * bump arm version and remove dev feed from nuget config
    
    * regen after version bump

--- src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
+++ src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
@@ -38,7 +38,7 @@ namespace AutoRest.CSharp.AutoRest.Plugins
   </PropertyGroup>
 
   <ItemGroup>
-    <PackageReference Include=""Azure.ResourceManager"" Version=""1.0.0-alpha.20210825.3"" />
+    <PackageReference Include=""Azure.ResourceManager"" Version=""1.0.0-beta.1"" />
   </ItemGroup>
 ";
 
