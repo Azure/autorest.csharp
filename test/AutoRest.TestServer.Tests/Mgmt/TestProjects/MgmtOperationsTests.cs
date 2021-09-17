200aa5e21f155eafdd0659c90a89d11f89fcc2a0
ArcturusZhang <dapzhang@microsoft.com>
2021-09-17 18:32:33 +0800

    fix test build failure

--- test/AutoRest.TestServer.Tests/Mgmt/TestProjects/MgmtOperationsTests.cs
+++ test/AutoRest.TestServer.Tests/Mgmt/TestProjects/MgmtOperationsTests.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Threading;
+using MgmtOperations;
 using MgmtOperations.Models;
 using NUnit.Framework;
 
@@ -56,7 +57,7 @@ namespace AutoRest.TestServer.Tests.Mgmt.TestProjects
 
             Assert.AreEqual(3, method.GetParameters().Length);
             var param1 = TypeAsserts.HasParameter(method, "parameters");
-            Assert.AreEqual(typeof(ConnectionSharedKey), param1.ParameterType);
+            Assert.AreEqual(typeof(ConnectionSharedKeyData), param1.ParameterType);
             Assert.False(param1.IsOptional);
             var param2 = TypeAsserts.HasParameter(method, "waitForCompletion");
             Assert.AreEqual(typeof(bool), param2.ParameterType);
