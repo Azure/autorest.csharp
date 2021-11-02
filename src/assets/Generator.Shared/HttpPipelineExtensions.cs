a0fc01d7f0aecab3e54662ecb38acf6dca7ea2e4
Shivangi Reja <shreja@microsoft.com>
2021-11-02 15:21:40 -0700

    Add EXPERIMENTAL conditions for Generator.Shared code

--- src/assets/Generator.Shared/HttpPipelineExtensions.cs
+++ src/assets/Generator.Shared/HttpPipelineExtensions.cs
@@ -12,6 +12,7 @@ namespace Azure.Core
 {
     internal static class HttpPipelineExtensions
     {
+#if EXPERIMENTAL
         public static async ValueTask<Response> ProcessMessageAsync(this HttpPipeline pipeline, HttpMessage message, ClientDiagnostics clientDiagnostics, RequestContext? requestContext, CancellationToken cancellationToken = default)
         {
             var (userCt, statusOption) = ApplyRequestContext(requestContext);
@@ -105,5 +106,6 @@ namespace Azure.Core
 
             return (requestContext.CancellationToken, requestContext.ErrorOptions);
         }
+#endif
     }
 }
