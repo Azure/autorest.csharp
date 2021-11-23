e38ae42ae6140a7dda6c22b3e0fde897e6918eba
m-nash <prognash@gmail.com>
2021-11-23 14:51:43 -0800

    add back setting to modify globally

--- src/assets/Generator.Shared/OperationInternals.cs
+++ src/assets/Generator.Shared/OperationInternals.cs
@@ -21,7 +21,7 @@ namespace Azure.Core
     /// </summary>
     internal class OperationInternals
     {
-        public static TimeSpan DefaultPollingInterval { get; } = TimeSpan.FromSeconds(1);
+        public static TimeSpan DefaultPollingInterval { get; private set; } = TimeSpan.FromSeconds(1);
 
         private readonly OperationInternal _operationInternal;
 
