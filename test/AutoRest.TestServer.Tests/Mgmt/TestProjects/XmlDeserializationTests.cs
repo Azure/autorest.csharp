8cee42380ef738f1b10da206c904f92ff94fdcfb
Yao Kou <yaokou@microsoft.com>
2021-12-20 09:59:11 +0800

    split test

--- test/AutoRest.TestServer.Tests/Mgmt/TestProjects/XmlDeserializationTests.cs
+++ test/AutoRest.TestServer.Tests/Mgmt/TestProjects/XmlDeserializationTests.cs
@@ -16,29 +16,45 @@ namespace AutoRest.TestServer.Tests.Mgmt.TestProjects
             : base("XmlDeserialization") { }
 
         [Test]
-        public void ValidateMgmtXmlDeserialization()
+        public void ValidateXmlDeserialization()
         {
             string id = "/subscriptions/27af6a7d-4b66-4d59-8786-0999a97b32b9/resourceGroups/testRg/providers/Microsoft.XmlDeserialization/xmls/fakeXml";
             string name = "fakeXml";
             string type = "Microsoft.XmlDeserialization/xmls";
-            XElement xElement = new XElement("XmlInstance",
-                new XElement("id", id),
-                new XElement("name", name),
-                new XElement("type", type)
-            );
+            XElement xElement = BuildXElement(id, name, type);
             XmlInstanceData xmlInstanceData = XmlInstanceData.DeserializeXmlInstanceData(xElement);
             Assert.AreEqual(id, xmlInstanceData.Id.ToString());
             Assert.AreEqual(name, xmlInstanceData.Name);
             Assert.AreEqual(type, xmlInstanceData.Type.ToString());
+        }
 
-            string wrongId = "/subscriptions/testSub/resourceGroups/testRg/providers/Microsoft.XmlDeserialization/xmls/fakeXml";
-            xElement.SetElementValue("id", wrongId);
+        [Test]
+        public void ValidateXmlDeserializationWithWrongId()
+        {
+            string id = "/subscriptions/testSub/resourceGroups/testRg/providers/Microsoft.XmlDeserialization/xmls/fakeXml";
+            string name = "fakeXml";
+            string type = "Microsoft.XmlDeserialization/xmls";
+            XElement xElement = BuildXElement(id, name, type);
             Assert.Throws<ArgumentOutOfRangeException>(() => XmlInstanceData.DeserializeXmlInstanceData(xElement));
+        }
 
-            string wrongType = "";
-            xElement.SetElementValue("id", id);
-            xElement.SetElementValue("type", wrongType);
+        [Test]
+        public void ValidateXmlDeserializationWithWrongType()
+        {
+            string id = "/subscriptions/27af6a7d-4b66-4d59-8786-0999a97b32b9/resourceGroups/testRg/providers/Microsoft.XmlDeserialization/xmls/fakeXml";
+            string name = "fakeXml";
+            string type = "";
+            XElement xElement = BuildXElement(id, name, type);
             Assert.Throws<ArgumentException>(() => XmlInstanceData.DeserializeXmlInstanceData(xElement));
+        }
+
+        private static XElement BuildXElement(string id, string name, string type)
+        {
+            return new XElement("XmlInstance",
+                new XElement("id", id),
+                new XElement("name", name),
+                new XElement("type", type)
+            );
         }
     }
 }
