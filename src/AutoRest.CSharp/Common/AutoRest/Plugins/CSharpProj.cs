8450c40c0252fd89d31ba4f6176c202b7d511960
Yao Kou <yaokou@microsoft.com>
2021-12-24 11:29:09 +0800

    update arm core to beta.7

--- src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
+++ src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
@@ -38,7 +38,7 @@ namespace AutoRest.CSharp.AutoRest.Plugins
   </PropertyGroup>
 
   <ItemGroup>
-    <PackageReference Include=""Azure.ResourceManager"" Version=""1.0.0-alpha.20211220.4"" />
+    <PackageReference Include=""Azure.ResourceManager"" Version=""1.0.0-beta.7"" />
   </ItemGroup>
 ";
 
