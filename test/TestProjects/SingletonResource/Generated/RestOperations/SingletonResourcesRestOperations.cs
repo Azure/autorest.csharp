b803f45241abd4c126c707df7d3b2bc4c93a59fc
Dapeng Zhang <dapzhang@microsoft.com>
2022-01-10 18:14:45 +0800

    combined two test projects (#1824)
    * combined two test projects
    
    * update shared assets
    
    * regenerate and fix test cases

--- test/TestProjects/SingletonResource/Generated/RestOperations/SingletonResourcesRestOperations.cs
+++ test/TestProjects/SingletonResource/Generated/RestOperations/SingletonResourcesRestOperations.cs
@@ -14,9 +14,9 @@ using Azure.Core;
 using Azure.Core.Pipeline;
 using Azure.ResourceManager.Core;
 
-namespace MgmtSingleton
+namespace SingletonResource
 {
-    internal partial class SingletonConfigsRestOperations
+    internal partial class SingletonResourcesRestOperations
     {
         private Uri endpoint;
         private string apiVersion;
@@ -24,20 +24,130 @@ namespace MgmtSingleton
         private HttpPipeline _pipeline;
         private readonly string _userAgent;
 
-        /// <summary> Initializes a new instance of SingletonConfigsRestOperations. </summary>
+        /// <summary> Initializes a new instance of SingletonResourcesRestOperations. </summary>
         /// <param name="clientDiagnostics"> The handler for diagnostic messaging in the client. </param>
         /// <param name="pipeline"> The HTTP pipeline for sending and receiving REST requests and responses. </param>
         /// <param name="options"> The client options used to construct the current client. </param>
         /// <param name="endpoint"> server parameter. </param>
         /// <param name="apiVersion"> Api Version. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="apiVersion"/> is null. </exception>
-        public SingletonConfigsRestOperations(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, ClientOptions options, Uri endpoint = null, string apiVersion = "2020-06-01")
+        public SingletonResourcesRestOperations(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, ClientOptions options, Uri endpoint = null, string apiVersion = "2020-06-01")
         {
             this.endpoint = endpoint ?? new Uri("https://management.azure.com");
             this.apiVersion = apiVersion ?? throw new ArgumentNullException(nameof(apiVersion));
             _clientDiagnostics = clientDiagnostics;
             _pipeline = pipeline;
             _userAgent = HttpMessageUtilities.GetUserAgentName(this, options);
+        }
+
+        internal HttpMessage CreateCreateOrUpdateRequest(string subscriptionId, string resourceGroupName, string parentName, SingletonResourceData parameters)
+        {
+            var message = _pipeline.CreateMessage();
+            var request = message.Request;
+            request.Method = RequestMethod.Put;
+            var uri = new RawRequestUriBuilder();
+            uri.Reset(endpoint);
+            uri.AppendPath("/subscriptions/", false);
+            uri.AppendPath(subscriptionId, true);
+            uri.AppendPath("/resourceGroups/", false);
+            uri.AppendPath(resourceGroupName, true);
+            uri.AppendPath("/providers/Microsoft.Billing/parentResources/", false);
+            uri.AppendPath(parentName, true);
+            uri.AppendPath("/singletonResources/", false);
+            uri.AppendPath("current", true);
+            uri.AppendQuery("api-version", apiVersion, true);
+            request.Uri = uri;
+            request.Headers.Add("Accept", "application/json");
+            request.Headers.Add("Content-Type", "application/json");
+            var content = new Utf8JsonRequestContent();
+            content.JsonWriter.WriteObjectValue(parameters);
+            request.Content = content;
+            message.SetProperty("UserAgentOverride", _userAgent);
+            return message;
+        }
+
+        /// <summary> Singleton non-resource test example with a single-value enum name parameter. </summary>
+        /// <param name="subscriptionId"> The String to use. </param>
+        /// <param name="resourceGroupName"> The String to use. </param>
+        /// <param name="parentName"> The String to use. </param>
+        /// <param name="parameters"> The SingletonResource to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="parentName"/>, or <paramref name="parameters"/> is null. </exception>
+        public async Task<Response<SingletonResourceData>> CreateOrUpdateAsync(string subscriptionId, string resourceGroupName, string parentName, SingletonResourceData parameters, CancellationToken cancellationToken = default)
+        {
+            if (subscriptionId == null)
+            {
+                throw new ArgumentNullException(nameof(subscriptionId));
+            }
+            if (resourceGroupName == null)
+            {
+                throw new ArgumentNullException(nameof(resourceGroupName));
+            }
+            if (parentName == null)
+            {
+                throw new ArgumentNullException(nameof(parentName));
+            }
+            if (parameters == null)
+            {
+                throw new ArgumentNullException(nameof(parameters));
+            }
+
+            using var message = CreateCreateOrUpdateRequest(subscriptionId, resourceGroupName, parentName, parameters);
+            await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
+            switch (message.Response.Status)
+            {
+                case 200:
+                    {
+                        SingletonResourceData value = default;
+                        using var document = await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false);
+                        value = SingletonResourceData.DeserializeSingletonResourceData(document.RootElement);
+                        return Response.FromValue(value, message.Response);
+                    }
+                default:
+                    throw await _clientDiagnostics.CreateRequestFailedExceptionAsync(message.Response).ConfigureAwait(false);
+            }
+        }
+
+        /// <summary> Singleton non-resource test example with a single-value enum name parameter. </summary>
+        /// <param name="subscriptionId"> The String to use. </param>
+        /// <param name="resourceGroupName"> The String to use. </param>
+        /// <param name="parentName"> The String to use. </param>
+        /// <param name="parameters"> The SingletonResource to use. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="parentName"/>, or <paramref name="parameters"/> is null. </exception>
+        public Response<SingletonResourceData> CreateOrUpdate(string subscriptionId, string resourceGroupName, string parentName, SingletonResourceData parameters, CancellationToken cancellationToken = default)
+        {
+            if (subscriptionId == null)
+            {
+                throw new ArgumentNullException(nameof(subscriptionId));
+            }
+            if (resourceGroupName == null)
+            {
+                throw new ArgumentNullException(nameof(resourceGroupName));
+            }
+            if (parentName == null)
+            {
+                throw new ArgumentNullException(nameof(parentName));
+            }
+            if (parameters == null)
+            {
+                throw new ArgumentNullException(nameof(parameters));
+            }
+
+            using var message = CreateCreateOrUpdateRequest(subscriptionId, resourceGroupName, parentName, parameters);
+            _pipeline.Send(message, cancellationToken);
+            switch (message.Response.Status)
+            {
+                case 200:
+                    {
+                        SingletonResourceData value = default;
+                        using var document = JsonDocument.Parse(message.Response.ContentStream);
+                        value = SingletonResourceData.DeserializeSingletonResourceData(document.RootElement);
+                        return Response.FromValue(value, message.Response);
+                    }
+                default:
+                    throw _clientDiagnostics.CreateRequestFailedException(message.Response);
+            }
         }
 
         internal HttpMessage CreateGetRequest(string subscriptionId, string resourceGroupName, string parentName)
@@ -53,7 +163,8 @@ namespace MgmtSingleton
             uri.AppendPath(resourceGroupName, true);
             uri.AppendPath("/providers/Microsoft.Billing/parentResources/", false);
             uri.AppendPath(parentName, true);
-            uri.AppendPath("/singletonConfigs/web", false);
+            uri.AppendPath("/singletonResources/", false);
+            uri.AppendPath("current", true);
             uri.AppendQuery("api-version", apiVersion, true);
             request.Uri = uri;
             request.Headers.Add("Accept", "application/json");
@@ -61,13 +172,13 @@ namespace MgmtSingleton
             return message;
         }
 
-        /// <summary> Singleton Test Example. See /providers/Microsoft.Billing/billingAccounts/{billingAccountName}/billingProfiles/{billingProfileName}/policies/default GET,PUT. </summary>
+        /// <summary> Singleton non-resource test example with a single-value enum name parameter. </summary>
         /// <param name="subscriptionId"> The String to use. </param>
         /// <param name="resourceGroupName"> The String to use. </param>
         /// <param name="parentName"> The String to use. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, or <paramref name="parentName"/> is null. </exception>
-        public async Task<Response<SingletonConfigData>> GetAsync(string subscriptionId, string resourceGroupName, string parentName, CancellationToken cancellationToken = default)
+        public async Task<Response<SingletonResourceData>> GetAsync(string subscriptionId, string resourceGroupName, string parentName, CancellationToken cancellationToken = default)
         {
             if (subscriptionId == null)
             {
@@ -88,25 +199,25 @@ namespace MgmtSingleton
             {
                 case 200:
                     {
-                        SingletonConfigData value = default;
+                        SingletonResourceData value = default;
                         using var document = await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false);
-                        value = SingletonConfigData.DeserializeSingletonConfigData(document.RootElement);
+                        value = SingletonResourceData.DeserializeSingletonResourceData(document.RootElement);
                         return Response.FromValue(value, message.Response);
                     }
                 case 404:
-                    return Response.FromValue((SingletonConfigData)null, message.Response);
+                    return Response.FromValue((SingletonResourceData)null, message.Response);
                 default:
                     throw await _clientDiagnostics.CreateRequestFailedExceptionAsync(message.Response).ConfigureAwait(false);
             }
         }
 
-        /// <summary> Singleton Test Example. See /providers/Microsoft.Billing/billingAccounts/{billingAccountName}/billingProfiles/{billingProfileName}/policies/default GET,PUT. </summary>
+        /// <summary> Singleton non-resource test example with a single-value enum name parameter. </summary>
         /// <param name="subscriptionId"> The String to use. </param>
         /// <param name="resourceGroupName"> The String to use. </param>
         /// <param name="parentName"> The String to use. </param>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, or <paramref name="parentName"/> is null. </exception>
-        public Response<SingletonConfigData> Get(string subscriptionId, string resourceGroupName, string parentName, CancellationToken cancellationToken = default)
+        public Response<SingletonResourceData> Get(string subscriptionId, string resourceGroupName, string parentName, CancellationToken cancellationToken = default)
         {
             if (subscriptionId == null)
             {
@@ -127,120 +238,13 @@ namespace MgmtSingleton
             {
                 case 200:
                     {
-                        SingletonConfigData value = default;
+                        SingletonResourceData value = default;
                         using var document = JsonDocument.Parse(message.Response.ContentStream);
-                        value = SingletonConfigData.DeserializeSingletonConfigData(document.RootElement);
+                        value = SingletonResourceData.DeserializeSingletonResourceData(document.RootElement);
                         return Response.FromValue(value, message.Response);
                     }
                 case 404:
-                    return Response.FromValue((SingletonConfigData)null, message.Response);
-                default:
-                    throw _clientDiagnostics.CreateRequestFailedException(message.Response);
-            }
-        }
-
-        internal HttpMessage CreateCreateOrUpdateRequest(string subscriptionId, string resourceGroupName, string parentName, SingletonConfigData parameters)
-        {
-            var message = _pipeline.CreateMessage();
-            var request = message.Request;
-            request.Method = RequestMethod.Put;
-            var uri = new RawRequestUriBuilder();
-            uri.Reset(endpoint);
-            uri.AppendPath("/subscriptions/", false);
-            uri.AppendPath(subscriptionId, true);
-            uri.AppendPath("/resourceGroups/", false);
-            uri.AppendPath(resourceGroupName, true);
-            uri.AppendPath("/providers/Microsoft.Billing/parentResources/", false);
-            uri.AppendPath(parentName, true);
-            uri.AppendPath("/singletonConfigs/web", false);
-            uri.AppendQuery("api-version", apiVersion, true);
-            request.Uri = uri;
-            request.Headers.Add("Accept", "application/json");
-            request.Headers.Add("Content-Type", "application/json");
-            var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(parameters);
-            request.Content = content;
-            message.SetProperty("UserAgentOverride", _userAgent);
-            return message;
-        }
-
-        /// <param name="subscriptionId"> The String to use. </param>
-        /// <param name="resourceGroupName"> The String to use. </param>
-        /// <param name="parentName"> The String to use. </param>
-        /// <param name="parameters"> The SingletonConfig to use. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="parentName"/>, or <paramref name="parameters"/> is null. </exception>
-        public async Task<Response<SingletonConfigData>> CreateOrUpdateAsync(string subscriptionId, string resourceGroupName, string parentName, SingletonConfigData parameters, CancellationToken cancellationToken = default)
-        {
-            if (subscriptionId == null)
-            {
-                throw new ArgumentNullException(nameof(subscriptionId));
-            }
-            if (resourceGroupName == null)
-            {
-                throw new ArgumentNullException(nameof(resourceGroupName));
-            }
-            if (parentName == null)
-            {
-                throw new ArgumentNullException(nameof(parentName));
-            }
-            if (parameters == null)
-            {
-                throw new ArgumentNullException(nameof(parameters));
-            }
-
-            using var message = CreateCreateOrUpdateRequest(subscriptionId, resourceGroupName, parentName, parameters);
-            await _pipeline.SendAsync(message, cancellationToken).ConfigureAwait(false);
-            switch (message.Response.Status)
-            {
-                case 200:
-                    {
-                        SingletonConfigData value = default;
-                        using var document = await JsonDocument.ParseAsync(message.Response.ContentStream, default, cancellationToken).ConfigureAwait(false);
-                        value = SingletonConfigData.DeserializeSingletonConfigData(document.RootElement);
-                        return Response.FromValue(value, message.Response);
-                    }
-                default:
-                    throw await _clientDiagnostics.CreateRequestFailedExceptionAsync(message.Response).ConfigureAwait(false);
-            }
-        }
-
-        /// <param name="subscriptionId"> The String to use. </param>
-        /// <param name="resourceGroupName"> The String to use. </param>
-        /// <param name="parentName"> The String to use. </param>
-        /// <param name="parameters"> The SingletonConfig to use. </param>
-        /// <param name="cancellationToken"> The cancellation token to use. </param>
-        /// <exception cref="ArgumentNullException"> <paramref name="subscriptionId"/>, <paramref name="resourceGroupName"/>, <paramref name="parentName"/>, or <paramref name="parameters"/> is null. </exception>
-        public Response<SingletonConfigData> CreateOrUpdate(string subscriptionId, string resourceGroupName, string parentName, SingletonConfigData parameters, CancellationToken cancellationToken = default)
-        {
-            if (subscriptionId == null)
-            {
-                throw new ArgumentNullException(nameof(subscriptionId));
-            }
-            if (resourceGroupName == null)
-            {
-                throw new ArgumentNullException(nameof(resourceGroupName));
-            }
-            if (parentName == null)
-            {
-                throw new ArgumentNullException(nameof(parentName));
-            }
-            if (parameters == null)
-            {
-                throw new ArgumentNullException(nameof(parameters));
-            }
-
-            using var message = CreateCreateOrUpdateRequest(subscriptionId, resourceGroupName, parentName, parameters);
-            _pipeline.Send(message, cancellationToken);
-            switch (message.Response.Status)
-            {
-                case 200:
-                    {
-                        SingletonConfigData value = default;
-                        using var document = JsonDocument.Parse(message.Response.ContentStream);
-                        value = SingletonConfigData.DeserializeSingletonConfigData(document.RootElement);
-                        return Response.FromValue(value, message.Response);
-                    }
+                    return Response.FromValue((SingletonResourceData)null, message.Response);
                 default:
                     throw _clientDiagnostics.CreateRequestFailedException(message.Response);
             }
