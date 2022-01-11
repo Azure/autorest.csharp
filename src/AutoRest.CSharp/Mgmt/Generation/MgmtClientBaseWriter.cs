f675e916d63b9492fd3fd3184e3505fb8a52c06c
m-nash <prognash@gmail.com>
2022-01-11 10:19:05 -0800

    add culture info to get rid of ca1305

--- src/AutoRest.CSharp/Mgmt/Generation/MgmtClientBaseWriter.cs
+++ src/AutoRest.CSharp/Mgmt/Generation/MgmtClientBaseWriter.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
@@ -93,7 +94,7 @@ namespace AutoRest.CSharp.Mgmt.Generation
             using (writer.Scope($"internal static void ValidateResourceId({typeof(Azure.Core.ResourceIdentifier)} id)"))
             {
                 writer.Line($"if (id.ResourceType != {validResourceType})");
-                writer.Line($"throw new {typeof(ArgumentException)}(string.Format(\"Invalid resource type {{0}} expected {{1}}\", id.ResourceType, {validResourceType}), nameof(id));");
+                writer.Line($"throw new {typeof(ArgumentException)}(string.Format({typeof(CultureInfo)}.CurrentCulture, \"Invalid resource type {{0}} expected {{1}}\", id.ResourceType, {validResourceType}), nameof(id));");
             }
         }
 
