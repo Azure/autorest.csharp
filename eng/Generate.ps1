063e5317259b6ccb6f947f5f97fed6ab62d1848f
Changlong Liu <changlong.liu@microsoft.com>
2022-01-12 09:17:41 +0800

    test-pipeline

--- eng/Generate.ps1
+++ eng/Generate.ps1
@@ -274,10 +274,10 @@ $keys | %{ $swaggerDefinitions[$_] } | ForEach-Object -Parallel {
     Invoke-AutoRest $_.output $_.projectName $_.arguments $using:sharedSource $using:fast;
 } -ThrottleLimit $parallel
 
-$keys | %{ $swaggerTestDefinitions[$_] } | ForEach-Object -Parallel {
-    if ($_.output -ne $null) {
-        Import-Module "$using:PSScriptRoot\Generation.psm1" -DisableNameChecking;
-        Invoke-AutoRest $_.output $_.projectName $_.arguments $using:sharedSource $using:fast;
-    }
-} -ThrottleLimit $parallel
+# $keys | %{ $swaggerTestDefinitions[$_] } | ForEach-Object -Parallel {
+#     if ($_.output -ne $null) {
+#         Import-Module "$using:PSScriptRoot\Generation.psm1" -DisableNameChecking;
+#         Invoke-AutoRest $_.output $_.projectName $_.arguments $using:sharedSource $using:fast;
+#     }
+# } -ThrottleLimit $parallel
 
