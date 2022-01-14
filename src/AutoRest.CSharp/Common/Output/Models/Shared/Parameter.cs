0465329e7f18289d0af27a7685594d8e75fcda4e
m-nash <prognash@gmail.com>
2022-01-14 12:57:26 -0800

    remove usings

--- src/AutoRest.CSharp/Common/Output/Models/Shared/Parameter.cs
+++ src/AutoRest.CSharp/Common/Output/Models/Shared/Parameter.cs
@@ -2,10 +2,7 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.ComponentModel;
 using AutoRest.CSharp.Generation.Types;
-using AutoRest.CSharp.Output.Models.Requests;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace AutoRest.CSharp.Output.Models.Shared
 {
