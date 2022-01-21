59692a5e18e2fa81bc3c84393177f1f2e13e5a85
m-nash <prognash@gmail.com>
2022-01-21 15:30:19 -0800

    regen after updates

--- test/TestProjects/MgmtExtensionResource/Generated/Extensions/SubscriptionExtensions.cs
+++ test/TestProjects/MgmtExtensionResource/Generated/Extensions/SubscriptionExtensions.cs
@@ -5,7 +5,11 @@
 
 #nullable disable
 
+using System.Threading;
+using System.Threading.Tasks;
+using Azure;
 using Azure.ResourceManager.Resources;
+using MgmtExtensionResource.Models;
 
 namespace MgmtExtensionResource
 {
@@ -29,6 +33,30 @@ namespace MgmtExtensionResource
                 return new SubscriptionExtensionClient(armClient, subscription.Id);
             }
             );
+        }
+
+        /// RequestPath: /subscriptions/{subscriptionId}/providers/Microsoft.OrphanedPost/validateSomething
+        /// ContextualPath: /subscriptions/{subscriptionId}
+        /// OperationId: OrphanedPost_ValidateSomething
+        /// <param name="subscription"> The <see cref="Subscription" /> instance the method will execute against. </param>
+        /// <param name="validateSomethingOptions"> Information to validate. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="System.ArgumentNullException"> <paramref name="validateSomethingOptions"/> is null. </exception>
+        public static async Task<Response> ValidateSomethingOrphanedPostAsync(this Subscription subscription, ValidateSomethingOptions validateSomethingOptions, CancellationToken cancellationToken = default)
+        {
+            return await GetExtensionClient(subscription).ValidateSomethingOrphanedPostAsync(validateSomethingOptions, cancellationToken).ConfigureAwait(false);
+        }
+
+        /// RequestPath: /subscriptions/{subscriptionId}/providers/Microsoft.OrphanedPost/validateSomething
+        /// ContextualPath: /subscriptions/{subscriptionId}
+        /// OperationId: OrphanedPost_ValidateSomething
+        /// <param name="subscription"> The <see cref="Subscription" /> instance the method will execute against. </param>
+        /// <param name="validateSomethingOptions"> Information to validate. </param>
+        /// <param name="cancellationToken"> The cancellation token to use. </param>
+        /// <exception cref="System.ArgumentNullException"> <paramref name="validateSomethingOptions"/> is null. </exception>
+        public static Response ValidateSomethingOrphanedPost(this Subscription subscription, ValidateSomethingOptions validateSomethingOptions, CancellationToken cancellationToken = default)
+        {
+            return GetExtensionClient(subscription).ValidateSomethingOrphanedPost(validateSomethingOptions, cancellationToken);
         }
     }
 }
