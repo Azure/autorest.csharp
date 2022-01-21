f8d8032530c507e79cfcb8b10a3bec96f712dfce
pshao25 <97225342+pshao25@users.noreply.github.com>
2022-01-21 15:33:32 +0800

    Add Uri public property

--- src/AutoRest.CSharp/LowLevel/Generation/LowLevelClientWriter.cs
+++ src/AutoRest.CSharp/LowLevel/Generation/LowLevelClientWriter.cs
@@ -101,6 +101,15 @@ namespace AutoRest.CSharp.Generation.Writers
                 .WriteXmlDocumentationSummary($"The HTTP pipeline for sending and receiving REST requests and responses.")
                 .Line($"public virtual {typeof(HttpPipeline)} Pipeline => {client.Fields.PipelineField.Name};");
 
+            var uriField = client.Fields.GetFieldByParameter("endpoint", new CSharpType(typeof(Uri)));
+            if (uriField != null)
+            {
+                writer
+                    .Line()
+                    .WriteXmlDocumentationSummary($"The HTTP Uri.")
+                    .Line($"public virtual {typeof(Uri)} Uri => {uriField.Declaration.ActualName};");
+            }
+
             writer.Line();
         }
 
