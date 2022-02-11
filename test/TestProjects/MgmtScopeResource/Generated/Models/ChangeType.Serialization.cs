9ee906a72e9dab820e0a9393583ed37b46f2812f
Arcturus Zhang <dapzhang@microsoft.com>
2022-02-11 15:51:18 +0800

    full regenerate

--- test/TestProjects/MgmtScopeResource/Generated/Models/ChangeType.Serialization.cs
+++ test/TestProjects/MgmtScopeResource/Generated/Models/ChangeType.Serialization.cs
@@ -13,11 +13,11 @@ namespace MgmtScopeResource.Models
     {
         public static string ToSerialString(this ChangeType value) => value switch
         {
+            ChangeType.NoChange => "NoChange",
             ChangeType.Create => "Create",
             ChangeType.Delete => "Delete",
             ChangeType.Ignore => "Ignore",
             ChangeType.Deploy => "Deploy",
-            ChangeType.NoChange => "NoChange",
             ChangeType.Modify => "Modify",
             ChangeType.Unsupported => "Unsupported",
             _ => throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown ChangeType value.")
@@ -25,11 +25,11 @@ namespace MgmtScopeResource.Models
 
         public static ChangeType ToChangeType(this string value)
         {
+            if (string.Equals(value, "NoChange", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.NoChange;
             if (string.Equals(value, "Create", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.Create;
             if (string.Equals(value, "Delete", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.Delete;
             if (string.Equals(value, "Ignore", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.Ignore;
             if (string.Equals(value, "Deploy", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.Deploy;
-            if (string.Equals(value, "NoChange", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.NoChange;
             if (string.Equals(value, "Modify", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.Modify;
             if (string.Equals(value, "Unsupported", StringComparison.InvariantCultureIgnoreCase)) return ChangeType.Unsupported;
             throw new ArgumentOutOfRangeException(nameof(value), value, "Unknown ChangeType value.");
--- test/TestProjects/MgmtScopeResource/Generated/Models/ResourceIdentityType.cs
+++ test/TestProjects/MgmtScopeResource/Generated/Models/ResourceIdentityType.cs
@@ -10,9 +10,9 @@ namespace MgmtScopeResource.Models
     /// <summary> The identity type. This is the only required field when adding a system assigned identity to a resource. </summary>
     public enum ResourceIdentityType
     {
-        /// <summary> Indicates that a system assigned identity is associated with the resource. </summary>
-        SystemAssigned,
         /// <summary> Indicates that no identity is associated with the resource or that the existing identity should be removed. </summary>
-        None
+        None,
+        /// <summary> Indicates that a system assigned identity is associated with the resource. </summary>
+        SystemAssigned
     }
 }
