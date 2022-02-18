741e7509b9bd9ede13e44fc328c11ffe1b5a08ec
Minghao Chen <30464227+HarveyLink@users.noreply.github.com>
2022-02-18 17:23:47 +0800

    Update fix-1

--- src/AutoRest.CSharp/Mgmt/AutoRest/MgmtTarget.cs
+++ src/AutoRest.CSharp/Mgmt/AutoRest/MgmtTarget.cs
@@ -147,10 +147,11 @@ namespace AutoRest.CSharp.AutoRest.Plugins
 
             if (!MgmtContext.Library.ArmResourceExtensions.IsEmpty)
             {
-                var armResourceExt = MgmtContext.Library.ArmResourceExtensions;
-                var armResourceExtensionsCodeWriter = new ArmResourceExtensionsWriter(armResourceExt);
-                armResourceExtensionsCodeWriter.Write();
-                AddGeneratedFile(project, $"Extensions/{armResourceExtensionsCodeWriter.FileName}.cs", armResourceExtensionsCodeWriter.ToString());
+                WriteExtensionPair(project, MgmtContext.Library.ArmResourceExtensionsClient);
+                //var armResourceExt = MgmtContext.Library.ArmResourceExtensions;
+                //var armResourceExtensionsCodeWriter = new ArmResourceExtensionsWriter(armResourceExt);
+                //armResourceExtensionsCodeWriter.Write();
+                //AddGeneratedFile(project, $"Extensions/{armResourceExtensionsCodeWriter.FileName}.cs", armResourceExtensionsCodeWriter.ToString());
             }
 
             var lroWriter = new MgmtLongRunningOperationWriter(true);
--- test/TestProjects/MgmtScopeResource/Generated/Extensions/ArmResourceExtensions.cs
+++ test/TestProjects/MgmtScopeResource/Generated/Extensions/ArmResourceExtensions.cs
@@ -12,16 +12,21 @@ namespace MgmtScopeResource
     /// <summary> A class to add extension methods to ArmResource. </summary>
     public static partial class ArmResourceExtensions
     {
+        private static ArmResourceExtensionClient GetExtensionClient(ArmResource armResource)
+        {
+            return armResource.GetCachedClient((client) =>
+            {
+                return new ArmResourceExtensionClient(client, armResource.Id);
+            }
+            );
+        }
+
         /// <summary> Gets a collection of PolicyAssignments in the PolicyAssignment. </summary>
         /// <param name="armResource"> The <see cref="ArmResource" /> instance the method will execute against. </param>
         /// <returns> An object representing collection of PolicyAssignments and their operations over a PolicyAssignment. </returns>
         public static PolicyAssignmentCollection GetPolicyAssignments(this ArmResource armResource)
         {
-            return armResource.GetCachedClient((client) =>
-            {
-                return new PolicyAssignmentCollection(client, armResource.Id);
-            }
-            );
+            return GetExtensionClient(armResource).GetPolicyAssignments();
         }
     }
 }
