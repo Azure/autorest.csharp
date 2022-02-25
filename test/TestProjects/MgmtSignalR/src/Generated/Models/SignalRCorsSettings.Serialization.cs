8a3c5b559aaf3143c5183beee6024e370fa62372
Changlong Liu <changlong.liu@microsoft.com>
2022-02-25 14:23:42 +0800

    generate-after-rebase

--- test/TestProjects/MgmtSignalR/src/Generated/Models/SignalRCorsSettings.Serialization.cs
+++ test/TestProjects/MgmtSignalR/src/Generated/Models/SignalRCorsSettings.Serialization.cs
@@ -11,7 +11,7 @@ using Azure.Core;
 
 namespace MgmtSignalR.Models
 {
-    public partial class SignalRCorsSettings : IUtf8JsonSerializable
+    internal partial class SignalRCorsSettings : IUtf8JsonSerializable
     {
         void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
         {
