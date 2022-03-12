36d610c053f4672d43aa3f7fd6fc8ccae1e93dfc
Feng Zhou <fenzhou@microsoft.com>
2022-03-12 11:56:02 +0800

    update resourcemanager version

--- src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
+++ src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
@@ -38,7 +38,7 @@ namespace AutoRest.CSharp.AutoRest.Plugins
   </PropertyGroup>
 
   <ItemGroup>
-    <PackageReference Include=""Azure.ResourceManager"" Version=""1.0.0-alpha.20220309.5"" />
+    <PackageReference Include=""Azure.ResourceManager"" Version=""1.0.0-alpha.20220311.1"" />
   </ItemGroup>
 ";
 
