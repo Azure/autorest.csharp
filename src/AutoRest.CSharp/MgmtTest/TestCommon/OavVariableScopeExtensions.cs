c5093a6fe12ea1a4ad4cbcd793f89b671d72c666
Changlong Liu <changlong.liu@microsoft.com>
2022-03-22 18:33:17 +0800

    refactor

---
+++ src/AutoRest.CSharp/MgmtTest/TestCommon/OavVariableScopeExtensions.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using AutoRest.CSharp.Input;
+using AutoRest.CSharp.MgmtTest.Models;
+
+namespace AutoRest.CSharp.MgmtTest.TestCommon
+{
+    internal static class OavVariableScopeExtensions
+    {
+        public static string GetVariableDefaultValue(this OavVariableScope ovs, string variableKey, string defaultValue = "default")
+        {
+            if (ovs.Variables is not null)
+            {
+                if (ovs.Variables.ContainsKey(variableKey))
+                {
+                    var rawValue = new JsonRawValue(ovs.Variables[variableKey]);
+                    if (rawValue.IsString())
+                    {
+                        return rawValue.AsString()!;
+                    }
+                    else if (rawValue.IsDictionary())
+                    {
+                        var dict = rawValue.AsDictionary();
+                        if (dict.ContainsKey("defaultValue"))
+                        {
+                            return dict["defaultValue"]?.ToString() ?? defaultValue;
+                        }
+                    }
+                }
+            }
+            return defaultValue;
+        }
+    }
+}
---
+++ src/AutoRest.CSharp/MgmtTest/TestCommon/OperationExtensions.cs
@@ -0,0 +1,81 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Diagnostics.CodeAnalysis;
+using AutoRest.CSharp.Input;
+using AutoRest.CSharp.Mgmt.Output;
+using AutoRest.CSharp.Mgmt.AutoRest;
+using AutoRest.CSharp.Mgmt.Models;
+
+namespace AutoRest.CSharp.MgmtTest.TestCommon
+{
+    internal static class OperationExtensions
+    {
+        public static bool IsCollectionOperation(this Operation operation, [MaybeNullWhen(false)] out ResourceCollection collection, [MaybeNullWhen(false)] out MgmtClientOperation clientOperation, [MaybeNullWhen(false)] out MgmtRestOperation restOperation)
+        {
+            collection = null;
+            clientOperation = null;
+            restOperation = null;
+            foreach (var c in MgmtContext.Library.ResourceCollections)
+            {
+                foreach (var co in c.AllOperations)
+                {
+                    foreach (var ro in co)
+                    {
+                        if (ro.Operation == operation)
+                        {
+                            collection = c;
+                            clientOperation = co;
+                            restOperation = ro;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool IsResourceOperation(this Operation operation, [MaybeNullWhen(false)] out Resource resource, [MaybeNullWhen(false)] out MgmtClientOperation clientOperation, [MaybeNullWhen(false)] out MgmtRestOperation restOperation)
+        {
+            resource = null;
+            clientOperation = null;
+            restOperation = null;
+            foreach (var r in MgmtContext.Library.ArmResources)
+            {
+                foreach (var co in r.AllOperations)
+                {
+                    foreach (var ro in co)
+                    {
+                        if (ro.Operation == operation)
+                        {
+                            resource = r;
+                            clientOperation = co;
+                            restOperation = ro;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        public static bool IsSubscriptionOperation(this Operation operation, [MaybeNullWhen(false)] out MgmtClientOperation clientOperation, [MaybeNullWhen(false)] out MgmtRestOperation restOperation)
+        {
+            clientOperation = null;
+            restOperation = null;
+            foreach (var co in MgmtContext.Library.SubscriptionExtensions.ClientOperations)
+            {
+                foreach (var ro in co)
+                {
+                    if (ro.Operation == operation)
+                    {
+                        clientOperation = co;
+                        restOperation = ro;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
