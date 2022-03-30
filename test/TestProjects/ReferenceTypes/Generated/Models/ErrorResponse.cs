74a5c8d85164a9ac496147d72ab8ac219e544b51
m-nash <prognash@gmail.com>
2022-03-30 14:15:41 -0700

    regen after changes

--- test/TestProjects/ReferenceTypes/Generated/Models/ErrorResponse.cs
+++ test/TestProjects/ReferenceTypes/Generated/Models/ErrorResponse.cs
@@ -5,6 +5,7 @@
 
 #nullable disable
 
+using Azure;
 using Azure.ResourceManager;
 
 namespace Azure.ResourceManager.Fake.Models
@@ -22,12 +23,12 @@ namespace Azure.ResourceManager.Fake.Models
         /// <summary> Initializes a new instance of ErrorResponse. </summary>
         /// <param name="error"> The error object. </param>
         [SerializationConstructor]
-        internal ErrorResponse(ErrorDetail error)
+        internal ErrorResponse(ResponseError error)
         {
             Error = error;
         }
 
         /// <summary> The error object. </summary>
-        public ErrorDetail Error { get; set; }
+        public ResponseError Error { get; set; }
     }
 }
