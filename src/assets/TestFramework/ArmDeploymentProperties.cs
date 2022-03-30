0e2d81da2120f8ab8bbfc2945b2e43af9a919a47
Changlong Liu <changlong.liu@microsoft.com>
2022-03-30 17:17:14 +0800

    armdeployment

--- src/assets/TestFramework/ArmDeploymentProperties.cs
+++ src/assets/TestFramework/ArmDeploymentProperties.cs
@@ -5,20 +5,20 @@
 
 #nullable disable
 
-namespace Resources.Models
+namespace Azure.ResourceManager.Resources.Models
 {
-    /// <summary> Deployment properties. </summary>
-    public partial class DeploymentProperties
+    /// <summary> ArmDeployment properties. </summary>
+    public partial class ArmDeploymentProperties
     {
-        /// <summary> Initializes a new instance of DeploymentProperties. </summary>
+        /// <summary> Initializes a new instance of ArmDeploymentProperties. </summary>
         /// <param name="mode"> The mode that is used to deploy resources. This value can be either Incremental or Complete. In Incremental mode, resources are deployed without deleting existing resources that are not included in the template. In Complete mode, resources are deployed and existing resources in the resource group that are not included in the template are deleted. Be careful when using Complete mode as you may unintentionally delete resources. </param>
-        public DeploymentProperties(DeploymentMode mode)
+        public ArmDeploymentProperties(ArmDeploymentMode mode)
         {
             Mode = mode;
         }
 
-        public DeploymentMode Mode { get; }
+        public ArmDeploymentMode Mode { get; }
 
-        public object Template { get; set; }
+        public System.BinaryData Template { get; set; }
     }
 }
