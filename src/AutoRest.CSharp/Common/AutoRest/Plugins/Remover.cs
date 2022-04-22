84a6ac9ae5e93bfdc6a7135e663b6907577051e4
ArcturusZhang <dapzhang@microsoft.com>
2022-04-22 15:59:02 +0800

    now we can have all the references of a declaration

--- src/AutoRest.CSharp/Common/AutoRest/Plugins/Remover.cs
+++ src/AutoRest.CSharp/Common/AutoRest/Plugins/Remover.cs
@@ -2,8 +2,11 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FindSymbols;
 
 namespace AutoRest.CSharp.Common.AutoRest.Plugins
 {
@@ -11,10 +14,14 @@ namespace AutoRest.CSharp.Common.AutoRest.Plugins
     {
         public static async Task<Project> RemoveUnusedAsync(Project project)
         {
+            var compilation = await project.GetCompilationAsync();
+            if (compilation == null)
+                return project;
+
             // find all the declarations, including non-public declared
             var definitions = await Internalizer.GetModels(project, false);
             // build reference map
-            var referenceMap = BuildReferenceMap(project);
+            var referenceMap = await BuildReferenceMap(compilation, project, definitions);
             // traverse the map to determine the declarations that we are about to remove
             // TODO
             // remove those declarations one by one
@@ -23,14 +30,29 @@ namespace AutoRest.CSharp.Common.AutoRest.Plugins
             return project;
         }
 
-        private static ReferenceMap BuildReferenceMap(Project project)
+        private static async Task<Dictionary<BaseTypeDeclarationSyntax, List<Document>>> BuildReferenceMap(Compilation compilation, Project project, IEnumerable<BaseTypeDeclarationSyntax> definitions)
         {
-            return new ReferenceMap();
-        }
+            var references = new Dictionary<BaseTypeDeclarationSyntax, List<Document>>();
+            foreach (var definition in definitions)
+            {
+                var semanticModel = compilation.GetSemanticModel(definition.SyntaxTree);
+                var symbol = semanticModel.GetDeclaredSymbol(definition);
+                if (symbol == null)
+                    continue;
 
-        private class ReferenceMap
-        {
-            // TODO -- design a data structure for this
+                foreach (var reference in await SymbolFinder.FindReferencesAsync(symbol, project.Solution))
+                {
+                    foreach (var location in reference.Locations)
+                    {
+                        if (!references.ContainsKey(definition))
+                            references.Add(definition, new List<Document>());
+                        references[definition].Add(location.Document);
+                    }
+                }
+            }
+
+            // TODO -- we need to reverse this dictionary to make things easier to travese
+            return references;
         }
     }
 }
