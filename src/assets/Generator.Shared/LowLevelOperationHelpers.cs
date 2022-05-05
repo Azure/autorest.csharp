5f88a2c5240fd9e8cccd292509c04070db7c83a7
Alexander Sher <alsher@microsoft.com>
2022-05-05 15:41:06 -0500

    Add LRO Grow-Up

--- src/assets/Generator.Shared/LowLevelOperationHelpers.cs
+++ src/assets/Generator.Shared/LowLevelOperationHelpers.cs
@@ -13,6 +13,11 @@ namespace Azure.Core
 {
     internal static class LowLevelOperationHelpers
     {
+        public static Operation<TTo> Convert<TFrom, TTo>(Operation<TFrom> operation, ClientDiagnostics diagnostics, string scopeName, Func<Response, TTo> convertFunc)
+            where TFrom : notnull
+            where TTo : notnull
+            => new ConvertOperation<TFrom, TTo>(operation, diagnostics, scopeName, convertFunc);
+
         public static ValueTask<Operation<BinaryData>> ProcessMessageAsync(HttpPipeline pipeline, HttpMessage message, ClientDiagnostics clientDiagnostics, string scopeName, OperationFinalStateVia finalStateVia, RequestContext? requestContext, WaitUntil waitUntil)
             => ProcessMessageAsync(pipeline, message, clientDiagnostics, scopeName, finalStateVia, requestContext, waitUntil, r => r.Content);
 
@@ -61,6 +66,173 @@ namespace Azure.Core
                 operation.WaitForCompletion(requestContext?.CancellationToken ?? default);
             }
             return operation;
+        }
+
+        private class ConvertOperation<TFrom, TTo> : Operation<TTo>
+            where TFrom : notnull
+            where TTo : notnull
+        {
+            private readonly Operation<TFrom> _operation;
+            private readonly ClientDiagnostics _diagnostics;
+            private readonly string _operationName;
+            private readonly string _updateStatusScopeName;
+            private readonly Func<Response, TTo> _convertFunc;
+            private Response<TTo>? _response;
+
+            public ConvertOperation(Operation<TFrom> operation, ClientDiagnostics diagnostics, string operationName, Func<Response, TTo> convertFunc)
+            {
+                _operation = operation;
+                _diagnostics = diagnostics;
+                _operationName = operationName;
+                _updateStatusScopeName = $"{operationName}.UpdateStatus";
+                _convertFunc = convertFunc;
+                _response = null;
+            }
+
+            public override string Id => _operation.Id;
+            public override TTo Value => GetOrCreateValue();
+            public override bool HasValue => _operation.HasValue;
+            public override bool HasCompleted => _operation.HasCompleted;
+            public override Response GetRawResponse() => _operation.GetRawResponse();
+
+            public override Response UpdateStatus(CancellationToken cancellationToken = default)
+            {
+                if (HasCompleted)
+                {
+                    return GetRawResponse();
+                }
+
+                using var scope = CreateScope(_updateStatusScopeName);
+                try
+                {
+                    return _operation.UpdateStatus(cancellationToken);
+                }
+                catch (Exception e)
+                {
+                    scope.Failed(e);
+                    throw;
+                }
+            }
+
+            public override async ValueTask<Response> UpdateStatusAsync(CancellationToken cancellationToken = default)
+            {
+                if (HasCompleted)
+                {
+                    return GetRawResponse();
+                }
+
+                using var scope = CreateScope(_updateStatusScopeName);
+                try
+                {
+                    return await _operation.UpdateStatusAsync(cancellationToken);
+                }
+                catch (Exception e)
+                {
+                    scope.Failed(e);
+                    throw;
+                }
+            }
+
+            public override Response<TTo> WaitForCompletion(CancellationToken cancellationToken = default)
+            {
+                if (_response != null)
+                {
+                    return _response;
+                }
+
+                using var scope = CreateScope(_operationName);
+                try
+                {
+                    var result = _operation.WaitForCompletion(cancellationToken);
+                    return CreateResponseOfTTo(result);
+                }
+                catch (Exception e)
+                {
+                    scope.Failed(e);
+                    throw;
+                }
+            }
+
+            public override Response<TTo> WaitForCompletion(TimeSpan pollingInterval, CancellationToken cancellationToken)
+            {
+                if (_response != null)
+                {
+                    return _response;
+                }
+
+                using var scope = CreateScope(_operationName);
+                try
+                {
+                    var result = _operation.WaitForCompletion(pollingInterval, cancellationToken);
+                    return CreateResponseOfTTo(result);
+                }
+                catch (Exception e)
+                {
+                    scope.Failed(e);
+                    throw;
+                }
+            }
+
+            public override async ValueTask<Response<TTo>> WaitForCompletionAsync(CancellationToken cancellationToken = default)
+            {
+                if (_response != null)
+                {
+                    return _response;
+                }
+
+                using var scope = CreateScope(_operationName);
+                try
+                {
+                    var result = await _operation.WaitForCompletionAsync(cancellationToken);
+                    return CreateResponseOfTTo(result);
+                }
+                catch (Exception e)
+                {
+                    scope.Failed(e);
+                    throw;
+                }
+            }
+
+            public override async ValueTask<Response<TTo>> WaitForCompletionAsync(TimeSpan pollingInterval, CancellationToken cancellationToken)
+            {
+                if (_response != null)
+                {
+                    return _response;
+                }
+
+                using var scope = CreateScope(_operationName);
+                try
+                {
+                    var result = await _operation.WaitForCompletionAsync(pollingInterval, cancellationToken);
+                    return CreateResponseOfTTo(result);
+                }
+                catch (Exception e)
+                {
+                    scope.Failed(e);
+                    throw;
+                }
+            }
+
+            private TTo GetOrCreateValue()
+                => _response != null ? _response.Value : CreateResponseOfTTo(GetRawResponse()).Value;
+
+            private Response<TTo> CreateResponseOfTTo(Response<TFrom> responseTFrom)
+                => CreateResponseOfTTo(responseTFrom.GetRawResponse());
+
+            private Response<TTo> CreateResponseOfTTo(Response rawResponse)
+            {
+                var value = _convertFunc(rawResponse);
+                var response = Response.FromValue(value, rawResponse);
+                Interlocked.CompareExchange(ref _response, response, null);
+                return _response;
+            }
+
+            private DiagnosticScope CreateScope(string name)
+            {
+                var scope = _diagnostics.CreateScope(name);
+                scope.Start();
+                return scope;
+            }
         }
     }
 }
--- test/AutoRest.TestServerLowLevel.Tests/dpg-customization.cs
+++ test/AutoRest.TestServerLowLevel.Tests/dpg-customization.cs
@@ -30,7 +30,7 @@ namespace AutoRest.TestServer.Tests
 
             Response<Product> result = await new DPGClient(Key, host).GetModelValueAsync("model");
             Assert.AreEqual(1, scopes.Count);
-            Assert.AreEqual("DPGClient.GetModelValue", scopes[0].Name);
+            Assert.AreEqual("DPGClient.GetModel", scopes[0].Name);
             Assert.True(scopes[0].IsCompleted);
             Assert.AreEqual("model", $"{result.Value.Received}");
         });
@@ -72,6 +72,61 @@ namespace AutoRest.TestServer.Tests
                 var firstItem = JsonData.FromBytes(page.Values.First());
                 Assert.AreEqual("raw", (string)firstItem["received"]);
             }
+        });
+
+        [Test]
+        public Task HandwrittenModelLro() => Test(async (host) =>
+        {
+            using var diagnosticListener = new ClientDiagnosticListener("dpg_customization_LowLevel", asyncLocal: true);
+            CollectionAssert.IsEmpty(diagnosticListener.Scopes);
+
+            Operation<Product> lro = await new DPGClient(Key, host).LroValueAsync(WaitUntil.Started, "model");
+            Assert.AreEqual(1, diagnosticListener.Scopes.Count);
+            Assert.AreEqual("DPGClient.LroValue", diagnosticListener.Scopes[0].Name);
+            Assert.IsTrue(diagnosticListener.Scopes[0].IsCompleted);
+
+            await lro.WaitForCompletionAsync();
+            Assert.AreEqual(2, diagnosticListener.Scopes.Count);
+            Assert.AreEqual("model", $"{lro.Value.Received}");
+            Assert.AreEqual("DPGClient.LroValue", diagnosticListener.Scopes[1].Name);
+            Assert.IsTrue(diagnosticListener.Scopes[1].IsCompleted);
+        });
+
+        [Test]
+        public Task HandwrittenModelLro_ManualIteration() => Test(async (host) =>
+        {
+            using var diagnosticListener = new ClientDiagnosticListener("dpg_customization_LowLevel", asyncLocal: true);
+            CollectionAssert.IsEmpty(diagnosticListener.Scopes);
+
+            Operation<Product> lro = await new DPGClient(Key, host).LroValueAsync(WaitUntil.Started, "model");
+            Assert.AreEqual(1, diagnosticListener.Scopes.Count);
+            Assert.AreEqual("DPGClient.LroValue", diagnosticListener.Scopes[0].Name);
+            Assert.IsTrue(diagnosticListener.Scopes[0].IsCompleted);
+
+            var updatesCount = 0;
+            while (!lro.HasCompleted)
+            {
+                await lro.UpdateStatusAsync();
+                updatesCount++;
+                Assert.AreEqual("DPGClient.LroValue.UpdateStatus", diagnosticListener.Scopes[updatesCount].Name);
+                Assert.IsTrue(diagnosticListener.Scopes[updatesCount].IsCompleted);
+            }
+
+            // +1 due to the first scope being created by LroValueAsync
+            Assert.AreEqual(updatesCount + 1, diagnosticListener.Scopes.Count);
+        });
+
+        [Test]
+        public Task HandwrittenModelLro_WaitUntilCompleted() => Test(async (host) =>
+        {
+            using var diagnosticListener = new ClientDiagnosticListener("dpg_customization_LowLevel", asyncLocal: true);
+            CollectionAssert.IsEmpty(diagnosticListener.Scopes);
+
+            Operation<Product> lro = await new DPGClient(Key, host).LroValueAsync(WaitUntil.Completed, "model");
+            Assert.AreEqual(1, diagnosticListener.Scopes.Count);
+            Assert.AreEqual("DPGClient.LroValue", diagnosticListener.Scopes[0].Name);
+            Assert.IsTrue(diagnosticListener.Scopes[0].IsCompleted);
+            Assert.AreEqual("model", $"{lro.Value.Received}");
         });
 
         [Test]
