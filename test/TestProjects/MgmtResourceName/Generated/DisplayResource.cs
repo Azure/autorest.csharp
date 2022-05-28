063954298d7653038c0cd6d7a6aadcc9ad8fdf3a
ArcturusZhang <dapzhang@microsoft.com>
2022-05-28 18:38:23 +0800

    regenerate

--- test/TestProjects/MgmtResourceName/Generated/DisplayResource.cs
+++ test/TestProjects/MgmtResourceName/Generated/DisplayResource.cs
@@ -89,7 +89,7 @@ namespace MgmtResourceName
 
         /// <summary>
         /// Request Path: /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/displayResources/{displayResourceName}
-        /// Operation Id: DisplayResources_Get
+        /// Operation Id: displayResources_Get
         /// </summary>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         public virtual async Task<Response<DisplayResource>> GetAsync(CancellationToken cancellationToken = default)
@@ -112,7 +112,7 @@ namespace MgmtResourceName
 
         /// <summary>
         /// Request Path: /subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}/providers/Microsoft.Compute/displayResources/{displayResourceName}
-        /// Operation Id: DisplayResources_Get
+        /// Operation Id: displayResources_Get
         /// </summary>
         /// <param name="cancellationToken"> The cancellation token to use. </param>
         public virtual Response<DisplayResource> Get(CancellationToken cancellationToken = default)
