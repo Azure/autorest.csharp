d77a04c5767aedb7962f39fa49a1d81275f174b6
Alexander Sher <alsher@microsoft.com>
2022-06-20 20:17:15 -0500

    Fix working directory

--- eng/GetOrCreateAzureSdkForNetBranch.ps1
+++ eng/GetOrCreateAzureSdkForNetBranch.ps1
@@ -2,21 +2,26 @@ param($Version, $PullRequestNumber, $SdkRepoRoot)
 
 $RemoteName = "origin"
 $BranchName = "autorest/pr$PullRequestNumber"
-Write-Host "Branch name: $BranchName"
+$SdkRepoRoot = Resolve-Path $SdkRepoRoot
+$RepoRoot = Resolve-Path "$PSScriptRoot/.."
+
+Write-Host "version: $BranchName"
+Write-Host "pull request number: $PullRequestNumber"
+Write-Host "autorest repo root: $RepoRoot"
+Write-Host "azure-sdk-for-net: $SdkRepoRoot"
 
 # Fetch and checkout remote branch if it already exists otherwise create a new branch.
-git ls-remote --exit-code --heads $RemoteName $BranchName
+git --git-dir=$SdkRepoRoot\.git ls-remote --exit-code --heads $RemoteName $BranchName
 if ($LASTEXITCODE -eq 0) {
-    Write-Host "git fetch $RemoteName $BranchName"
-    git fetch $RemoteName $BranchName
-    Write-Host "git checkout $BranchName."
-    git checkout $BranchName
+    Write-Host "git --git-dir=$SdkRepoRoot\.git fetch $RemoteName $BranchName"
+    git --git-dir=$SdkRepoRoot\.git fetch $RemoteName $BranchName
+    Write-Host "git --git-dir=$SdkRepoRoot\.git checkout $BranchName."
+    git --git-dir=$SdkRepoRoot\.git checkout $BranchName
 } else {
-    Write-Host "git checkout -b $BranchName."
-    git checkout -b $BranchName
+    Write-Host "git --git-dir=$SdkRepoRoot\.git checkout -b $BranchName."
+    git --git-dir=$SdkRepoRoot\.git checkout -b $BranchName
 }
 
-$SdkRepoRoot = Resolve-Path $SdkRepoRoot
 $PackageJson = Resolve-Path "$SdkRepoRoot\package.json"
 $PackagesProps = Resolve-Path "$SdkRepoRoot\eng\Packages.Data.props"
 
