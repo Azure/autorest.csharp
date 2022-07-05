d0c0cbd53036bb08c0c73e7e1b383165ca715a25
pshao25 <97225342+pshao25@users.noreply.github.com>
2022-07-05 10:40:20 +0800

    Add tests to validate the generated authentication functions

---
+++ test/AutoRest.TestServerLowLevel.Tests/security-aad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using AutoRest.TestServer.Tests.Infrastructure;
+using Azure.Core;
+using NUnit.Framework;
+using security_aad_LowLevel;
+
+namespace AutoRest.TestServer.Tests
+{
+    public class SecurityAadTest : TestServerLowLevelTestBase
+    {
+        [Test]
+        public Task SecurityAad() => Test(async (host) =>
+        {
+            await new AutorestSecurityAadClient(new MockCredential()).HeadAsync();
+        });
+
+        public class MockCredential : TokenCredential
+        {
+            public override ValueTask<AccessToken> GetTokenAsync(TokenRequestContext requestContext, CancellationToken cancellationToken)
+            {
+                return new(GetToken(requestContext, cancellationToken));
+            }
+
+            public override AccessToken GetToken(TokenRequestContext requestContext, CancellationToken cancellationToken)
+            {
+                return new AccessToken(string.Join(" ", requestContext.Scopes), DateTimeOffset.MaxValue);
+            }
+        }
+    }
+}
