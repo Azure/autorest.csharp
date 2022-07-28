a08282415b5fe521c0120338ccd04e64f2510ee6
Feng Zhou <fenzhou@microsoft.com>
2022-07-28 14:46:25 +0800

    regen samples

--- samples/Azure.ResourceManager.Sample/Generated/Models/RequestRateByIntervalContent.Serialization.cs
+++ samples/Azure.ResourceManager.Sample/Generated/Models/RequestRateByIntervalContent.Serialization.cs
@@ -20,9 +20,9 @@ namespace Azure.ResourceManager.Sample.Models
             writer.WritePropertyName("blobContainerSasUri");
             writer.WriteStringValue(BlobContainerSasUri.AbsoluteUri);
             writer.WritePropertyName("fromTime");
-            writer.WriteStringValue(FromOn, "O");
+            writer.WriteStringValue(FromTime, "O");
             writer.WritePropertyName("toTime");
-            writer.WriteStringValue(ToOn, "O");
+            writer.WriteStringValue(ToTime, "O");
             if (Optional.IsDefined(GroupByThrottlePolicy))
             {
                 writer.WritePropertyName("groupByThrottlePolicy");
