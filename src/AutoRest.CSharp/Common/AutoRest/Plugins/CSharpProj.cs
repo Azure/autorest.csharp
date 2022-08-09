6ccea7da66dbf2b6a48ca5c765e7ff825250dfb6
Yao Kou <yaokou@microsoft.com>
2022-08-09 21:53:30 +0800

    Revert "upgrade resource manager"
    This reverts commit 3e92de53af8dc9fba3d8e453ddc93f190ed56ab6.

--- src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
+++ src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
@@ -38,7 +38,7 @@ namespace AutoRest.CSharp.AutoRest.Plugins
   </PropertyGroup>
 
   <ItemGroup>
-    <PackageReference Include=""Azure.ResourceManager"" Version=""1.3.0-alpha.20220808.1"" />
+    <PackageReference Include=""Azure.ResourceManager"" Version=""1.2.1"" />
   </ItemGroup>
 ";
 
