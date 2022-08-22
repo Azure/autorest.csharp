e82f6c0a5d004da28356e44bf0ce1d6f4a004499
Crystal YU <chunyu@microsoft.com>
2022-08-22 22:45:29 +0800

    cadl emitter ci and release pipeline (#2615)
    * cadl emitter ci
    
    * update test
    
    * update ci yml
    
    correct work directory
    
    * Rename cadlGenerate.ps1 to CadlGenerate.ps1
    
    * copy packages
    
    * Rename petstore.cadl to petStore.cadl
    
    * update emitter readme.md
    
    * rename emit UT script name
    
    * resolve comment

---
+++ eng/CadlGenerate.ps1
@@ -0,0 +1,36 @@
+#Requires -Version 7.0
+param($filter)
+$repoRoot = Resolve-Path (Join-Path $PSScriptRoot '..')
+
+$cadlProjectPath = Join-Path $repoRoot "src/CADL.Extension/Emitter.Csharp"
+Push-Location $cadlProjectPath
+
+# testProjects
+$testEmitterPath = (Join-Path $cadlProjectPath "test/TestProjects")
+
+foreach ($directory in Get-ChildItem $testEmitterPath -Directory)
+{
+    $testName = $directory.Name
+    if ($filter -and $filter -ne $testName) {
+        continue;
+    }
+    Write-Host "Emit CADL json for $testName"
+    $projectPath = Join-Path $testEmitterPath $testName
+    node node_modules/@cadl-lang/compiler/dist/core/cli.js compile --output-path $projectPath/Generated $projectPath/$testName.cadl --emit @azure-tools/cadl-csharp
+}
+
+# samples
+$samplePath = (Join-Path $cadlProjectPath "samples")
+
+foreach ($directory in Get-ChildItem $samplePath -Directory)
+{
+    $testName = $directory.Name
+    if ($filter -and $filter -ne $testName) {
+        continue;
+    }
+    Write-Host "Emit CADL json for $testName"
+    $projectPath = Join-Path $samplePath $testName
+    node node_modules/@cadl-lang/compiler/dist/core/cli.js compile --output-path $projectPath/Generated $projectPath/$testName.cadl --emit @azure-tools/cadl-csharp
+}
+
+Pop-Location
