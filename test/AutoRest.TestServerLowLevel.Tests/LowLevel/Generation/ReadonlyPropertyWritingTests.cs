c24b8d240701522f7d79b710d9bcd3ee283288aa
Mingzhe Huang <archerzz@users.noreply.github.com>
2022-08-25 21:32:45 +0800

    test(dpg): add read-only model property case (#2647)
    resolve #2341

--- test/AutoRest.TestServerLowLevel.Tests/LowLevel/Generation/ReadonlyPropertyWritingTests.cs
+++ test/AutoRest.TestServerLowLevel.Tests/LowLevel/Generation/ReadonlyPropertyWritingTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using AutoRest.CSharp.Common.Input;
 using AutoRest.CSharp.Generation.Types;
+using AutoRest.CSharp.Output.Models;
 using AutoRest.CSharp.Output.Models.Types;
 using NUnit.Framework;
 
@@ -12,38 +13,39 @@ namespace AutoRest.CSharp.Generation.Writers.Tests
         public void RoundTripModel(string expectedModelCodes, string expectedSerializationCodes)
         {
             // refer to the original CADL file: https://github.com/Azure/cadl-ranch/blob/main/packages/cadl-ranch-specs/http/models/readonly-properties/main.cadl
-            // TODO: add model types after resolving how to deal with properties of model types
-            var model = new ModelTypeProvider(
-                new InputModelType("RoundTripModel", "Cadl.TestServer.ReadonlyProperties.Models", "public", "Readonly model", InputModelTypeUsage.RoundTrip,
-                    ReadOnlyProperties, null, null, null),
-                CadlTypeFactory,
-                "test",
-                null);
+            var model = new InputModelType("RoundTripModel", "Cadl.TestServer.ReadonlyProperties.Models", "public", "Readonly model", InputModelTypeUsage.RoundTrip,
+                    ReadOnlyProperties, null, null, null);
 
-            ValidateGeneratedCodes(model, expectedModelCodes, expectedSerializationCodes);
+            var library = new DpgOutputLibraryBuilder(new InputNamespace("Cadl.TestServer.ReadonlyProperties.Models", null, new List<string>(),
+                new List<InputEnumType>(), new List<InputModelType> { ReadonlyModel, model }, new List<InputClient>(), new InputAuth()), default).Build(true);
+
+            ValidateGeneratedCodes("RoundTripModel", expectedModelCodes, expectedSerializationCodes, library);
         }
 
         [TestCaseSource(nameof(OutputModelCase))]
         public void OutputModel(string expectedModelCodes, string expectedSerializationCodes)
         {
             // refer to the original CADL file: https://github.com/Azure/cadl-ranch/blob/dc6d00c98983f34b2f723c90fe840678a863438c/packages/cadl-ranch-specs/http/models/readonly-properties/main.cadl#L24-L65
-            // TODO: add model types after resolving how to deal with properties of model types
-            var model = new ModelTypeProvider(
-                new InputModelType("OutputModel", "Cadl.TestServer.ReadonlyProperties.Models", "public", "Readonly model", InputModelTypeUsage.Output,
-                    ReadOnlyProperties, null, null, null),
-                CadlTypeFactory,
-                "test",
-                null);
+            var model = new InputModelType("OutputModel", "Cadl.TestServer.ReadonlyProperties.Models", "public", "Readonly model", InputModelTypeUsage.Output,
+                    ReadOnlyProperties, null, null, null);
 
-            ValidateGeneratedCodes(model, expectedModelCodes, expectedSerializationCodes);
+            var library = new DpgOutputLibraryBuilder(new InputNamespace("Cadl.TestServer.ReadonlyProperties.Models", null, new List<string>(),
+                new List<InputEnumType>(), new List<InputModelType> { ReadonlyModel, model }, new List<InputClient>(), new InputAuth()), default).Build(true);
+
+            ValidateGeneratedCodes("OutputModel", expectedModelCodes, expectedSerializationCodes, library);
         }
 
         // below are test cases
+        private static readonly InputModelType ReadonlyModel = new InputModelType("ReadonlyModel", "Cadl.TestServer.ReadonlyProperties.Models", "public", "Readonly model", InputModelTypeUsage.Output,
+                    new List<InputModelProperty> { RequiredStringProperty }, null, null, null);
+
         private static readonly IReadOnlyList<InputModelProperty> ReadOnlyProperties = new List<InputModelProperty>{
             new InputModelProperty("requiredReadonlyString", "requiredReadonlyString", "Required string, illustrating a readonly reference type property.", InputPrimitiveType.String, true, true, false),
             new InputModelProperty("requiredReadonlyInt", "requiredReadonlyInt", "Required int, illustrating a readonly reference type property.", InputPrimitiveType.Int32, true, true, false),
             new InputModelProperty("optionalReadonlyString", "optionalReadonlyString", "Optional string, illustrating a readonly reference type property.", InputPrimitiveType.String, false, true, false),
             new InputModelProperty("optionalReadonlyInt", "optionalReadonlyInt", "Optional int, illustrating a readonly reference type property.", InputPrimitiveType.Int32, false, true, false),
+            new InputModelProperty("requiredReadonlyModel", "requiredReadonlyModel", "Required readonly model.", ReadonlyModel, true, true, false),
+            new InputModelProperty("optionalReadonlyModel", "optionalReadonlyModel", "Optional readonly model", ReadonlyModel, false, true, false),
             new InputModelProperty("requiredReadonlyStringList", "requiredReadonlyStringList", "Required readonly string collection.", new InputListType("requiredReadonlyStringList", InputPrimitiveType.String), true, true, false),
             new InputModelProperty("requiredReadonlyIntList", "requiredReadonlyIntList", "Required readonly int collection.", new InputListType("requiredReadonlyIntList", InputPrimitiveType.Int32), true, true, false),
             new InputModelProperty("optionalReadonlyStringList", "optionalReadonlyStringList", "Optional readonly string collection.", new InputListType("optionalReadonlyStringList", InputPrimitiveType.String), false, true, false),
@@ -81,16 +83,20 @@ OptionalReadonlyIntList = Array.Empty<int>();
 /// <param name=""requiredReadonlyInt""> Required int, illustrating a readonly reference type property. </param>
 /// <param name=""optionalReadonlyString""> Optional string, illustrating a readonly reference type property. </param>
 /// <param name=""optionalReadonlyInt""> Optional int, illustrating a readonly reference type property. </param>
+/// <param name=""requiredReadonlyModel""> Required readonly model. </param>
+/// <param name=""optionalReadonlyModel""> Optional readonly model. </param>
 /// <param name=""requiredReadonlyStringList""> Required readonly string collection. </param>
 /// <param name=""requiredReadonlyIntList""> Required readonly int collection. </param>
 /// <param name=""optionalReadonlyStringList""> Optional readonly string collection. </param>
 /// <param name=""optionalReadonlyIntList""> Optional readonly int collection. </param>
-internal RoundTripModel(string requiredReadonlyString,int requiredReadonlyInt,string optionalReadonlyString,int? optionalReadonlyInt,global::System.Collections.Generic.IReadOnlyList<string> requiredReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> requiredReadonlyIntList,global::System.Collections.Generic.IReadOnlyList<string> optionalReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> optionalReadonlyIntList)
+internal RoundTripModel(string requiredReadonlyString,int requiredReadonlyInt,string optionalReadonlyString,int? optionalReadonlyInt,global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel requiredReadonlyModel,global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel optionalReadonlyModel,global::System.Collections.Generic.IReadOnlyList<string> requiredReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> requiredReadonlyIntList,global::System.Collections.Generic.IReadOnlyList<string> optionalReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> optionalReadonlyIntList)
 {
 RequiredReadonlyString = requiredReadonlyString;
 RequiredReadonlyInt = requiredReadonlyInt;
 OptionalReadonlyString = optionalReadonlyString;
 OptionalReadonlyInt = optionalReadonlyInt;
+RequiredReadonlyModel = requiredReadonlyModel;
+OptionalReadonlyModel = optionalReadonlyModel;
 RequiredReadonlyStringList = requiredReadonlyStringList;
 RequiredReadonlyIntList = requiredReadonlyIntList;
 OptionalReadonlyStringList = optionalReadonlyStringList;
@@ -108,6 +114,12 @@ public string OptionalReadonlyString{ get; }
 
 /// <summary> Optional int, illustrating a readonly reference type property. </summary>
 public int? OptionalReadonlyInt{ get; }
+
+/// <summary> Required readonly model. </summary>
+public global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel RequiredReadonlyModel{ get; }
+
+/// <summary> Optional readonly model. </summary>
+public global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel OptionalReadonlyModel{ get; }
 
 /// <summary> Required readonly string collection. </summary>
 public global::System.Collections.Generic.IReadOnlyList<string> RequiredReadonlyStringList{ get; }
@@ -151,6 +163,8 @@ string requiredReadonlyString = default;
 int requiredReadonlyInt = default;
 global::Azure.Core.Optional<string> optionalReadonlyString = default;
 global::Azure.Core.Optional<int?> optionalReadonlyInt = default;
+global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel requiredReadonlyModel = default;
+global::Azure.Core.Optional<global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel> optionalReadonlyModel = default;
 global::System.Collections.Generic.IReadOnlyList<string> requiredReadonlyStringList = default;
 global::System.Collections.Generic.IReadOnlyList<int> requiredReadonlyIntList = default;
 global::Azure.Core.Optional<global::System.Collections.Generic.IReadOnlyList<string>> optionalReadonlyStringList = default;
@@ -175,6 +189,18 @@ if (property.Value.ValueKind == global::System.Text.Json.JsonValueKind.Null)
 optionalReadonlyInt = null;
 continue;}
 optionalReadonlyInt = property.Value.GetInt32();
+continue;
+}
+if(property.NameEquals(""requiredReadonlyModel"")){
+requiredReadonlyModel = global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel.DeserializeReadonlyModel(property.Value);
+continue;
+}
+if(property.NameEquals(""optionalReadonlyModel"")){
+if (property.Value.ValueKind == global::System.Text.Json.JsonValueKind.Null)
+{
+property.ThrowNonNullablePropertyIsNull();
+continue;}
+optionalReadonlyModel = global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel.DeserializeReadonlyModel(property.Value);
 continue;
 }
 if(property.NameEquals(""requiredReadonlyStringList"")){
@@ -218,7 +244,7 @@ optionalReadonlyIntList = array;
 continue;
 }
 }
-return new global::Cadl.TestServer.ReadonlyProperties.Models.RoundTripModel(requiredReadonlyString, requiredReadonlyInt, optionalReadonlyString, global::Azure.Core.Optional.ToNullable(optionalReadonlyInt), requiredReadonlyStringList, requiredReadonlyIntList, global::Azure.Core.Optional.ToList(optionalReadonlyStringList), global::Azure.Core.Optional.ToList(optionalReadonlyIntList));}
+return new global::Cadl.TestServer.ReadonlyProperties.Models.RoundTripModel(requiredReadonlyString, requiredReadonlyInt, optionalReadonlyString, global::Azure.Core.Optional.ToNullable(optionalReadonlyInt), requiredReadonlyModel, optionalReadonlyModel, requiredReadonlyStringList, requiredReadonlyIntList, global::Azure.Core.Optional.ToList(optionalReadonlyStringList), global::Azure.Core.Optional.ToList(optionalReadonlyIntList));}
 
 internal global::Azure.Core.RequestContent ToRequestContent()
 {
@@ -269,16 +295,20 @@ OptionalReadonlyIntList = Array.Empty<int>();
 /// <param name=""requiredReadonlyInt""> Required int, illustrating a readonly reference type property. </param>
 /// <param name=""optionalReadonlyString""> Optional string, illustrating a readonly reference type property. </param>
 /// <param name=""optionalReadonlyInt""> Optional int, illustrating a readonly reference type property. </param>
+/// <param name=""requiredReadonlyModel""> Required readonly model. </param>
+/// <param name=""optionalReadonlyModel""> Optional readonly model. </param>
 /// <param name=""requiredReadonlyStringList""> Required readonly string collection. </param>
 /// <param name=""requiredReadonlyIntList""> Required readonly int collection. </param>
 /// <param name=""optionalReadonlyStringList""> Optional readonly string collection. </param>
 /// <param name=""optionalReadonlyIntList""> Optional readonly int collection. </param>
-internal OutputModel(string requiredReadonlyString,int requiredReadonlyInt,string optionalReadonlyString,int? optionalReadonlyInt,global::System.Collections.Generic.IReadOnlyList<string> requiredReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> requiredReadonlyIntList,global::System.Collections.Generic.IReadOnlyList<string> optionalReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> optionalReadonlyIntList)
+internal OutputModel(string requiredReadonlyString,int requiredReadonlyInt,string optionalReadonlyString,int? optionalReadonlyInt,global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel requiredReadonlyModel,global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel optionalReadonlyModel,global::System.Collections.Generic.IReadOnlyList<string> requiredReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> requiredReadonlyIntList,global::System.Collections.Generic.IReadOnlyList<string> optionalReadonlyStringList,global::System.Collections.Generic.IReadOnlyList<int> optionalReadonlyIntList)
 {
 RequiredReadonlyString = requiredReadonlyString;
 RequiredReadonlyInt = requiredReadonlyInt;
 OptionalReadonlyString = optionalReadonlyString;
 OptionalReadonlyInt = optionalReadonlyInt;
+RequiredReadonlyModel = requiredReadonlyModel;
+OptionalReadonlyModel = optionalReadonlyModel;
 RequiredReadonlyStringList = requiredReadonlyStringList;
 RequiredReadonlyIntList = requiredReadonlyIntList;
 OptionalReadonlyStringList = optionalReadonlyStringList;
@@ -296,6 +326,12 @@ public string OptionalReadonlyString{ get; }
 
 /// <summary> Optional int, illustrating a readonly reference type property. </summary>
 public int? OptionalReadonlyInt{ get; }
+
+/// <summary> Required readonly model. </summary>
+public global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel RequiredReadonlyModel{ get; }
+
+/// <summary> Optional readonly model. </summary>
+public global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel OptionalReadonlyModel{ get; }
 
 /// <summary> Required readonly string collection. </summary>
 public global::System.Collections.Generic.IReadOnlyList<string> RequiredReadonlyStringList{ get; }
@@ -333,6 +369,8 @@ string requiredReadonlyString = default;
 int requiredReadonlyInt = default;
 global::Azure.Core.Optional<string> optionalReadonlyString = default;
 global::Azure.Core.Optional<int?> optionalReadonlyInt = default;
+global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel requiredReadonlyModel = default;
+global::Azure.Core.Optional<global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel> optionalReadonlyModel = default;
 global::System.Collections.Generic.IReadOnlyList<string> requiredReadonlyStringList = default;
 global::System.Collections.Generic.IReadOnlyList<int> requiredReadonlyIntList = default;
 global::Azure.Core.Optional<global::System.Collections.Generic.IReadOnlyList<string>> optionalReadonlyStringList = default;
@@ -357,6 +395,18 @@ if (property.Value.ValueKind == global::System.Text.Json.JsonValueKind.Null)
 optionalReadonlyInt = null;
 continue;}
 optionalReadonlyInt = property.Value.GetInt32();
+continue;
+}
+if(property.NameEquals(""requiredReadonlyModel"")){
+requiredReadonlyModel = global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel.DeserializeReadonlyModel(property.Value);
+continue;
+}
+if(property.NameEquals(""optionalReadonlyModel"")){
+if (property.Value.ValueKind == global::System.Text.Json.JsonValueKind.Null)
+{
+property.ThrowNonNullablePropertyIsNull();
+continue;}
+optionalReadonlyModel = global::Cadl.TestServer.ReadonlyProperties.Models.ReadonlyModel.DeserializeReadonlyModel(property.Value);
 continue;
 }
 if(property.NameEquals(""requiredReadonlyStringList"")){
@@ -400,7 +450,7 @@ optionalReadonlyIntList = array;
 continue;
 }
 }
-return new global::Cadl.TestServer.ReadonlyProperties.Models.OutputModel(requiredReadonlyString, requiredReadonlyInt, optionalReadonlyString, global::Azure.Core.Optional.ToNullable(optionalReadonlyInt), requiredReadonlyStringList, requiredReadonlyIntList, global::Azure.Core.Optional.ToList(optionalReadonlyStringList), global::Azure.Core.Optional.ToList(optionalReadonlyIntList));}
+return new global::Cadl.TestServer.ReadonlyProperties.Models.OutputModel(requiredReadonlyString, requiredReadonlyInt, optionalReadonlyString, global::Azure.Core.Optional.ToNullable(optionalReadonlyInt), requiredReadonlyModel, optionalReadonlyModel, requiredReadonlyStringList, requiredReadonlyIntList, global::Azure.Core.Optional.ToList(optionalReadonlyStringList), global::Azure.Core.Optional.ToList(optionalReadonlyIntList));}
 
 internal static global::Cadl.TestServer.ReadonlyProperties.Models.OutputModel FromResponse(global::Azure.Response response)
 {
