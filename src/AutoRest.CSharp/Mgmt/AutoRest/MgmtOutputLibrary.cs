c5d31c9ce94582a48e07e58c25d70bf58eb1da59
Arcturus Zhang <dapzhang@microsoft.com>
2022-09-23 13:28:34 +0800

    minor fix after merge

--- src/AutoRest.CSharp/Mgmt/AutoRest/MgmtOutputLibrary.cs
+++ src/AutoRest.CSharp/Mgmt/AutoRest/MgmtOutputLibrary.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using AutoRest.CSharp.Common.Input;
 using AutoRest.CSharp.Common.Output.Builders;
 using AutoRest.CSharp.Common.Utilities;
 using AutoRest.CSharp.Generation.Types;
