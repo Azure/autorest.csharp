102f3eff2cbf7b5aaa1c57f7d2794c6c3a30226b
archerzz <archerzz@gmail.com>
2022-10-18 11:15:59 +0800

    fix broken test

--- test/AutoRest.TestServerLowLevel.Tests/LowLevel/ParameterSequenceTests.cs
+++ test/AutoRest.TestServerLowLevel.Tests/LowLevel/ParameterSequenceTests.cs
@@ -9,13 +9,13 @@ namespace AutoRest.LowLevel.Tests
 {
     public class ParameterSequenceTests
     {
-        [TestCase("OptionalParameters", new string[] { "id", "skip", "name", "context" }, new bool[] { false, false, true, true})]
-        [TestCase("OptionalParametersWithMixedSequence", new string[] { "id", "name", "skip", "context" }, new bool[] { false, true, true, true})]
-        public void OptionalParameterAtEnd(string methodName, string[] parameterNames)
+        [TestCase("OptionalPathParameters", new string[] { "id", "skip", "name", "context" }, new bool[] { false, false, true, true})]
+        [TestCase("OptionalPathParametersWithMixedSequence", new string[] { "id", "name", "skip", "context" }, new bool[] { false, true, true, true})]
+        public void OptionalParameterAtEnd(string methodName, string[] parameterNames, bool[] isOptional)
         {
             var method = typeof(ParametersLowlevelClient).GetMethod(methodName);
             Assert.AreEqual(parameterNames, method.GetParameters().Select(p => p.Name).ToArray());
-            method.GetParameters().Select(p => p.HasDefaultValue).ToArray();
+            Assert.AreEqual(isOptional, method.GetParameters().Select(p => p.HasDefaultValue).ToArray());
         }
     }
 }
