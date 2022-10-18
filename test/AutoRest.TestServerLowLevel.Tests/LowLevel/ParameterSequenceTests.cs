c08f057eef99fefffea947522cff92d13bbfacb3
archerzz <archerzz@gmail.com>
2022-10-18 14:35:24 +0800

    add cadl test

--- test/AutoRest.TestServerLowLevel.Tests/LowLevel/ParameterSequenceTests.cs
+++ test/AutoRest.TestServerLowLevel.Tests/LowLevel/ParameterSequenceTests.cs
@@ -1,19 +1,25 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Linq;
 using Parameters_LowLevel;
+using ParametersCadl;
 using NUnit.Framework;
 
 namespace AutoRest.LowLevel.Tests
 {
     public class ParameterSequenceTests
     {
-        [TestCase("OptionalPathParameters", new string[] { "id", "skip", "name", "context" }, new bool[] { false, false, true, true})]
-        [TestCase("OptionalPathParametersWithMixedSequence", new string[] { "id", "name", "skip", "context" }, new bool[] { false, true, true, true})]
-        public void OptionalParameterAtEnd(string methodName, string[] parameterNames, bool[] isOptional)
+        [TestCase(typeof(ParametersLowlevelClient), "OptionalPathParameters", new string[] { "id", "skip", "name", "context" }, new bool[] { false, false, true, true})]
+        [TestCase(typeof(ParametersLowlevelClient),"OptionalPathParametersWithMixedSequence", new string[] { "id", "name", "skip", "context" }, new bool[] { false, true, true, true})]
+        [TestCase(typeof(ParameterOrdersClient),"OperationValue", new string[] { "start", "end", "cancellationToken" }, new bool[] { false, true, true})]
+        [TestCase(typeof(ParameterOrdersClient),"Operation", new string[] { "start", "end", "context" }, new bool[] { false, true, true})]
+        [TestCase(typeof(ParameterOrdersClient),"Operation2Value", new string[] { "end", "start", "cancellationToken" }, new bool[] { false, true, true})]
+        [TestCase(typeof(ParameterOrdersClient),"Operation2", new string[] { "end", "start", "context" }, new bool[] { false, true, true})]
+        public void OptionalParameterAtEnd(Type type, string methodName, string[] parameterNames, bool[] isOptional)
         {
-            var method = typeof(ParametersLowlevelClient).GetMethod(methodName);
+            var method = type.GetMethod(methodName);
             Assert.AreEqual(parameterNames, method.GetParameters().Select(p => p.Name).ToArray());
             Assert.AreEqual(isOptional, method.GetParameters().Select(p => p.HasDefaultValue).ToArray());
         }
