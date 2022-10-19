dec9ae982b0b8d9ab24ae3323cf3c175eda22d24
Crystal YU <chunyu@microsoft.com>
2022-10-19 11:11:05 +0800

    Emitter option and generate configuration (#2780)
    * support options and emit configuration
    
    * generate configuration.json
    
    * Delete Configuration.json
    
    * Update emitter.ts
    
    * update CadlRanchProjects.Tests

--- test/CadlRanchProjects/property-types/Generated/FloatClient.cs
+++ test/CadlRanchProjects/property-types/Generated/FloatClient.cs
@@ -11,7 +11,7 @@ using Azure;
 using Azure.Core;
 using Azure.Core.Pipeline;
 
-namespace property_types
+namespace Models.Property.Types
 {
     // Data plane generated client. The Float service client.
     /// <summary> The Float service client. </summary>
