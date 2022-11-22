af27cc3549f0bb286ef3475b837eebbe0f7cdc0e
chunyu3 <chunyu@microsoft.com>
2022-11-22 18:11:44 +0800

    update emitter version

--- test/CadlRanchProjects.Tests/InheritanceTests.cs
+++ test/CadlRanchProjects.Tests/InheritanceTests.cs
@@ -5,6 +5,7 @@ using System;
 using System.Threading.Tasks;
 using AutoRest.TestServer.Tests.Infrastructure;
 using Models.Inheritance;
+using Models.Inheritance.Models;
 using NUnit.Framework;
 
 namespace CadlRanchProjects.Tests
