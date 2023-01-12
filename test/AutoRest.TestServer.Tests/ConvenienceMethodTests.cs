16be672203213086acf165f5ebe7777b33938682
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-01-12 14:37:32 +0800

    Update

--- test/AutoRest.TestServer.Tests/ConvenienceMethodTests.cs
+++ test/AutoRest.TestServer.Tests/ConvenienceMethodTests.cs
@@ -1,103 +1,72 @@
-﻿//// Copyright (c) Microsoft Corporation. All rights reserved.
-//// Licensed under the MIT License.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-//using System.Linq;
-//using System.Reflection;
-//using System.Threading;
-//using Azure;
-//using ConvenienceInitialInCadl;
-//using ConvenienceInCadl;
-//using NUnit.Framework;
+using System.Linq;
+using System.Threading;
+using Azure;
+using ConvenienceInCadl;
+using NUnit.Framework;
 
-//namespace AutoRest.TestServer.Tests
-//{
-//    public class ConvenienceMethodTests
-//    {
-//        [Test]
-//        public void InitialProtocolScenario()
-//        {
-//            var protocolInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialProtocol");
-//            Assert.AreEqual(true, protocolInInitial.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInInitial.GetParameters().Last().ParameterType);
-//            var convenienceInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialProtocolValue");
-//            Assert.IsNull(convenienceInInitial);
+namespace AutoRest.TestServer.Tests
+{
+    public class ConvenienceMethodTests
+    {
+        [Test]
+        public void ProtocolScenario()
+        {
+            var protocolInUpdate = typeof(ConvenienceInCadlClient).GetMethod("Protocol");
+            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
+            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
+            var convenienceInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ProtocolValue");
+            Assert.AreEqual(typeof(CancellationToken), convenienceInUpdate.GetParameters().Last().ParameterType);
+        }
 
-//            var protocolInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialProtocol");
-//            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
-//            var convenienceInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialProtocolValue");
-//            Assert.AreEqual(typeof(CancellationToken), convenienceInUpdate.GetParameters().Last().ParameterType);
-//        }
+        [Test]
+        public void ConvenienceWithOptionalScenario()
+        {
+            var protocolInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ConvenienceWithOptional");
+            var convenienceInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ConvenienceWithOptionalValue");
+            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
+            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
+            Assert.AreEqual(typeof(CancellationToken), convenienceInUpdate.GetParameters().Last().ParameterType);
+        }
 
-//        [Test]
-//        public void InitialConvenienceWithOptionalScenario()
-//        {
-//            var protocolInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialConvenienceWithOptional");
-//            var convenienceInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialConvenienceWithOptionalValue");
-//            Assert.AreEqual(true, protocolInInitial.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInInitial.GetParameters().Last().ParameterType);
-//            Assert.AreEqual(typeof(CancellationToken), convenienceInInitial.GetParameters().Last().ParameterType);
+        [Test]
+        public void ConvenienceWithRequiredScenario()
+        {
+            var protocolInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ConvenienceWithRequired", new[] { typeof(RequestContext) });
+            var convenienceInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ConvenienceWithRequired", new[] { typeof(CancellationToken) });
+            Assert.AreEqual(false, protocolInUpdate.GetParameters().Last().IsOptional);
+            Assert.IsNotNull(convenienceInUpdate);
+        }
 
-//            var protocolInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialConvenienceWithOptional");
-//            var convenienceInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialConvenienceWithOptionalValue");
-//            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
-//            Assert.AreEqual(typeof(CancellationToken), convenienceInUpdate.GetParameters().Last().ParameterType);
-//        }
+        [Test]
+        public void ConvenienceShouldNotGenerateScenario()
+        {
+            var protocolInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ConvenienceShouldNotGenerate");
+            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
+            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
+            var convenienceInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ConvenienceShouldNotGenerateValue");
+            Assert.IsNull(convenienceInUpdate);
+        }
 
-//        [Test]
-//        public void InitialConvenienceWithRequiredScenario()
-//        {
-//            var protocolInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialConvenienceWithRequired", new[] { typeof(RequestContext) });
-//            var convenienceInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialConvenienceWithRequired", new[] { typeof(CancellationToken) });
-//            Assert.AreEqual(false, protocolInInitial.GetParameters().Last().IsOptional);
-//            Assert.IsNotNull(convenienceInInitial);
+        [Test]
+        public void ProtocolShouldNotGenerateConvenienceScenario()
+        {
+            var protocolInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ProtocolShouldNotGenerateConvenience");
+            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
+            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
+            var convenienceInUpdate = typeof(ConvenienceInCadlClient).GetMethod("ProtocolShouldNotGenerateConvenienceValue");
+            Assert.IsNull(convenienceInUpdate);
+        }
 
-//            var protocolInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialConvenienceWithRequired", new[] { typeof(RequestContext) });
-//            var convenienceInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialConvenienceWithRequired", new[] { typeof(CancellationToken) });
-//            Assert.AreEqual(false, protocolInUpdate.GetParameters().Last().IsOptional);
-//            Assert.IsNotNull(convenienceInUpdate);
-//        }
-
-//        [Test]
-//        public void InitialConvenienceShouldNotGenerateScenario()
-//        {
-//            var protocolInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialConvenienceShouldNotGenerate");
-//            Assert.AreEqual(true, protocolInInitial.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInInitial.GetParameters().Last().ParameterType);
-//            var convenienceInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialConvenienceShouldNotGenerateValue");
-//            Assert.IsNull(convenienceInInitial);
-
-//            var protocolInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialConvenienceShouldNotGenerate");
-//            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
-//            var convenienceInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialConvenienceShouldNotGenerateValue");
-//            Assert.IsNull(convenienceInUpdate);
-//        }
-
-//        [Test]
-//        public void InitialProtocolShouldNotGenerateConvenienceScenario()
-//        {
-//            var protocolInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialProtocolShouldNotGenerateConvenience");
-//            Assert.AreEqual(true, protocolInInitial.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInInitial.GetParameters().Last().ParameterType);
-//            var convenienceInInitial = typeof(ConvenienceInitialInCadlClient).GetMethod("InitialProtocolShouldNotGenerateConvenienceValue");
-//            Assert.IsNull(convenienceInInitial);
-
-//            var protocolInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialProtocolShouldNotGenerateConvenience");
-//            Assert.AreEqual(true, protocolInUpdate.GetParameters().Last().IsOptional);
-//            Assert.AreEqual(typeof(RequestContext), protocolInUpdate.GetParameters().Last().ParameterType);
-//            var convenienceInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("InitialProtocolShouldNotGenerateConvenienceValue");
-//            Assert.IsNull(convenienceInUpdate);
-//        }
-
-//        [Test]
-//        public void UpdateConvenienceScenario()
-//        {
-//            var protocolInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("UpdateConvenience", new[] { typeof(RequestContext) });
-//            var convenienceInUpdate = typeof(ConvenienceUpdateInCadlClient).GetMethod("UpdateConvenience", new[] { typeof(CancellationToken) });
-//            Assert.AreEqual(false, protocolInUpdate.GetParameters().Last().IsOptional);
-//            Assert.IsNotNull(convenienceInUpdate);
-//        }
-//    }
-//}
+        [Test]
+        public void UpdateConvenienceScenario()
+        {
+            var protocolInUpdate = typeof(ConvenienceInCadlClient).GetMethod("UpdateConvenience", new[] { typeof(RequestContext) });
+            var convenienceInUpdate = typeof(ConvenienceInCadlClient).GetMethod("UpdateConvenience", new[] { typeof(CancellationToken) });
+            Assert.AreEqual(false, protocolInUpdate.GetParameters().Last().IsOptional);
+            Assert.IsNotNull(convenienceInUpdate);
+        }
+    }
+}
