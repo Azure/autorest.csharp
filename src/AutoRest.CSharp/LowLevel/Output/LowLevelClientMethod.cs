e1007753bfb7b6b23c51833b4bef5d123504ab2b
ArcturusZhang <dapzhang@microsoft.com>
2023-01-19 18:21:43 +0800

    refactor

--- src/AutoRest.CSharp/LowLevel/Output/LowLevelClientMethod.cs
+++ src/AutoRest.CSharp/LowLevel/Output/LowLevelClientMethod.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using AutoRest.CSharp.Common.Input;
 using AutoRest.CSharp.Common.Output.Models;
 using AutoRest.CSharp.Output.Models.Requests;
