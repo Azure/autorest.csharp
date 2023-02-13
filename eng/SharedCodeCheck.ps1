7053658fd611e4e914b40a273009fc3d8cc881b0
Mingzhe Huang <archerzz@users.noreply.github.com>
2023-02-13 11:59:08 +0800

    build(common): split and parallelize `check if code is up-to-date` (#3101)
    - change smoke test template into code generation test template, since:
      - the smoke test is actually a subset of code generation test
      - both of them are run in our pipeline, which is duplication
    - refactor corresponding scripts
    
    resolve #3100

--- eng/SharedCodeCheck.ps1
+++ eng/SharedCodeCheck.ps1
@@ -5,13 +5,13 @@ Set-StrictMode -Version 1
 
 Write-Host 'Downloading shared source files...'
 & (Join-Path $PSScriptRoot 'DownloadSharedSource.ps1')
+Write-Host 'Shared source files are downloaded'
 
-Write-Host 'Generating test clients...'
-& (Join-Path $PSScriptRoot 'Generate.ps1') -reset
-
-Write-Host 'Checking generated file differences...'
+Write-Host 'Checking file difference...'
 git -c core.safecrlf=false diff --ignore-space-at-eol --exit-code
 if ($LastExitCode -ne 0)
 {
-    Write-Error 'Generated code is not up to date. Please run: eng/Generate.ps1'
+    Write-Error 'Shared source files are updated. Please run eng/DownloadSharedSource.ps1'
+    exit 1
 }
+Write-Host 'Done. No change is detected.'
