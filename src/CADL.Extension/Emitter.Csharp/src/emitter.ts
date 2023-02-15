00fe849fb49a9eda0c343ea8ba935c6575a7fcca
chunyu3 <chunyu@microsoft.com>
2023-02-15 14:24:24 +0800

    upgrade APIs and test project cadl file

--- src/CADL.Extension/Emitter.Csharp/src/emitter.ts
+++ src/CADL.Extension/Emitter.Csharp/src/emitter.ts
@@ -5,19 +5,19 @@ import {
     createCadlLibrary,
     getDeprecated,
     getDoc,
-    getServiceNamespace,
-    getServiceNamespaceString,
-    getServiceTitle,
-    getServiceVersion,
+    getNamespaceFullName,
     getSummary,
     ignoreDiagnostics,
     isErrorModel,
     JSONSchemaType,
+    listServices,
     Model,
     ModelProperty,
+    Namespace,
     Operation,
     Program,
-    resolvePath
+    resolvePath,
+    Service
 } from "@cadl-lang/compiler";
 import {
     getAllHttpServices,
@@ -79,7 +79,6 @@ import {
     Client,
     createDpgContext,
     DpgEmitterOptions,
-    getConvenienceAPIName,
     isApiVersion,
     isOperationGroup,
     listClients,
@@ -90,7 +89,7 @@ import {
     shouldGenerateProtocol
 } from "@azure-tools/cadl-dpg";
 import { ClientKind } from "./type/ClientKind.js";
-import { getVersions } from "@cadl-lang/versioning";
+import { buildVersionProjections, getVersions } from "@cadl-lang/versioning";
 import { EmitContext } from "@cadl-lang/compiler/*";
 import { capitalize } from "./lib/utils.js";
 
@@ -188,9 +187,10 @@ export async function $onEmit(context: EmitContext<NetEmitterOptions>) {
 
     if (!program.compilerOptions.noEmit && !program.hasError()) {
         // Write out the dotnet model to the output path
-        const namespace = getServiceNamespaceString(program) || "";
+        // const namespace = getServiceNamespaceString(program) || "";
 
         const root = createModel(context);
+        const namespace = root.Name;
         // await program.host.writeFile(outPath, prettierOutput(JSON.stringify(root, null, 2)));
         if (root) {
             const generatedFolder = resolvePath(outputFolder, "Generated");
@@ -302,21 +302,54 @@ function getClient(
 }
 
 export function createModel(context: EmitContext<NetEmitterOptions>): any {
-    const program = context.program;
-    const serviceNamespaceType = getServiceNamespace(program);
-    if (!serviceNamespaceType) {
-        return;
+    const services = listServices(context.program);
+    if (services.length === 0) {
+      services.push({ type: context.program.getGlobalNamespaceType() });
     }
-    const title = getServiceTitle(program);
-    const apiVersions: Set<string> = new Set<string>();
-    let version = getServiceVersion(program);
-    if (version !== "0000-00-00") {
-        apiVersions.add(version);
+    
+    // TODO: support multiple service. Current only chose the first service.
+    const service = services[0];
+    const serviceNamespaceType = service.type;
+    if (serviceNamespaceType === undefined) {
+      throw Error("Can not emit yaml for a namespace that doesn't exist.");
     }
 
+    return createModelForService(context, service);
+
+}
+
+export function createModelForService(context: EmitContext<NetEmitterOptions>, service: Service): any {
+    const program = context.program;
+    // const services = listServices(program);
+    // if (services.length === 0) {
+    //   services.push({ type: program.getGlobalNamespaceType() });
+    // }
+    
+    // // TODO: support multiple service. Current only chose the first service.
+    // const service = services[0];
+    // const serviceNamespaceType = service.type;
+    // if (serviceNamespaceType === undefined) {
+    //   throw Error("Can not emit yaml for a namespace that doesn't exist.");
+    // }
+
+    //const versions = buildVersionProjections(program, service.type);
+
+
+
+    // const serviceNamespaceType = getServiceNamespace(program);
+    // if (!serviceNamespaceType) {
+    //     return;
+    // }
+    const title = service.title;
+    const serviceNamespaceType = service.type;
+    const apiVersions: Set<string> = new Set<string>();
+    let version = service.version;
+    if (version && version !== "0000-00-00") {
+        apiVersions.add(version);
+    }
     const versions = getVersions(
         program,
-        serviceNamespaceType
+        service.type
     )[1]?.getVersions();
     if (versions) {
         for (const ver of versions) {
@@ -359,7 +392,8 @@ export function createModel(context: EmitContext<NetEmitterOptions>): any {
             Value: version
         } as InputConstant
     };
-    const namespace = getServiceNamespaceString(program) || "client";
+    // const namespace = getServiceNamespaceString(program) || "client";
+    const namespace = getNamespaceFullName(serviceNamespaceType) || "client";
     const authentication = getAuthentication(program, serviceNamespaceType);
     let auth = undefined;
     if (authentication) {
@@ -372,6 +406,7 @@ export function createModel(context: EmitContext<NetEmitterOptions>): any {
     let url: string = "";
     const convenienceOperations: HttpOperation[] = [];
     let lroMonitorOperations: Set<Operation>;
+    const dpgContext = createDpgContext(context as EmitContext<any>);
     try {
         //create endpoint parameter from servers
         if (servers !== undefined) {
@@ -396,10 +431,10 @@ export function createModel(context: EmitContext<NetEmitterOptions>): any {
 
         lroMonitorOperations = getAllLroMonitorOperations(routes, program);
         const clients: InputClient[] = [];
-        const dpgClients = listClients(program);
+        const dpgClients = listClients(dpgContext);
         for (const client of dpgClients) {
             clients.push(emitClient(client));
-            const dpgOperationGroups = listOperationGroups(program, client);
+            const dpgOperationGroups = listOperationGroups(dpgContext, client);
             for (const dpgGroup of dpgOperationGroups) {
                 clients.push(emitClient(dpgGroup, client));
             }
@@ -467,7 +502,7 @@ export function createModel(context: EmitContext<NetEmitterOptions>): any {
         client: Client | OperationGroup,
         parent?: Client
     ): InputClient {
-        const operations = listOperationsInOperationGroup(program, client);
+        const operations = listOperationsInOperationGroup(dpgContext, client);
         let clientDesc = "";
         if (operations.length > 0) {
             const container = ignoreDiagnostics(
@@ -497,6 +532,7 @@ export function createModel(context: EmitContext<NetEmitterOptions>): any {
                 httpOperation,
                 url,
                 urlParameters,
+                serviceNamespaceType,
                 modelMap,
                 enumMap
             );
@@ -654,7 +690,7 @@ function processServiceAuthentication(
     return auth;
 }
 
-function getOperationGroupName(program: Program, operation: Operation): string {
+function getOperationGroupName(program: Program, operation: Operation, serviceNamespaceType: Namespace): string {
     const explicitOperationId = getOperationId(program, operation);
     if (explicitOperationId) {
         const ids: string[] = explicitOperationId.split("_");
@@ -670,7 +706,7 @@ function getOperationGroupName(program: Program, operation: Operation): string {
     if (!namespace) {
         namespace =
             program.checker.getGlobalNamespaceType() ??
-            getServiceNamespace(program);
+            serviceNamespaceType;
     }
 
     if (namespace) return namespace.name;
@@ -682,10 +718,12 @@ function loadOperation(
     operation: HttpOperation,
     uri: string,
     urlParameters: InputParameter[] | undefined = undefined,
+    serviceNamespaceType: Namespace,
     models: Map<string, InputModelType>,
     enums: Map<string, InputEnumType>
 ): InputOperation {
     const program = context.program;
+    const dpgContext = createDpgContext(context);
     const {
         path: fullPath,
         operation: op,
@@ -761,13 +799,12 @@ function loadOperation(
     }
     const requestMethod = parseHttpRequestMethod(verb);
     const generateProtocol: boolean = shouldGenerateProtocol(
-        createDpgContext(context),
+        dpgContext,
         op
     );
     const generateConvenience: boolean =
         requestMethod !== RequestMethod.PATCH &&
-        (shouldGenerateConvenient(createDpgContext(context), op) ||
-            getConvenienceAPIName(program, op) !== undefined);
+        shouldGenerateConvenient(dpgContext, op);
 
     /* handle lro */
     /* handle paging. */
@@ -798,7 +835,7 @@ function loadOperation(
         Name: op.name,
         ResourceName:
             resourceOperation?.resourceType.name ??
-            getOperationGroupName(program, op),
+            getOperationGroupName(program, op, serviceNamespaceType),
         Summary: summary,
         Deprecated: getDeprecated(program, op),
         Description: desc,
@@ -842,7 +879,7 @@ function loadOperation(
             } as InputConstant;
         }
         const requestLocation = requestLocationMap[location];
-        const isApiVer: boolean = isApiVersion(program, parameter);
+        const isApiVer: boolean = isApiVersion(dpgContext, parameter);
         const isContentType: boolean =
             requestLocation === RequestLocation.Header &&
             name.toLowerCase() === "content-type";
