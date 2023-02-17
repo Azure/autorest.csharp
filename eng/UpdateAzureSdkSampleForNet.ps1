89ebeaebdf3f4f3292c4334f93e4d22b67c0a912
archerzz <archerzz@gmail.com>
2023-02-17 15:20:12 +0800

    run code/sample generation in parallel

--- eng/UpdateAzureSdkSampleForNet.ps1
+++ eng/UpdateAzureSdkSampleForNet.ps1
@@ -1,4 +1,4 @@
-param($AutorestCSharpVersion, $CadlEmitterVersion, $SdkRepoRoot)
+param([string]$AutorestCSharpVersion, [string]$CadlEmitterVersion, [string]$SdkRepoRoot, [string[]]$ServiceDirectoryFilters = @("*"))
 
 Import-Module "$PSScriptRoot\UpdateGeneratorMetadata.psm1" -DisableNameChecking -Force;
 
@@ -7,4 +7,12 @@ $SdkRepoRoot = Resolve-Path $SdkRepoRoot
 UpdateGeneratorMetaData $AutorestCSharpVersion $CadlEmitterVersion $SdkRepoRoot
 
 Write-Host "Generating Azure SDK Samples..."
-dotnet msbuild /restore /t:GenerateTests "$SdkRepoRoot\eng\service.proj"
+foreach ($filter in $ServiceDirectoryFilters) {
+    Write-Host 'Generating projects under service directory ' -ForegroundColor Green -NoNewline
+    Write-Host "$filter" -ForegroundColor Yellow
+    dotnet msbuild /restore /t:GenerateTests /p:ServiceDirectory=$filter "$SdkRepoRoot\eng\service.proj"
+    if ($LastExitCode -ne 0) {
+        Write-Error 'Generation error'
+        exit 1
+    }
+}
