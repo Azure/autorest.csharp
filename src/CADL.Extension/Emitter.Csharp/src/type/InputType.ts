e09009228ca6575d08f3a49c889dd3bc1c745f69
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-02-27 13:57:46 +0800

    Step 1 to support multiple content types

--- src/CADL.Extension/Emitter.Csharp/src/type/InputType.ts
+++ src/CADL.Extension/Emitter.Csharp/src/type/InputType.ts
@@ -20,9 +20,17 @@ export interface InputLiteralType extends InputType {
     Value: any;
 }
 
+export function isInputLiteralType(type: InputType): type is InputLiteralType {
+    return type.Name === "Literal";
+}
+
 export interface InputUnionType extends InputType {
     Name: "Union";
     UnionItemTypes: InputType[];
+}
+
+export function isInputUnionType(type: InputType): type is InputUnionType {
+    return type.Name === "Union";
 }
 
 export interface InputModelType extends InputType {
