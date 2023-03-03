cd9a641ab97050e1659d5a7a078db6309bddc7a7
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-03-03 16:36:32 +0800

    Update

--- eng/PackEmitter.ps1
+++ eng/PackEmitter.ps1
@@ -5,11 +5,14 @@ $emitterPath = Resolve-Path (Join-Path $repoRoot 'src' 'CADL.Extension' 'Emitter
 
 # Pack emitter npm package "@azure-tools/cadl-csharp"
 Push-Location $emitterPath
-"registry=https://pkgs.dev.azure.com/azure-sdk/_packaging/azure-sdk-for-net-private/npm/registry/`n`nalways-auth=true" | Out-File -FilePath (Join-Path $emitterPath '.npmrc')
-npm install -g vsts-npm-auth
-vsts-npm-auth -config .npmrc
-npm install @autorest/csharp@$AutorestVersion --save-exact
 npm run build
+
+$packageFile = "$emitterPath\package.json"
+(Get-Content -Raw $packageFile) -replace `
+    '"@azure-tools/cadl-csharp": ".*?"',
+"`"@azure-tools/cadl-csharp`": `"$AutorestVersion`"" | `
+    Set-Content $packageFile -NoNewline
+    
 npm version --no-git-tag-version $AutorestVersion | Out-Null;
 $file = npm pack -q;
 Copy-Item $file -Destination $StagingDirectory
