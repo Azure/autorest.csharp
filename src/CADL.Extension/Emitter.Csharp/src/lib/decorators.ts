68d87b329d4f4bedf1e1598ca9a49bdb560998b0
Crystal YU <chunyu@microsoft.com>
2023-03-16 13:35:05 +0800

    Typespec renaming (#3199)
    * typespec rename and upgrade to typespec 0.41.0
    
    * update typespec projects in testProjects
    
    * upgrade cadl-ranch
    
    * update cadl-ranch projects and emitter test projects
    
    * update readme
    
    * update test
    
    * update emitter unit test
    
    * typespec renaming test projects
    
    * rename cadl ranch test
    
    * rename emitter testproject
    
    * rename cadlRanchProjects.Tests
    
    * upgrade emitter version to 0.2.0
    
    * Update eng/CadlGenerate.ps1
    
    Co-authored-by: Renhe Li <renhe.li@hotmail.com>
    
    * rename script name
    
    * update document and the build target
    
    ---------
    
    Co-authored-by: Renhe Li <renhe.li@hotmail.com>

--- src/CADL.Extension/Emitter.Csharp/src/lib/decorators.ts
+++ src/CADL.Extension/Emitter.Csharp/src/lib/decorators.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { DecoratedType, Operation, Program, Type } from "@cadl-lang/compiler";
+import { DecoratedType, Operation, Program, Type } from "@typespec/compiler";
 import { ExternalDocs } from "../type/externalDocs.js";
 
 const externalDocsKey = Symbol("externalDocs");
