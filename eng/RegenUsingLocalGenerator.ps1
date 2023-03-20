b7066c6373de3c2edfaf610bb56d6e8725ccbfdb
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-03-20 17:02:54 +0800

    Update

--- eng/RegenUsingLocalGenerator.ps1
+++
@@ -1,9 +0,0 @@
-param($SdkRepoRoot, [switch]$skipGenerateTest)
-
-& (Join-Path $PSScriptRoot 'UpdateLocalReference.ps1') -SdkRepoRoot $SdkRepoRoot
-
-dotnet msbuild /restore /t:GenerateCode "$SdkRepoRoot\eng\service.proj"
-
-if (-not $skipGenerateTest) {
-    dotnet msbuild /restore /t:GenerateTests "$SdkRepoRoot\eng\service.proj"
-}
