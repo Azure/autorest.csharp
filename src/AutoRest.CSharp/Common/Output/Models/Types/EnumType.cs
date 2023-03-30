3d7e026ff786dcc9142a80e65223c80133dd768f
Arcturus Zhang <dapzhang@microsoft.com>
2023-03-30 16:34:43 +0800

    some initial work

--- src/AutoRest.CSharp/Common/Output/Models/Types/EnumType.cs
+++ src/AutoRest.CSharp/Common/Output/Models/Types/EnumType.cs
@@ -62,8 +62,8 @@ namespace AutoRest.CSharp.Output.Models.Types
         public CSharpType ValueType { get; }
         public bool IsExtensible { get; }
         public bool IsStringValueType => ValueType.Equals(typeof(string));
-        public bool IsIntValueType => ValueType.Equals(typeof(Int32)) || IsLongValueType;
-        public bool IsLongValueType => ValueType.Equals(typeof(Int64));
+        public bool IsIntValueType => ValueType.Equals(typeof(int)) || IsLongValueType;
+        public bool IsLongValueType => ValueType.Equals(typeof(long));
         public string? Description { get; }
         protected override string DefaultName { get; }
         protected override string DefaultAccessibility { get; }
