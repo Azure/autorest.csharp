f15107d551a31186ef53a3c4f70d8370f9a50df5
Alexander Sher <AlexanderSher@users.noreply.github.com>
2023-04-06 13:36:47 -0500

    Improve DateTimeOffset parsing for headers (#3204)

--- src/assets/Generator.Shared/ResponseHeadersExtensions.cs
+++ src/assets/Generator.Shared/ResponseHeadersExtensions.cs
@@ -12,6 +12,34 @@ namespace Azure.Core
 {
     internal static class ResponseHeadersExtensions
     {
+        private static readonly string[] KnownFormats =
+        {
+            // "r", // RFC 1123, required output format but too strict for input
+            "ddd, d MMM yyyy H:m:s 'GMT'", // RFC 1123 (r, except it allows both 1 and 01 for date and time)
+            "ddd, d MMM yyyy H:m:s 'UTC'", // RFC 1123, UTC
+            "ddd, d MMM yyyy H:m:s", // RFC 1123, no zone - assume GMT
+            "d MMM yyyy H:m:s 'GMT'", // RFC 1123, no day-of-week
+            "d MMM yyyy H:m:s 'UTC'", // RFC 1123, UTC, no day-of-week
+            "d MMM yyyy H:m:s", // RFC 1123, no day-of-week, no zone
+            "ddd, d MMM yy H:m:s 'GMT'", // RFC 1123, short year
+            "ddd, d MMM yy H:m:s 'UTC'", // RFC 1123, UTC, short year
+            "ddd, d MMM yy H:m:s", // RFC 1123, short year, no zone
+            "d MMM yy H:m:s 'GMT'", // RFC 1123, no day-of-week, short year
+            "d MMM yy H:m:s 'UTC'", // RFC 1123, UTC, no day-of-week, short year
+            "d MMM yy H:m:s", // RFC 1123, no day-of-week, short year, no zone
+
+            "dddd, d'-'MMM'-'yy H:m:s 'GMT'", // RFC 850
+            "dddd, d'-'MMM'-'yy H:m:s 'UTC'", // RFC 850, UTC
+            "dddd, d'-'MMM'-'yy H:m:s zzz", // RFC 850, offset
+            "dddd, d'-'MMM'-'yy H:m:s", // RFC 850 no zone
+            "ddd MMM d H:m:s yyyy", // ANSI C's asctime() format
+
+            "ddd, d MMM yyyy H:m:s zzz", // RFC 5322
+            "ddd, d MMM yyyy H:m:s", // RFC 5322 no zone
+            "d MMM yyyy H:m:s zzz", // RFC 5322 no day-of-week
+            "d MMM yyyy H:m:s", // RFC 5322 no day-of-week, no zone
+        };
+
         public static bool TryGetValue(this ResponseHeaders headers, string name, out byte[]? value)
         {
             if (headers.TryGetValue(name, out string? stringValue))
@@ -40,7 +68,16 @@ namespace Azure.Core
         {
             if (headers.TryGetValue(name, out string? stringValue))
             {
-                value = TypeFormatters.ParseDateTimeOffset(stringValue, "");
+                if (DateTimeOffset.TryParseExact(stringValue, "r", DateTimeFormatInfo.InvariantInfo, DateTimeStyles.None, out var dto) ||
+                    DateTimeOffset.TryParseExact(stringValue, KnownFormats, DateTimeFormatInfo.InvariantInfo, DateTimeStyles.AllowInnerWhite | DateTimeStyles.AssumeUniversal, out dto))
+                {
+                    value = dto;
+                }
+                else
+                {
+                    value = TypeFormatters.ParseDateTimeOffset(stringValue, "");
+                }
+
                 return true;
             }
 
