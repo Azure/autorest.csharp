65a6be479fb58099060ddae039f6179b5e181130
Arcturus Zhang <dapzhang@microsoft.com>
2023-04-12 14:50:34 +0800

    using clean up

--- src/AutoRest.CSharp/Mgmt/Generation/MgmtExtensionWrapperWriter.cs
+++ src/AutoRest.CSharp/Mgmt/Generation/MgmtExtensionWrapperWriter.cs
@@ -1,15 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System.Linq;
 using AutoRest.CSharp.Generation.Writers;
 using AutoRest.CSharp.Mgmt.Output;
-using AutoRest.CSharp.Output.Models;
-using AutoRest.CSharp.Output.Models.Shared;
-using AutoRest.CSharp.Utilities;
-using Azure.Core;
-using Azure.ResourceManager;
-using static AutoRest.CSharp.Output.Models.MethodSignatureModifiers;
 
 namespace AutoRest.CSharp.Mgmt.Generation
 {
