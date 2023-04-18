c4c5c6e4a02e19035d466a4b31f3bb13b0939f32
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-04-18 15:19:45 -0700

    Update core version

--- src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
+++ src/AutoRest.CSharp/Common/AutoRest/Plugins/CSharpProj.cs
@@ -33,7 +33,7 @@ namespace AutoRest.CSharp.AutoRest.Plugins
 ";
         private string _coreCsProjContent = @"
   <ItemGroup>
-    <PackageReference Include=""Azure.Core"" Version=""1.32.0-alpha.20230417.5"" />
+    <PackageReference Include=""Azure.Core"" Version=""1.31.0"" />
   </ItemGroup>";
 
         private string _armCsProjContent = @"
