84d8305c74a4b42aa83a954fb359b212044304a8
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-04-18 17:11:47 -0700

    regen

--- test/TestProjects/SubscriptionExtensions/Generated/LongRunningOperation/SubscriptionExtensionsArmOperationOfT.cs
+++ test/TestProjects/SubscriptionExtensions/Generated/LongRunningOperation/SubscriptionExtensionsArmOperationOfT.cs
@@ -34,7 +34,7 @@ namespace SubscriptionExtensions
         internal SubscriptionExtensionsArmOperation(IOperationSource<T> source, ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
         {
             var nextLinkOperation = NextLinkOperationImplementation.Create(source, pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
-            _operation = new OperationInternal<T>(clientDiagnostics, nextLinkOperation, response, "SubscriptionExtensionsArmOperation", fallbackStrategy: new ExponentialDelayStrategy());
+            _operation = new OperationInternal<T>(nextLinkOperation, clientDiagnostics, response, "SubscriptionExtensionsArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
         /// <inheritdoc />
