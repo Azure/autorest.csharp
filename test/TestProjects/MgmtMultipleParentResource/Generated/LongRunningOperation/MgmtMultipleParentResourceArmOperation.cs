43344cedf63dfb244315bc50125185066c7075a5
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-04-20 11:07:22 -0700

    Use DelayStrategy in LRO (#3281)
    * Use DelayStrategy in LRO
    
    * Update url temporarily
    
    * Fix shared sources
    
    * Update version
    
    * Update shared source
    
    * Update core version
    
    * regen

--- test/TestProjects/MgmtMultipleParentResource/Generated/LongRunningOperation/MgmtMultipleParentResourceArmOperation.cs
+++ test/TestProjects/MgmtMultipleParentResource/Generated/LongRunningOperation/MgmtMultipleParentResourceArmOperation.cs
@@ -34,7 +34,7 @@ namespace MgmtMultipleParentResource
         internal MgmtMultipleParentResourceArmOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response, OperationFinalStateVia finalStateVia)
         {
             var nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, finalStateVia);
-            _operation = new OperationInternal(clientDiagnostics, nextLinkOperation, response, "MgmtMultipleParentResourceArmOperation", fallbackStrategy: new ExponentialDelayStrategy());
+            _operation = new OperationInternal(nextLinkOperation, clientDiagnostics, response, "MgmtMultipleParentResourceArmOperation", fallbackStrategy: new SequentialDelayStrategy());
         }
 
         /// <inheritdoc />
