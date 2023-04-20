60ddd7a9cd5064de2781db60f5d17b1b1519baee
Alexander Sher <alexander.sher@outlook.com>
2023-04-20 11:03:35 -0500

    WIP

--- src/AutoRest.CSharp/Common/Output/Builders/ClientBuilder.cs
+++ src/AutoRest.CSharp/Common/Output/Builders/ClientBuilder.cs
@@ -30,9 +30,6 @@ namespace AutoRest.CSharp.Common.Output.Builders
                 ? $"The {clientPrefix} service client."
                 : BuilderHelpers.EscapeXmlDescription(description);
 
-        public static string GetClientPrefix(string name, BuildContext context)
-            => GetClientPrefix(name, context.DefaultName);
-
         public static string GetClientPrefix(string? name, string namespaceName)
         {
             name = string.IsNullOrEmpty(name) ? namespaceName : name.ToCleanName();
@@ -75,47 +72,6 @@ namespace AutoRest.CSharp.Common.Output.Builders
                     BuilderHelpers.EscapeXmlDescription(operation.Description),
                     new Diagnostic($"{declaration.Name}.{name}", Array.Empty<DiagnosticAttribute>()),
                     operation.Accessibility ?? "public");
-            }
-        }
-
-        /// <summary>
-        /// This function builds an enumerable of <see cref="PagingMethod"/> from an <see cref="OperationGroup"/> and a <see cref="RestClient"/>
-        /// </summary>
-        /// <param name="operationGroup">The OperationGroup to build methods from</param>
-        /// <param name="restClient">The corresponding RestClient to the operation group</param>
-        /// <param name="declaration">The type declaration options</param>
-        /// <param name="nameOverrider">A delegate used for overriding the name of output <see cref="ClientMethod"/></param>
-        /// <returns>An enumerable of <see cref="PagingMethod"/></returns>
-        public static IEnumerable<PagingMethod> BuildPagingMethods(OperationGroup operationGroup, CmcRestClient restClient, TypeDeclarationOptions Declaration,
-            Func<OperationGroup, Operation, RestClientMethod, string>? nameOverrider = default)
-        {
-            foreach (var operation in operationGroup.Operations)
-            {
-                Paging? paging = operation.Language.Default.Paging;
-                if (paging == null || operation.IsLongRunning)
-                {
-                    continue;
-                }
-
-                foreach (var serviceRequest in operation.Requests)
-                {
-                    RestClientMethod method = restClient.GetOperationMethod(serviceRequest);
-                    RestClientMethod? nextPageMethod = restClient.GetNextOperationMethod(serviceRequest);
-
-                    if (!(method.Responses.SingleOrDefault(r => r.ResponseBody != null)?.ResponseBody is ObjectResponseBody objectResponseBody))
-                    {
-                        throw new InvalidOperationException($"Method {method.Name} has to have a return value");
-                    }
-
-                    var name = nameOverrider?.Invoke(operationGroup, operation, method) ?? method.Name;
-
-                    yield return new PagingMethod(
-                        method,
-                        nextPageMethod,
-                        name,
-                        new Diagnostic($"{Declaration.Name}.{name}"),
-                        new PagingResponseInfo(paging.NextLinkName, paging.ItemName, objectResponseBody.Type));
-                }
             }
         }
     }
--- src/AutoRest.CSharp/Mgmt/Output/MgmtRestClient.cs
+++ src/AutoRest.CSharp/Mgmt/Output/MgmtRestClient.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using AutoRest.CSharp.Common.Input;
@@ -9,11 +8,9 @@ using AutoRest.CSharp.Input;
 using AutoRest.CSharp.Mgmt.AutoRest;
 using AutoRest.CSharp.Mgmt.Decorator;
 using AutoRest.CSharp.Output.Models;
-using AutoRest.CSharp.Output.Models.Requests;
 using AutoRest.CSharp.Output.Models.Shared;
 using AutoRest.CSharp.Output.Models.Types;
 using Azure.Core;
-using YamlDotNet.Core.Tokens;
 
 namespace AutoRest.CSharp.Mgmt.Output
 {
