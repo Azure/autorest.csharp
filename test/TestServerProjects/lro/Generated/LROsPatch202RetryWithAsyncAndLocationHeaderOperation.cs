8006bec6476c0b1cfe486527f1708bfac607f2b5
JoshLove-msft <54595583+JoshLove-msft@users.noreply.github.com>
2023-04-21 10:00:08 -0700

    Update dataplane LRO writer (#3298)
    * Update dataplane LRO writer
    
    * regenerate tests

--- test/TestServerProjects/lro/Generated/LROsPatch202RetryWithAsyncAndLocationHeaderOperation.cs
+++ test/TestServerProjects/lro/Generated/LROsPatch202RetryWithAsyncAndLocationHeaderOperation.cs
@@ -29,7 +29,7 @@ namespace lro
         internal LROsPatch202RetryWithAsyncAndLocationHeaderOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response)
         {
             IOperation<Product> nextLinkOperation = NextLinkOperationImplementation.Create(this, pipeline, request.Method, request.Uri.ToUri(), response, OperationFinalStateVia.Location);
-            _operation = new OperationInternal<Product>(clientDiagnostics, nextLinkOperation, response, "LROsPatch202RetryWithAsyncAndLocationHeaderOperation");
+            _operation = new OperationInternal<Product>(nextLinkOperation, clientDiagnostics, response, "LROsPatch202RetryWithAsyncAndLocationHeaderOperation");
         }
 
         /// <inheritdoc />
--- test/TestServerProjects/lro/Generated/LrosaDsDelete204SucceededOperation.cs
+++ test/TestServerProjects/lro/Generated/LrosaDsDelete204SucceededOperation.cs
@@ -27,7 +27,7 @@ namespace lro
         internal LrosaDsDelete204SucceededOperation(ClientDiagnostics clientDiagnostics, HttpPipeline pipeline, Request request, Response response)
         {
             IOperation nextLinkOperation = NextLinkOperationImplementation.Create(pipeline, request.Method, request.Uri.ToUri(), response, OperationFinalStateVia.Location);
-            _operation = new OperationInternal(clientDiagnostics, nextLinkOperation, response, "LrosaDsDelete204SucceededOperation");
+            _operation = new OperationInternal(nextLinkOperation, clientDiagnostics, response, "LrosaDsDelete204SucceededOperation");
         }
 
         /// <inheritdoc />
