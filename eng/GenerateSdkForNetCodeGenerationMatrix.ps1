b0b2bb22d67e6b79c159e1c779271852409c0b99
Patrick Hallisey <pahallis@microsoft.com>
2023-04-27 12:14:08 -0700

    Add dynamic matrix generation for sdk-for-net

---
+++ eng/GenerateSdkForNetCodeGenerationMatrix.ps1
@@ -0,0 +1,106 @@
+[CmdLetBinding()]
+param (
+  [Parameter()]
+  [string]$SdkForNetPath,
+    
+  [Parameter()]
+  [int]$GroupCount,
+
+  [Parameter()]
+  [string]$OutputFolder
+)
+
+# Divide the items into groups of approximately equal size.
+function Split-Items([array]$Items) {
+  # given $Items.Length = 22 and $GroupCount = 5
+  # then $itemsPerGroup = 4
+  # and $largeGroupCount = 2
+  # and $group.Length = 5, 5, 4, 4, 4
+  $itemsPerGroup = [math]::Floor($Items.Length / $GroupCount)
+  $largeGroupCount = $Items.Length % $itemsPerGroup
+  $groups = [object[]]::new($GroupCount)
+
+  $i = 0
+  for($g = 0;$g -lt $GroupCount;$g++) {
+    $groupLength = if($g -lt $largeGroupCount) { $itemsPerGroup + 1 } else { $itemsPerGroup }
+    $group = [string[]]::new($groupLength)
+    $groups[$g] = $group
+    for($gi = 0;$gi -lt $groupLength;$gi++) {
+      $group[$gi] = $Items[$i++]
+    }
+  }
+
+  return ,$groups
+}
+
+# Write each project group into a props file and return the array of generated project file paths
+function New-PropsFiles($ProjectGroups, $PropsFilePrefix) {
+  if (!$ProjectGroups -or !$ProjectGroups[0]) {
+    Throw "There should be some project files in the group. Please check the given project list."
+  }
+
+  $numOfGroups = $ProjectGroups.Count
+
+  for ($i = 0; $i -lt $numOfGroups; $i++) {
+    $propsFilePath = "$PropsFilePrefix$i.props"
+    $filePath = Join-Path $OutputFolder $propsFilePath
+    
+    $itemGroupNode = [Xml.Linq.XElement]'<ItemGroup />'
+
+    foreach($projectPath in $ProjectGroups[$i]) {
+      $newElemAttr = [Xml.Linq.XAttribute]::new('Include', $projectPath)
+      $newElem = [Xml.Linq.XElement]::new('ProjectReference', $newElemAttr)
+      $itemGroupNode.Add($newElem)
+    }
+
+    $projectNode = [Xml.Linq.XElement]::new('Project', $itemGroupNode)
+
+    $projectNode.ToString() | Out-File $filePath
+
+    Write-Host "$propsFilePath`:`n$($projectNode.ToString())`n"
+    Write-Output $propsFilePath
+  }
+}
+
+function Get-ProjectsWithAutorest() {
+  $projectsFilePath = "$SdkForNetPath/artifacts/projects.txt"
+  Remove-Item $projectsFilePath -Force -ErrorAction SilentlyContinue
+
+  Push-Location $SdkForNetPath
+  
+  dotnet build "$SdkForNetPath/eng/service.proj" `
+    /t:GetCodeGenProjects `
+    /p:IncludeTests=false `
+    /p:IncludePerf=false `
+    /p:IncludeStress=false `
+    /p:OutputProjectFilePath=$projectsFilePath | Out-Host
+
+  Pop-Location
+
+  $projects = Get-Content $projectsFilePath | Where-Object { $_ }
+
+  return ,$projects
+}
+
+function New-Matrix([array]$PropsFiles) {
+  $matrix = [ordered]@{}
+  for($i=0;$i -lt $PropsFiles.Length;$i++) {
+    $matrix["Set_$i"] = @{ 'ProjectListOverrideFile' = $PropsFiles[$i] }
+  }
+  return $matrix
+}
+
+function Write-JsonVariable($VariableName, $Value) {
+  $compressed = ConvertTo-Json $Value -Depth 100 -Compress
+  Write-Output "##vso[task.setVariable variable=$VariableName;isOutput=true]$compressed"
+}
+
+New-Item -Path $OutputFolder -ItemType "directory" -Force | Out-Null
+
+$projects = Get-ProjectsWithAutorest
+$projectGroups = Split-Items -Items $projects
+$propsFiles = New-PropsFiles -ProjectGroups $projectGroups -PropsFilePrefix 'projects_'
+$matrix = New-Matrix -PropsFiles $propsFiles
+Write-JsonVariable "matrix" $matrix
+
+Write-Host "Matrix:`n$(ConvertTo-Json $matrix -Depth 100)"
