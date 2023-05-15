a18fc0599f8396fbdd002bb721fd1d4b73725ac3
Pan Shao <pashao@microsoft.com>
2023-05-15 20:46:37 +0800

    Update

---
+++ eng/RemoveInstalledPackages.ps1
@@ -0,0 +1,14 @@
+$rootPath = Resolve-Path (Join-Path $PSScriptRoot '..')
+
+$packageLockPath = Join-Path $rootPath "package-lock.json"
+Remove-Item $packageLockPath
+
+$rootPackagePath = Join-Path $rootPath "npm_modules"
+if (Test-Path $rootPackagePath) {
+    Remove-Item $rootPackagePath -Recurse
+}
+
+$emitterPackagePath = Join-Path $rootPath "src" "CADL.Extension" "Emitter.Csharp" "node_modules"
+if (Test-Path $emitterPackagePath) {
+    Remove-Item $emitterPackagePath -Recurse
+}
