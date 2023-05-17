72646e22ce8e0007e832450e14866448adade1bb
Arcturus Zhang <dapzhang@microsoft.com>
2023-05-17 17:36:38 +0800

    fix test cases

--- test/CadlRanchProjects.Tests/LiteralValueTypeTests.cs
+++ test/CadlRanchProjects.Tests/LiteralValueTypeTests.cs
@@ -17,22 +17,26 @@ namespace CadlRanchProjects.Tests
         [Test]
         public async Task LiteralModelProperties() => await Test(async (host) =>
         {
-            Thing result = await new TypeSpecFirstTestClient(host).CreateLiteralAsync(new Thing("test", "test", "abc"));
+            Thing result = await new TypeSpecFirstTestClient(host).CreateLiteralAsync(new Thing("test", "test", "abc")
+            {
+                OptionalLiteralString = ThingOptionalLiteralString.Reject,
+                OptionalLiteralInt = ThingOptionalLiteralInt._456,
+                OptionalLiteralDouble = ThingOptionalLiteralDouble._456,
+                OptionalLiteralBool = true,
+            });
             Assert.AreEqual("literal", result.Name);
             Assert.AreEqual("union", result.RequiredUnion);
             Assert.AreEqual("def", result.RequiredBadDescription);
             // the type of the following properties are literal types, but if the server returns different value (for instance there is a service update but the SDK does not update, they are actually no longer literal types)
             // but nevertheless, the SDK should still be able to get the new values
-            Assert.AreEqual("reject", result.RequiredLiteralString);
-            Assert.AreEqual(12345, result.RequiredLiteralInt);
-            Assert.AreEqual(123.45, result.RequiredLiteralDouble);
+            Assert.AreEqual(new ThingRequiredLiteralString("reject"), result.RequiredLiteralString);
+            Assert.AreEqual(new ThingRequiredLiteralInt(12345), result.RequiredLiteralInt);
+            Assert.AreEqual(new ThingRequiredLiteralDouble(123.45f), result.RequiredLiteralDouble);
             Assert.AreEqual(true, result.RequiredLiteralBool);
-            Assert.AreEqual("accept", result.OptionalLiteralString);
-            Assert.AreEqual(12345, result.OptionalLiteralInt);
-            Assert.AreEqual(123.45, result.OptionalLiteralDouble);
+            Assert.AreEqual(new ThingOptionalLiteralString("accept"), result.OptionalLiteralString);
+            Assert.AreEqual(new ThingOptionalLiteralInt(12345), result.OptionalLiteralInt);
+            Assert.AreEqual(new ThingOptionalLiteralDouble(123.45f), result.OptionalLiteralDouble);
             Assert.AreEqual(false, result.OptionalLiteralBool);
         });
-
     }
-
 }
