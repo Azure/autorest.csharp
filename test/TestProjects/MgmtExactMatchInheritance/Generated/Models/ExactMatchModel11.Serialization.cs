b238a1b948503b4ccb915ec65b90137385de03ae
FAREAST\chunyu <chunyu@microsoft.com>
2023-06-16 17:04:41 +0800

    update test

--- test/TestProjects/MgmtExactMatchInheritance/Generated/Models/ExactMatchModel11.Serialization.cs
+++ test/TestProjects/MgmtExactMatchInheritance/Generated/Models/ExactMatchModel11.Serialization.cs
@@ -38,7 +38,7 @@ namespace MgmtExactMatchInheritance.Models
                 }
                 if (property.NameEquals("type"u8))
                 {
-                    if (property.Value.ValueKind == JsonValueKind.Null)
+                    if (property.Value.ValueKind == JsonValueKind.Null || property.Value.ValueKind == JsonValueKind.String && property.Value.GetString().Length == 0)
                     {
                         continue;
                     }
