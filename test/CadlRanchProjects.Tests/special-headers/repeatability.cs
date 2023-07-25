9406eb8ffc5347ea07455e0e0e6d360cfc03e3b9
archerzz <archerzz@gmail.com>
2023-07-25 14:35:07 +0800

    test(cadl-ranch): add trait repeatability test
    follow up of #1950 and https://github.com/Azure/cadl-ranch/issues/378

--- test/CadlRanchProjects.Tests/special-headers/repeatability.cs
+++ test/CadlRanchProjects.Tests/special-headers/repeatability.cs
@@ -2,12 +2,15 @@
 // Licensed under the MIT License.
 
 using SpecialHeaders.Repeatability;
+using _Specs_.Azure.Core.Traits;
 using AutoRest.TestServer.Tests.Infrastructure;
 using Azure;
 using NUnit.Framework;
 using System.Threading.Tasks;
 using System.Linq;
 using System.Reflection;
+using _Specs_.Azure.Core.Traits.Models;
+using System;
 
 namespace CadlRanchProjects.Tests
 {
@@ -23,9 +26,22 @@ namespace CadlRanchProjects.Tests
         });
 
         [Test]
-        public void RepeatabilityHeadersNotInMethodSignature()
+        public Task RepeatableAction() => Test(async (host) =>
         {
-            foreach (var m in typeof(RepeatabilityClient).GetMethods(BindingFlags.Public | BindingFlags.Instance).Where(m => m.Name.StartsWith("ImmediateSuccess")))
+            var response = await new TraitsClient(host, null).RepeatableActionAsync(1, new UserActionParam("test"));
+            Assert.AreEqual(200, response.GetRawResponse().Status);
+            Assert.IsTrue(response.GetRawResponse().Headers.TryGetValue("repeatability-result", out var headerValue));
+            Assert.AreEqual("accepted", headerValue);
+            Assert.AreEqual("test", response.Value.UserActionResult);
+        });
+
+        [TestCase(typeof(RepeatabilityClient), "ImmediateSuccess")]
+        [TestCase(typeof(TraitsClient), "RepeatableAction")]
+        public void RepeatabilityHeadersNotInMethodSignature(Type clientType, string methodName)
+        {
+            var methods = clientType.GetMethods(BindingFlags.Public | BindingFlags.Instance).Where(m => m.Name.StartsWith(methodName));
+            Assert.IsNotEmpty(methods);
+            foreach (var m in methods)
             {
                 Assert.False(m.GetParameters().Any(p => p.Name == "repeatabilityRequestId" || p.Name == "repeatabilityFirstSent"));
             }
