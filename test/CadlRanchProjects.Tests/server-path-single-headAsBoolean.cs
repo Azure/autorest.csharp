dbdf4c7a1fce40ea25aaaa89752a78b9566136db
FAREAST\chunyu <chunyu@microsoft.com>
2023-08-08 17:30:17 +0800

    update test

--- test/CadlRanchProjects.Tests/server-path-single-headAsBoolean.cs
+++ test/CadlRanchProjects.Tests/server-path-single-headAsBoolean.cs
@@ -5,13 +5,12 @@ using System.Threading.Tasks;
 
 namespace CadlRanchProjects.Tests
 {
-    public class ServerPathSingleTests : CadlRanchTestBase
+    public class ServerPathSingleHeadAsBooleanTests : CadlRanchTestBase
     {
         [Test]
         public Task Server_Path_Single_HeadAsBoolean_myOp() => Test(async (host) =>
         {
             var response = await new SingleClient(host, null).MyOpAsync();
-            Assert.AreEqual(200, response.Status);
             Assert.AreEqual(true, response.Value);
         });
     }
