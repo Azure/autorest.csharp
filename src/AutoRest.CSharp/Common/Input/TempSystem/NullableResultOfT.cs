ea958db79066e79c1c03d8b349f797a74a8952b3
m-nash <prognash@gmail.com>
2023-09-19 20:56:03 -0700

    rename temp folder so its included

---
+++ src/AutoRest.CSharp/Common/Input/TempSystem/NullableResultOfT.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+
+namespace System.ServiceModel.Rest
+{
+#pragma warning disable SA1649 // File name should match first type name
+    public class NullableResult<T>
+#pragma warning restore SA1649 // File name should match first type name
+    {
+        private T? _value;
+        private Result _result;
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <param result=""></param>
+        public NullableResult(T? value, Result result)
+        {
+            _value = value;
+            _result = result;
+        }
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        public virtual T? Value => _value;
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public virtual bool HasValue => _value != null;
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <returns></returns>
+        public virtual Result GetRawResult() => _result;
+    }
+}
---
+++ src/AutoRest.CSharp/Common/Input/TempSystem/ResultOfT.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.ComponentModel;
+
+namespace System.ServiceModel.Rest
+{
+#pragma warning disable SA1649 // File name should match first type name
+    public class Result<T> : NullableResult<T>
+#pragma warning restore SA1649 // File name should match first type name
+    {
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <param result=""></param>
+        public Result(T value, Result result) : base(value, result)
+        {
+        }
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        public override T Value => base.Value!;
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool HasValue => true;
+
+        /// <summary>
+        /// TBD.
+        /// </summary>
+        /// <returns></returns>
+        public override Result GetRawResult() => base.GetRawResult();
+    }
+}
