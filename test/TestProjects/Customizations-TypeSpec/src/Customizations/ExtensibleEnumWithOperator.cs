31cfb61f233540f0193efe25bf35c92dd9051a98
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-09-21 14:23:21 +0800

    update

--- test/TestProjects/Customizations-TypeSpec/src/Customizations/ExtensibleEnumWithOperator.cs
+++ test/TestProjects/Customizations-TypeSpec/src/Customizations/ExtensibleEnumWithOperator.cs
@@ -11,11 +11,11 @@ namespace CustomizationsInTsp.Models
     /// <summary> Extensible enum to customize operator. </summary>
     public readonly partial struct ExtensibleEnumWithOperator : IEquatable<ExtensibleEnumWithOperator>
     {
-        /// <summary> Determines if two <see cref="ExtensibleEnumWithOperator"/> values are the same. </summary>
+        /// <summary> Operator customization with new implematation. </summary>
         public static bool operator ==(ExtensibleEnumWithOperator left, ExtensibleEnumWithOperator right) => left.Equals(ExtensibleEnumWithOperator.Monday);
-        /// <summary> Determines if two <see cref="ExtensibleEnumWithOperator"/> values are not the same. </summary>
+        /// <summary> Operator customization with new implematation. </summary>
         public static bool operator !=(ExtensibleEnumWithOperator left, ExtensibleEnumWithOperator right) => !left.Equals(ExtensibleEnumWithOperator.Monday);
-        /// <summary> Converts a string to a <see cref="ExtensibleEnumWithOperator"/>. </summary>
+        /// <summary> Conversion customization with new implematation. </summary>
         public static implicit operator ExtensibleEnumWithOperator(string value) => new ExtensibleEnumWithOperator("Monday");
     }
 }
