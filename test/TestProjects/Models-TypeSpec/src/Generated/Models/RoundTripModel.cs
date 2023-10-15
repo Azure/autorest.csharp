dfb34e70b11b7a3a8d2b1b08b11c0ffe438e82d9
Alexander Sher <alexander.sher@outlook.com>
2023-10-15 16:10:18 -0400

    Fix AdditionalProperties doubling

--- test/TestProjects/Models-TypeSpec/src/Generated/Models/RoundTripModel.cs
+++ test/TestProjects/Models-TypeSpec/src/Generated/Models/RoundTripModel.cs
@@ -229,7 +229,7 @@ namespace ModelsTypeSpec.Models
         /// To assign an object to this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
         /// </para>
         /// <para>
-        /// To assign an already formated json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
         /// </para>
         /// <para>
         /// Examples:
@@ -260,7 +260,7 @@ namespace ModelsTypeSpec.Models
         /// To assign an object to this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
         /// </para>
         /// <para>
-        /// To assign an already formated json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
         /// </para>
         /// <para>
         /// Examples:
