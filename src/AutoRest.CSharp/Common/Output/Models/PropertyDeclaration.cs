ea9e7fd66e1f86794646932a7a79f31b6c84508f
Arcturus Zhang <dapzhang@microsoft.com>
2023-11-20 10:40:59 +0800

    initialize property declaration

---
+++ src/AutoRest.CSharp/Common/Output/Models/PropertyDeclaration.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System;
+using AutoRest.CSharp.Common.Output.Expressions.ValueExpressions;
+using AutoRest.CSharp.Generation.Types;
+
+namespace AutoRest.CSharp.Output.Models
+{
+    internal record PropertyDeclaration(FormattableString? Description, FieldModifiers Modifiers, CSharpType PropertyType, string Name, ValueExpression? InitializationValue, FieldModifiers? Get = null, FieldModifiers? Set = null)
+    {
+    }
+}
