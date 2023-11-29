c8dcd9303f041cd90a2e9e498f32352d62ec4e62
pshao25 <97225342+pshao25@users.noreply.github.com>
2023-11-29 11:10:34 +0800

    Remove flag `use-overloads-between-protocol-and-convenience`

--- test/AutoRest.TestServerLowLevel.Tests/LowLevel/Generation/ModelGenerationTestBase.cs
+++ test/AutoRest.TestServerLowLevel.Tests/LowLevel/Generation/ModelGenerationTestBase.cs
@@ -54,7 +54,6 @@ namespace AutoRest.CSharp.Generation.Writers.Tests
                 useCoreDataFactoryReplacements: true,
                 modelFactoryForHlc: Array.Empty<string>(),
                 unreferencedTypesHandling: Configuration.UnreferencedTypesHandlingOption.RemoveOrInternalize,
-                useOverloadsBetweenProtocolAndConvenience: true,
                 keepNonOverloadableProtocolSignature: false,
                 projectFolder: ".",
                 existingProjectFolder: null,
