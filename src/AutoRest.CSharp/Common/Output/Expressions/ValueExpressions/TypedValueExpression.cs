998714facbd551e2fa83bd02fafd6e587139e318
Arcturus Zhang <dapzhang@microsoft.com>
2023-11-30 13:37:41 +0800

    resolve comments and update

--- src/AutoRest.CSharp/Common/Output/Expressions/ValueExpressions/TypedValueExpression.cs
+++ src/AutoRest.CSharp/Common/Output/Expressions/ValueExpressions/TypedValueExpression.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using AutoRest.CSharp.Common.Output.Models;
 using AutoRest.CSharp.Generation.Types;
 using AutoRest.CSharp.Output.Models;
 using AutoRest.CSharp.Output.Models.Shared;
