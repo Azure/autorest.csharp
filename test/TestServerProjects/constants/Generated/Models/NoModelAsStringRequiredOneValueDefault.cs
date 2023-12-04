904b3206f39cbcbb6a9fcacdd0988636a2046fa5
ArcturusZhang <dapzhang@microsoft.com>
2023-12-04 14:14:01 +0800

    enable the flag on all test server projects

--- test/TestServerProjects/constants/Generated/Models/NoModelAsStringRequiredOneValueDefault.cs
+++ test/TestServerProjects/constants/Generated/Models/NoModelAsStringRequiredOneValueDefault.cs
@@ -5,11 +5,46 @@
 
 #nullable disable
 
+using System;
+using System.Collections.Generic;
+
 namespace constants.Models
 {
     /// <summary> The NoModelAsStringRequiredOneValueDefault. </summary>
     internal partial class NoModelAsStringRequiredOneValueDefault
     {
+        /// <summary>
+        /// Keeps track of any properties unknown to the library.
+        /// <para>
+        /// To assign an object to the value of this property use <see cref="BinaryData.FromObjectAsJson{T}(T, System.Text.Json.JsonSerializerOptions?)"/>.
+        /// </para>
+        /// <para>
+        /// To assign an already formatted json string to this property use <see cref="BinaryData.FromString(string)"/>.
+        /// </para>
+        /// <para>
+        /// Examples:
+        /// <list type="bullet">
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson("foo")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("\"foo\"")</term>
+        /// <description>Creates a payload of "foo".</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromObjectAsJson(new { key = "value" })</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// <item>
+        /// <term>BinaryData.FromString("{\"key\": \"value\"}")</term>
+        /// <description>Creates a payload of { "key": "value" }.</description>
+        /// </item>
+        /// </list>
+        /// </para>
+        /// </summary>
+        private IDictionary<string, BinaryData> _serializedAdditionalRawData;
+
         /// <summary> Initializes a new instance of <see cref="NoModelAsStringRequiredOneValueDefault"/>. </summary>
         internal NoModelAsStringRequiredOneValueDefault()
         {
@@ -18,9 +53,11 @@ namespace constants.Models
 
         /// <summary> Initializes a new instance of <see cref="NoModelAsStringRequiredOneValueDefault"/>. </summary>
         /// <param name="parameter"></param>
-        internal NoModelAsStringRequiredOneValueDefault(NoModelAsStringRequiredOneValueDefaultEnum parameter)
+        /// <param name="serializedAdditionalRawData"> Keeps track of any properties unknown to the library. </param>
+        internal NoModelAsStringRequiredOneValueDefault(NoModelAsStringRequiredOneValueDefaultEnum parameter, IDictionary<string, BinaryData> serializedAdditionalRawData)
         {
             Parameter = parameter;
+            _serializedAdditionalRawData = serializedAdditionalRawData;
         }
 
         /// <summary> Gets the parameter. </summary>
