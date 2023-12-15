de010c176a06d30df534be0f19206c0df3092e99
Arcturus Zhang <dapzhang@microsoft.com>
2023-12-15 14:00:28 +0800

    revert a change not suitable in this class

--- src/AutoRest.CSharp/Common/Input/InputTypes/Serialization/TypeSpecInputModelTypeConverter.cs
+++ src/AutoRest.CSharp/Common/Input/InputTypes/Serialization/TypeSpecInputModelTypeConverter.cs
@@ -108,7 +108,6 @@ namespace AutoRest.CSharp.Common.Input
             }
 
             var derivedModels = new List<InputModelType>();
-            // TODO -- deserialize the dictionary type
             var model = new InputModelType(name, ns, accessibility, deprecated, description, usage, properties, baseModel, derivedModels, discriminatorValue, discriminatorPropertyValue, IsNullable: isNullable)
             {
                 IsAnonymousModel = isAnonymousModel
