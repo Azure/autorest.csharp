2d807fc45c1b63f4dc06f2b0f27a06e3a8c745d4
Crystal YU <chunyu@microsoft.com>
2023-12-25 11:40:40 +0800

    calculate the usage of the LRO response type (#4065)
    * calculate the usage of the LRO response type
    
    * regen test
    
    * add test
    
    * resolve comment
    
    * add unit test
    
    * refine
    
    * regen test

--- src/TypeSpec.Extension/Emitter.Csharp/src/lib/model.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/lib/model.ts
@@ -1,7 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { isFixed } from "@azure-tools/typespec-azure-core";
+import { getLroMetadata, isFixed } from "@azure-tools/typespec-azure-core";
 import {
     EncodeData,
     Enum,
@@ -906,6 +906,26 @@ export function getUsages(
                     appendUsage(name, UsageFlags.Output);
                 }
             }
+            /* calculate the usage of the LRO result type. */
+            const metadata = getLroMetadata(program, op.operation);
+            if (metadata !== undefined) {
+                let bodyType: Model;
+                if (
+                    op.verb !== "delete" &&
+                    metadata.finalResult !== undefined &&
+                    metadata.finalResult !== "void"
+                ) {
+                    bodyType = metadata.finalEnvelopeResult as Model;
+                    if (bodyType) {
+                        getAllEffectedModels(
+                            bodyType,
+                            new Set<string>()
+                        ).forEach((element) => {
+                            affectedReturnTypes.add(element);
+                        });
+                    }
+                }
+            }
         }
     }
 
@@ -971,6 +991,16 @@ export function getUsages(
             for (const [_, prop] of model.properties) {
                 if (prop.type.kind === "Model") {
                     result.push(...getAllEffectedModels(prop.type, visited));
+                }
+            }
+            /*propagate usage to the property type of the base model. */
+            if (model.baseModel) {
+                for (const [_, prop] of model.baseModel.properties) {
+                    if (prop.type.kind === "Model") {
+                        result.push(
+                            ...getAllEffectedModels(prop.type, visited)
+                        );
+                    }
                 }
             }
         }
