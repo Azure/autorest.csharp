94f1fb90c3c8840350ecccbf5f5b0bfd40b1068f
ArcturusZhang <dapzhang@microsoft.com>
2023-12-28 16:25:23 +0800

    regen

--- test/TestServerProjects/xms-error-responses/Generated/Models/PetActionError.cs
+++ test/TestServerProjects/xms-error-responses/Generated/Models/PetActionError.cs
@@ -12,10 +12,10 @@ namespace xms_error_responses.Models
     /// Please note <see cref="PetActionError"/> is the base class. According to the scenario, a derived class of the base class might need to be assigned here, or this property needs to be casted to one of the possible derived classes.
     /// The available derived classes include <see cref="PetHungryOrThirstyError"/> and <see cref="PetSadError"/>.
     /// </summary>
-    public partial class PetActionError : PetAction
+    public abstract partial class PetActionError : PetAction
     {
         /// <summary> Initializes a new instance of <see cref="PetActionError"/>. </summary>
-        internal PetActionError()
+        protected PetActionError()
         {
         }
 
