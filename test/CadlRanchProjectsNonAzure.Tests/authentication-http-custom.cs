14200b5c0e37ecba95e95a32b42de511c1ce8716
pshao25 <97225342+pshao25@users.noreply.github.com>
2024-01-04 17:09:15 +0800

    update

---
+++ test/CadlRanchProjectsNonAzure.Tests/authentication-http-custom.cs
@@ -0,0 +1,18 @@
+﻿using System.Net.ClientModel;
+using System.Threading.Tasks;
+using Authentication.Http.Custom;
+using AutoRest.TestServer.Tests.Infrastructure;
+using NUnit.Framework;
+
+namespace CadlRanchProjectsNonAzure.Tests
+{
+    public class AuthenticationHttpCustomTests: CadlRanchNonAzureTestBase
+    {
+        [Test]
+        public Task Authentication_Http_Custom_valid() => Test(async (host) =>
+        {
+            Result result = await new AuthenticationClient(host, new KeyCredential("valid-key"), null).GetHttpClient().GetHttpCustomClient().ValidAsync();
+            Assert.AreEqual(204, result.GetRawResponse().Status);
+        });
+    }
+}
