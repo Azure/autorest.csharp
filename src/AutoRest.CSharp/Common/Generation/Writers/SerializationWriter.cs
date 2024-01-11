599c9a90c27d5b2222fedd5acf01f5c3745fe95a
Wei Hu <huwe@microsoft.com>
2024-01-11 21:28:26 +0800

    merge

--- src/AutoRest.CSharp/Common/Generation/Writers/SerializationWriter.cs
+++ src/AutoRest.CSharp/Common/Generation/Writers/SerializationWriter.cs
@@ -24,6 +24,12 @@ namespace AutoRest.CSharp.Generation.Writers
         {
             switch (schema)
             {
+                // The corresponding ResoruceData does not exist for PartialResource, so we do not need to generate serialization methods for it.
+                case PartialResource:
+                    break;
+                case Resource resource:
+                    WriteResourceJsonSerialization(writer, resource);
+                    break;
                 case SerializableObjectType obj:
                     if (obj.IncludeSerializer || obj.IncludeDeserializer)
                     {
@@ -33,6 +39,25 @@ namespace AutoRest.CSharp.Generation.Writers
                 case EnumType { IsExtensible: false } sealedChoiceSchema:
                     WriteEnumSerialization(writer, sealedChoiceSchema);
                     break;
+            }
+        }
+
+        private void WriteResourceJsonSerialization(CodeWriter writer, Resource resource)
+        {
+            var declaration = resource.Declaration;
+
+            using (writer.Namespace(declaration.Namespace))
+            {
+                var resourceDataType = resource.ResourceData.Type;
+                writer.Append($"{declaration.Accessibility} partial class {declaration.Name} : IJsonModel<{resourceDataType}>");
+
+                using (writer.Scope())
+                {
+                    foreach (var method in JsonSerializationMethodsBuilder.BuildResourceJsonSerializationMethods(resource))
+                    {
+                        writer.WriteMethod(method);
+                    }
+                }
             }
         }
 
