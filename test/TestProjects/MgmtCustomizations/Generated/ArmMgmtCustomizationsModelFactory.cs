c9dcfa9b2d62fb0a1ca08e6d816faf4a6c347e7b
Arcturus Zhang <dapzhang@microsoft.com>
2024-02-02 13:07:21 +0800

    regen after code changes

--- test/TestProjects/MgmtCustomizations/Generated/ArmMgmtCustomizationsModelFactory.cs
+++ test/TestProjects/MgmtCustomizations/Generated/ArmMgmtCustomizationsModelFactory.cs
@@ -6,7 +6,9 @@
 #nullable disable
 
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using Azure.Core;
 using Azure.ResourceManager.Models;
 using MgmtCustomizations;
