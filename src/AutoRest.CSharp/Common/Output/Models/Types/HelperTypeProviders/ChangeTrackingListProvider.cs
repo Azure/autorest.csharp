bd61c5386f6ca306f969a4628ece2ebb47b64f12
m-nash <64171366+m-nash@users.noreply.github.com>
2024-02-28 10:41:48 -0800

    wip

---
+++ src/AutoRest.CSharp/Common/Output/Models/Types/HelperTypeProviders/ChangeTrackingListProvider.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using AutoRest.CSharp.Input.Source;
+using AutoRest.CSharp.Output.Models.Types;
+
+namespace AutoRest.CSharp.Common.Output.Models.Types
+{
+    internal class ChangeTrackingListProvider : ExpressionTypeProvider
+    {
+        public ChangeTrackingListProvider(string defaultNamespace, SourceInputModel? sourceInputModel)
+            : base(defaultNamespace, sourceInputModel)
+        {
+        }
+
+        protected override string DefaultName => "ChangeTrackingList";
+
+        protected override string DefaultAccessibility => "internal";
+    }
+}
