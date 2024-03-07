4537c21d8e02b96f91c92bd6db718d31f95d765d
FAREAST\chunyu <chunyu@microsoft.com>
2024-03-07 20:20:20 +0800

    support azureLocation

---
+++ src/AutoRest.CSharp/Common/Output/Expressions/KnownValueExpressions/AzureLocationExpression.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoRest.CSharp.Common.Output.Expressions.ValueExpressions;
+using Azure.Core;
+using Microsoft.CodeAnalysis.Operations;
+
+namespace AutoRest.CSharp.Common.Output.Expressions.KnownValueExpressions
+{
+    internal sealed record AzureLocationExpression(ValueExpression Untyped) : TypedValueExpression<AzureLocation>(Untyped)
+    {
+    }
+}
