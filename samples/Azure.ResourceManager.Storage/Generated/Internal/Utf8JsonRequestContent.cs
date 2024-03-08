ea6dcf8e1fa9a252fa012520726210b75a8db353
ArcturusZhang <dapzhang@microsoft.com>
2024-03-08 10:15:06 +0800

    regen

--- samples/Azure.ResourceManager.Storage/Generated/Internal/Utf8JsonRequestContent.cs
+++ samples/Azure.ResourceManager.Storage/Generated/Internal/Utf8JsonRequestContent.cs
@@ -11,7 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
 
-namespace custom_baseUrl_paging
+namespace Azure.ResourceManager.Storage
 {
     internal class Utf8JsonRequestContent : RequestContent
     {
--- samples/CognitiveServices.TextAnalytics/Generated/Internal/Utf8JsonRequestContent.cs
+++ samples/CognitiveServices.TextAnalytics/Generated/Internal/Utf8JsonRequestContent.cs
@@ -11,7 +11,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Azure.Core;
 
-namespace custom_baseUrl_paging
+namespace CognitiveServices.TextAnalytics
 {
     internal class Utf8JsonRequestContent : RequestContent
     {
