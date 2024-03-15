c87d34e12bdaea9050e655e70035bfff7fe94c34
Pan Shao <97225342+pshao25@users.noreply.github.com>
2024-03-15 12:02:47 +0800

    Support extensible enum value type as a type (#4382)
    * Support extensible enum value type as a type
    
    * update
    
    * update

---
+++ test/CadlRanchProjects.Tests/server-endpoint-notDefined.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using AutoRest.TestServer.Tests.Infrastructure;
+using NUnit.Framework;
+using Server.Endpoint.NotDefined;
+
+namespace CadlRanchProjects.Tests
+{
+    public class ServerEndpointNotDefinedTests : CadlRanchTestBase
+    {
+        [Test]
+        public Task Server_Endpoint_NotDefined_valid() => Test(async (host) =>
+        {
+            var response = await new NotDefinedClient(host, null).ValidAsync();
+            Assert.AreEqual(200, response.Status);
+        });
+    }
+}
