4b61f95d0ecf187e772e14fe7a39c9d164c83ea6
Alexander Sher <AlexanderSher@users.noreply.github.com>
2024-03-21 10:50:10 -0400

    Add derived types list to the property description in DPG (#4436)

--- src/AutoRest.CSharp/Common/Output/Models/Types/ModelTypeProvider.cs
+++ src/AutoRest.CSharp/Common/Output/Models/Types/ModelTypeProvider.cs
@@ -358,16 +358,7 @@ namespace AutoRest.CSharp.Output.Models.Types
                 baseInitializers = baseParameterInitializers;
             }
 
-            // [TODO]: Add derived types list to the property description in DPG
-            if (Configuration.Generation1ConvenienceClient)
-            {
-                parameterList.AddRange(parameters.Select(p => p with { Description = $"{p.Description}{BuilderHelpers.CreateDerivedTypesDescription(p.Type)}" }));
-            }
-            else
-            {
-                parameterList.AddRange(parameters);
-            }
-
+            parameterList.AddRange(parameters.Select(p => p with { Description = $"{p.Description}{BuilderHelpers.CreateDerivedTypesDescription(p.Type)}" }));
             fullParameterList = parameterList.DistinctBy(p => p.Name).ToArray(); // we filter out the parameters with the same name since we might have the same property both in myself and my base.
         }
 
