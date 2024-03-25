7956d01015a376aa194e6a1a4f7fc0afc5a5fcf2
mcgallan <1186003288@qq.com>
2024-03-25 16:24:32 +0800

    Create type-model-inheritance-nested-discriminator.cs

---
+++ test/CadlRanchProjects.Tests/type-model-inheritance-nested-discriminator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using _Type.Model.Inheritance.NestedDiscriminator;
+using _Type.Model.Inheritance.NestedDiscriminator.Models;
+using AutoRest.TestServer.Tests.Infrastructure;
+using NUnit.Framework;
+
+namespace CadlRanchProjects.Tests
+{
+    public class type_model_inheritance_nested_discriminator : CadlRanchTestBase
+    {
+        [Test]
+        public Task Type_model_inheritance_nested_discriminator_getMissingDiscriminator() => Test(async (host) =>
+        {
+            var response = await new NestedDiscriminatorClient(host, null).GetMissingDiscriminatorAsync();
+            var value = response.Value;
+            Assert.AreEqual("wrongKind", value.Kind);
+            Assert.AreEqual(1, value.Age);
+        });
+    }
+}
