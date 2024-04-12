13d268c04ceb1a9c66dcd7563b616877e45e8f04
Arcturus Zhang <dapzhang@microsoft.com>
2024-04-12 11:46:26 +0800

    add a helper method to assert BinaryData values

---
+++ test/CadlRanchProjects.Tests/BinaryDataAssert.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace CadlRanchProjects.Tests
+{
+    public static class BinaryDataAssert
+    {
+        public static void AreEqual(BinaryData expected, BinaryData result)
+        {
+            CollectionAssert.AreEqual(expected?.ToArray(), result?.ToArray());
+        }
+    }
+}
