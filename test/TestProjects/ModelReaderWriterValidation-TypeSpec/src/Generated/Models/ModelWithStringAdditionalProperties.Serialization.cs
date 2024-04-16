dda738628e6ccafc44e427f93b9657c4113e1430
Arcturus Zhang <dapzhang@microsoft.com>
2024-04-16 14:18:44 +0800

    regen after merge

--- test/TestProjects/ModelReaderWriterValidation-TypeSpec/src/Generated/Models/ModelWithStringAdditionalProperties.Serialization.cs
+++ test/TestProjects/ModelReaderWriterValidation-TypeSpec/src/Generated/Models/ModelWithStringAdditionalProperties.Serialization.cs
@@ -16,7 +16,7 @@ namespace ModelReaderWriterValidationTypeSpec.Models
 {
     public partial class ModelWithStringAdditionalProperties : IUtf8JsonSerializable, IJsonModel<ModelWithStringAdditionalProperties>
     {
-        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ModelWithStringAdditionalProperties>)this).Write(writer, new ModelReaderWriterOptions("W"));
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer) => ((IJsonModel<ModelWithStringAdditionalProperties>)this).Write(writer, ModelSerializationExtensions.WireOptions);
 
         void IJsonModel<ModelWithStringAdditionalProperties>.Write(Utf8JsonWriter writer, ModelReaderWriterOptions options)
         {
@@ -68,7 +68,7 @@ namespace ModelReaderWriterValidationTypeSpec.Models
 
         internal static ModelWithStringAdditionalProperties DeserializeModelWithStringAdditionalProperties(JsonElement element, ModelReaderWriterOptions options = null)
         {
-            options ??= new ModelReaderWriterOptions("W");
+            options ??= ModelSerializationExtensions.WireOptions;
 
             if (element.ValueKind == JsonValueKind.Null)
             {
@@ -152,7 +152,7 @@ namespace ModelReaderWriterValidationTypeSpec.Models
         internal virtual RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this, new ModelReaderWriterOptions("W"));
+            content.JsonWriter.WriteObjectValue(this, ModelSerializationExtensions.WireOptions);
             return content;
         }
     }
