ecdb219d95dd69b7dce305bcdf2c0743993e572a
Wei Hu <huwe@microsoft.com>
2024-04-17 17:25:32 +0800

    remove InputSystemType

--- src/AutoRest.CSharp/Common/Input/InputTypes/InputSystemType.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-using System;
-
-namespace AutoRest.CSharp.Common.Input
-{
-    internal record InputSystemType(Type Type, InputType ElementType, bool IsNullable) : InputType(Type.Name, IsNullable);
-}
