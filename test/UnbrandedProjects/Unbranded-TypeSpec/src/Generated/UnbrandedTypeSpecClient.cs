a695486386385b04804739ab97fb089d6633c85a
Arcturus Zhang <dapzhang@microsoft.com>
2024-04-23 13:13:57 +0800

    regen

--- test/UnbrandedProjects/Unbranded-TypeSpec/src/Generated/UnbrandedTypeSpecClient.cs
+++ test/UnbrandedProjects/Unbranded-TypeSpec/src/Generated/UnbrandedTypeSpecClient.cs
@@ -1690,10 +1690,7 @@ namespace UnbrandedTypeSpec
             request.Headers.Set("Accept", "application/json");
             request.Headers.Set("Content-Type", "application/json");
             request.Content = content;
-            if (options != null)
-            {
-                message.Apply(options);
-            }
+            message.Apply(options);
             return message;
         }
 
