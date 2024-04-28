f9cc751b944b4b9ff95491ed608f0ad08c74919e
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-04-28 14:50:34 +0800

    client/naming (#4661)
    * error
    
    * update the test cases
    
    ---------
    
    Co-authored-by: ArcturusZhang <dapzhang@microsoft.com>

--- test/CadlRanchProjectsNonAzure.Tests/client-naming.cs
+++ test/CadlRanchProjectsNonAzure.Tests/client-naming.cs
@@ -1,18 +1,18 @@
 ﻿using System.Threading.Tasks;
 using AutoRest.TestServer.Tests.Infrastructure;
-using Client.Naming;
-using Client.Naming.Models;
 using NUnit.Framework;
+using Scm.Client.Naming;
+using Scm.Client.Naming.Models;
 
-namespace CadlRanchProjects.Tests
+namespace CadlRanchProjectsNonAzure.Tests
 {
-    public class ClientNamingTests: CadlRanchTestBase
+    public class ClientNamingTests: CadlRanchNonAzureTestBase
     {
         [Test]
         public Task Client_Naming_Property_client() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).ClientAsync(new ClientNameModel(true));
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
 
             Assert.NotNull(typeof(ClientNameModel).GetProperty("ClientName"));
             Assert.IsNull(typeof(ClientNameModel).GetProperty("DefaultName"));
@@ -22,7 +22,7 @@ namespace CadlRanchProjects.Tests
         public Task Client_Naming_Property_language() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).LanguageAsync(new LanguageClientNameModel(true));
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
 
             Assert.NotNull(typeof(LanguageClientNameModel).GetProperty("CSName"));
             Assert.IsNull(typeof(LanguageClientNameModel).GetProperty("DefaultName"));
@@ -32,7 +32,7 @@ namespace CadlRanchProjects.Tests
         public Task Client_Naming_Property_compatibleWithEncodedName() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).CompatibleWithEncodedNameAsync(new ClientNameAndJsonEncodedNameModel(true));
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
 
             Assert.NotNull(typeof(ClientNameModel).GetProperty("ClientName"));
             Assert.IsNull(typeof(ClientNameModel).GetProperty("DefaultName"));
@@ -42,31 +42,31 @@ namespace CadlRanchProjects.Tests
         public Task Client_Naming_operation() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).ClientNameAsync();
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
         });
 
         [Test]
         public Task Client_Naming_parameter() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).ParameterAsync(clientName: "true");
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
         });
 
         [Test]
         public Task Client_Naming_Header_request() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).RequestAsync(clientName: "true");
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
         });
 
         [Test]
         public Task Client_Naming_Header_response() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).ResponseAsync();
-            Assert.IsTrue(response.Headers.Contains("default-name"));
-            foreach (var header in response.Headers)
+            Assert.IsTrue(response.GetRawResponse().Headers.TryGetValue("default-name", out _));
+            foreach (var header in response.GetRawResponse().Headers)
             {
-                var key = header.Name;
+                var key = header.Key;
                 if (key == "default-name")
                 {
                     var value = header.Value;
@@ -78,29 +78,29 @@ namespace CadlRanchProjects.Tests
         [Test]
         public Task Client_Naming_Model_client() => Test(async (host) =>
         {
-            var response = await new NamingClient(host, null).GetClientModelClient().ClientAsync(new Client.Naming.Models.ClientModel(true));
-            Assert.AreEqual(204, response.Status);
+            var response = await new NamingClient(host, null).GetClientModelClient().ClientAsync(new Scm.Client.Naming.Models.ClientModel(true));
+            Assert.AreEqual(204, response.GetRawResponse().Status);
         });
 
         [Test]
         public Task Client_Naming_Model_language() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).GetClientModelClient().LanguageAsync(new CSModel(true));
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
         });
 
         [Test]
         public Task Client_Naming_UnionEnum_unionEnumName() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).GetUnionEnumClient().UnionEnumNameAsync(ClientExtensibleEnum.EnumValue1);
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
         });
 
         [Test]
         public Task Client_Naming_UnionEnum_unionEnumMemberName() => Test(async (host) =>
         {
             var response = await new NamingClient(host, null).GetUnionEnumClient().UnionEnumMemberNameAsync(ExtensibleEnum.ClientEnumValue1);
-            Assert.AreEqual(204, response.Status);
+            Assert.AreEqual(204, response.GetRawResponse().Status);
         });
     }
 }
