5660f7bb12e0bcd0f90d2486b3d9bda98ac7611e
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-04-28 18:21:21 +0800

    server/endpoint/not-defined (#4654)
    * error
    
    * Update
    
    * regen
    
    * remove duplicates
    
    * fix after merge
    
    ---------
    
    Co-authored-by: ArcturusZhang <dapzhang@microsoft.com>

---
+++ test/CadlRanchProjectsNonAzure.Tests/server-endpoint-notDefined.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using AutoRest.TestServer.Tests.Infrastructure;
+using NUnit.Framework;
+using Scm.Server.Endpoint.NotDefined;
+
+namespace CadlRanchProjectsNonAzure.Tests
+{
+    public class ServerEndpointNotDefinedTests : CadlRanchNonAzureTestBase
+    {
+        [Test]
+        public Task Server_Endpoint_NotDefined_valid() => Test(async (host) =>
+        {
+            var response = await new NotDefinedClient(host, null).ValidAsync();
+            Assert.AreEqual(200, response.GetRawResponse().Status);
+        });
+    }
+}
