1954f2b673cb07f487f4cfedbba9e75287bb73fc
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-04-28 20:36:31 +0800

    type/model/inheritance/single-discriminator (#4635)
    * error
    
    * error
    
    * fix the inheritance issue
    
    * refine the fix
    
    * regen
    
    * test
    
    * regen
    
    * Update launchSettings.json
    
    * regen
    
    ---------
    
    Co-authored-by: Arcturus Zhang <dapzhang@microsoft.com>

--- test/TestProjects/Models-TypeSpec/src/Generated/Models/UnknownBaseModelWithDiscriminatorDefinedOnBase.Serialization.cs
+++ test/TestProjects/Models-TypeSpec/src/Generated/Models/UnknownBaseModelWithDiscriminatorDefinedOnBase.Serialization.cs
@@ -140,7 +140,7 @@ namespace ModelsTypeSpec.Models
         internal override RequestContent ToRequestContent()
         {
             var content = new Utf8JsonRequestContent();
-            content.JsonWriter.WriteObjectValue(this, ModelSerializationExtensions.WireOptions);
+            content.JsonWriter.WriteObjectValue<BaseModelWithDiscriminatorDefinedOnBase>(this, ModelSerializationExtensions.WireOptions);
             return content;
         }
     }
