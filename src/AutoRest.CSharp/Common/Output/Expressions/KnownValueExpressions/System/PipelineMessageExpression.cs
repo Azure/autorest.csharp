a5ac2cea93f07d4cadd990eab51bbd630177f066
Dapeng Zhang <dapzhang@microsoft.com>
2024-05-16 02:04:19 +0800

    finish and regenerate (#4710)

--- src/AutoRest.CSharp/Common/Output/Expressions/KnownValueExpressions/System/PipelineMessageExpression.cs
+++ src/AutoRest.CSharp/Common/Output/Expressions/KnownValueExpressions/System/PipelineMessageExpression.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.ClientModel.Primitives;
 using AutoRest.CSharp.Common.Output.Expressions.ValueExpressions;
 
@@ -11,5 +12,9 @@ namespace AutoRest.CSharp.Common.Output.Expressions.KnownValueExpressions.System
         public PipelineRequestExpression Request => new(Property(nameof(PipelineMessage.Request)));
 
         public PipelineResponseExpression Response => new(Property(nameof(PipelineMessage.Response)));
+
+        public BoolExpression BufferResponse => new(Property(nameof(PipelineMessage.BufferResponse)));
+
+        public PipelineResponseExpression ExtractResponse() => new(new InvokeInstanceMethodExpression(Untyped, nameof(PipelineMessage.ExtractResponse), Array.Empty<ValueExpression>(), false));
     }
 }
