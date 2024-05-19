6c0d78d6c68204897d4c1a324be2b01ad103e774
FAREAST\chunyu <chunyu@microsoft.com>
2024-05-19 17:37:41 +0800

    adopt tsp-client

--- src/TypeSpec.Extension/Emitter.Csharp/src/emitter.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/emitter.ts
@@ -161,7 +161,7 @@ export async function $onEmit(context: EmitContext<NetEmitterOptions>) {
 
             if (options.skipSDKGeneration !== true) {
                 const csProjFile = resolvePath(
-                    outputFolder,
+                    outputFolder.endsWith("src") ? outputFolder : resolvePath(outputFolder, "src"),
                     `${configurations["library-name"]}.csproj`
                 );
                 Logger.getInstance().info(`Checking if ${csProjFile} exists`);
