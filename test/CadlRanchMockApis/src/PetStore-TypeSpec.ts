564d8e0da0127456150d222ed05e19cb7421a295
mcgallan <1186003288@qq.com>
2024-05-28 12:10:47 +0800

    remove

--- test/CadlRanchMockApis/src/PetStore-TypeSpec.ts
+++ test/CadlRanchMockApis/src/PetStore-TypeSpec.ts
@@ -7,62 +7,12 @@ const dog = {
   age: 12,
 };
 
-Scenarios.PetStore_DeletePetById = passOnSuccess(
-  mockapi.delete("/pets/1", () => {
-    return {
-      status: 200,
-    };
-  }),
-);
-
-Scenarios.PetStore_ReadPetById = passOnSuccess(
-  mockapi.get("/pets/1", () => {
-    return {
-      status: 200,
-      body: json(dog),
-    };
-  }),
-);
-
 Scenarios.PetStore_CreatePet = passOnSuccess(
   mockapi.post("/pets", (req) => {
     req.expect.bodyEquals(dog);
     return {
       status: 200,
       body: json(dog),
-    };
-  }),
-);
-
-Scenarios.PetStore_GetPetByKind = passOnSuccess(
-  mockapi.get("/pets/dog", () => {
-    return {
-      status: 200,
-      body: json(dog),
-    };
-  }),
-);
-
-Scenarios.PetStore_GetFirstPet = passOnSuccess(
-  mockapi.get("/pets", (req) => {
-    req.expect.containsQueryParam("start", "1");
-    return {
-      status: 200,
-      body: json(dog),
-    };
-  }),
-);
-
-Scenarios.PetStore_GetFish = passOnSuccess(
-  mockapi.get("/pets/getFish", (req) => {
-    req.expect.containsQueryParam("kind", "shark");
-    return {
-      status: 200,
-      body: json({
-        kind: "shark",
-        size: 100,
-        bite: "I can bite"
-      }),
     };
   }),
 );
