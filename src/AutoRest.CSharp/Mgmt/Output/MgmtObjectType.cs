6fddc5401d0061ecca4dd32d69feb47d3a46e813
Arcturus Zhang <dapzhang@microsoft.com>
2024-06-05 16:20:13 +0800

    fix the discriminator issue

--- src/AutoRest.CSharp/Mgmt/Output/MgmtObjectType.cs
+++ src/AutoRest.CSharp/Mgmt/Output/MgmtObjectType.cs
@@ -190,10 +190,7 @@ namespace AutoRest.CSharp.Mgmt.Output
             var descendantTypes = schemaObjectType.Discriminator.Implementations.Select(implementation => implementation.Type).ToHashSet();
 
             // We need this redundant check as the internal backing schema will not be a part of the discriminator implementations of its base type.
-            var immediateParents = InputModel.GetAllBaseModels().ToArray();
-            if (InputModel.DiscriminatorValue == "Unknown" &&
-                immediateParents.Length == 1 &&
-                immediateParents.Single().Equals(schemaObjectType.InputModel))
+            if (InputModel.DiscriminatorValue == "Unknown" && InputModel.BaseModel == schemaObjectType.InputModel)
             {
                 descendantTypes.Add(Type);
             }
