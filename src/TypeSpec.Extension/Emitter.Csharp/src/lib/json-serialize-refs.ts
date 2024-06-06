4763a7268d26fbb49de694841569f449b47fb055
Arcturus Zhang <dapzhang@microsoft.com>
2024-06-06 14:42:46 +0800

    resolve comments

--- src/TypeSpec.Extension/Emitter.Csharp/src/lib/json-serialize-refs.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/lib/json-serialize-refs.ts
@@ -1,6 +1,3 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
 // Copyright (c) 2019-2022 Lorenzo Delana
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
