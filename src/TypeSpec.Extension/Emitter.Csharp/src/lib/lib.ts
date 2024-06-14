522d12868dab5481141f19ddd28a9fb689f5c784
jolov <jolov@microsoft.com>
2024-06-14 09:48:47 -0700

    add TODO

--- src/TypeSpec.Extension/Emitter.Csharp/src/lib/lib.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/lib/lib.ts
@@ -15,6 +15,7 @@ const $lib = createTypeSpecLibrary({
             }
         },
         "no-route": {
+            // TODO - once https://github.com/Azure/typespec-azure/issues/1018 is fixed, change this back to error
             severity: "warning",
             messages: {
                 default: paramMessage`No Route for service for service ${"service"}`
