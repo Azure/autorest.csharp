7aa04aef8844cba879fc81b8f2bd6ad7b22e6622
Wei Hu <huwe@microsoft.com>
2024-07-01 13:48:38 +0800

    Revert "Ignore type comparison for subscriptionId during parent detection"
    This reverts commit 2bb05a94e73dd0ab05b3f83d2cf535d142011375.

---
+++ src/AutoRest.CSharp/Common/Input/InputTypes/Transformation/InputSubscriptionIdTransformer.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+
+namespace AutoRest.CSharp.Common.Input
+{
+    // TODO: remove this workaround
+    // We might firstly refine the logic of getting parent resource to get rid of the SubscriptionId type dependency for now.
+    // Ideally, we should get the parent resource from typespec directly to not depend on SubscriptionId type.
+    internal class InputSubscriptionIdTransformer
+    {
+        public static void Transform(InputNamespace inputNamespace)
+        {
+            bool setSubParam = false;
+            foreach (var client in inputNamespace.Clients)
+            {
+                foreach (var op in client.Operations)
+                {
+                    foreach (var p in op.Parameters)
+                    {
+                        // update the first subscriptionId parameter to be 'method' parameter
+                        if (!setSubParam && p.Name.Equals("subscriptionId", StringComparison.OrdinalIgnoreCase))
+                        {
+                            setSubParam = true;
+                            p.Kind = InputOperationParameterKind.Method;
+                            p.Type = InputPrimitiveType.String;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
