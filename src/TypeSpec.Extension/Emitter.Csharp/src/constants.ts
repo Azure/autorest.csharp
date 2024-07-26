9ce8b8591c7002bf58f3fc8e1d973d42fa3d5271
FAREAST\chunyu <chunyu@microsoft.com>
2024-07-26 18:36:14 +0800

    format

--- src/TypeSpec.Extension/Emitter.Csharp/src/constants.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/constants.ts
@@ -1,4 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-export const AzureDefaultAdditionalDecorators = ["Azure\\.ClientGenerator\\.Core\\.@clientName"];
+export const AzureDefaultAdditionalDecorators = [
+    "Azure\\.ClientGenerator\\.Core\\.@clientName"
+];
