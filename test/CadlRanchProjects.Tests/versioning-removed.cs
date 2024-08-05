5a23186ef07068f14e426921d40f2c78ad35e3b8
mcgallan <1186003288@qq.com>
2024-08-05 16:34:54 +0800

    Update versioning-removed.cs

--- test/CadlRanchProjects.Tests/versioning-removed.cs
+++ test/CadlRanchProjects.Tests/versioning-removed.cs
@@ -66,10 +66,30 @@ namespace CadlRanchProjects.Tests
         });
 
         [Test]
-        public Task Versioning_Removed_v3() => Test(async (host) =>
+        public Task Versioning_Removed_V3_V2() => Test(async (host) =>
         {
             ModelV3 modelV3 = new ModelV3("123", "bar");
             var response = await new RemovedClient(host, Versions.V2).V3Async(modelV3);
+            Assert.AreEqual(200, response.GetRawResponse().Status);
+            Assert.AreEqual("123", response.Value.Id);
+            Assert.AreEqual("bar", response.Value.Type);
+        });
+
+        [Test]
+        public Task Versioning_Removed_V3_V1() => Test(async (host) =>
+        {
+            ModelV3 modelV3 = new ModelV3("123", "bar");
+            var response = await new RemovedClient(host, Versions.V1).V3Async(modelV3);
+            Assert.AreEqual(200, response.GetRawResponse().Status);
+            Assert.AreEqual("123", response.Value.Id);
+            Assert.AreEqual("bar", response.Value.Type);
+        });
+
+        [Test]
+        public Task Versioning_Removed_V3_Beta() => Test(async (host) =>
+        {
+            ModelV3 modelV3 = new ModelV3("123", "bar");
+            var response = await new RemovedClient(host, Versions.Beta).V3Async(modelV3);
             Assert.AreEqual(200, response.GetRawResponse().Status);
             Assert.AreEqual("123", response.Value.Id);
             Assert.AreEqual("bar", response.Value.Type);
