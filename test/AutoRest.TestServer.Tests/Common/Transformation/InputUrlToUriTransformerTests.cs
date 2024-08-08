1a396fc451a93d73eab9824782efda60951ec027
Dapeng Zhang <dapzhang@microsoft.com>
2024-08-08 16:41:09 +0800

    fix the property name issue when it is url (#4972)
    * fix the property name issue when it is url
    
    * refine the implementation and added some test cases
    
    * minor improvement
    
    * add test to MgmtTypeSpec
    
    * resolve comments

---
+++ test/AutoRest.TestServer.Tests/Common/Transformation/InputUrlToUriTransformerTests.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using AutoRest.CSharp.Common.Input;
+using NUnit.Framework;
+
+namespace AutoRest.TestServer.Tests.Common.Transformation
+{
+    public class InputUrlToUriTransformerTests
+    {
+        [TestCase("serviceUrl", true, "serviceUri")]
+        [TestCase("ServiceUrl", true, "ServiceUri")]
+        [TestCase("somethingUrl", true, "somethingUri")]
+        [TestCase("SomethingUrl", true, "SomethingUri")]
+        [TestCase("ServiceUri", false, null)]
+        [TestCase("ServiceUri", false, null)]
+        [TestCase("somethingurl", false, null)]
+        [TestCase("something", false, null)]
+        public void ValidateUrlToUriTransformation(string name, bool canTransform, string? expected)
+        {
+            var result = InputUrlToUriTransformer.TryTransformUrlToUri(name, out var newName);
+            Assert.AreEqual(canTransform, result);
+            Assert.AreEqual(expected, newName);
+        }
+    }
+}
