84249bc3d2e3c6d688d37af58c27299e42c51081
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-08-22 11:27:13 +0800

    Remove Models-TypeSpec (#4993)
    * remove
    
    * remove
    
    * remove
    
    * Update SingleDiscriminatorTests.cs
    
    * update
    
    * Update type-property-nullable.cs
    
    * fix
    
    * Update type-property-nullable.cs

---
+++ test/CadlRanchProjects.Tests/TypeAsserts.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using static System.Reflection.BindingFlags;
+
+namespace CadlRanchProjects.Tests
+{
+    public static class TypeAsserts
+    {
+        public static PropertyInfo HasProperty(Type type, string name, BindingFlags bindingFlags)
+        {
+            var parameterInfo = type.GetProperties(bindingFlags).FirstOrDefault(p => p.Name == name);
+            Assert.NotNull(parameterInfo, $"Property '{name}' is not found");
+            return parameterInfo;
+        }
+    }
+}
