d7829d56065beec1fbb96e67a4b133710190e484
Arcturus Zhang <dapzhang@microsoft.com>
2024-09-10 10:42:43 +0800

    fix issues

--- src/AutoRest.CSharp/Common/Input/InputTypes/Serialization/TypeSpecInputEnumTypeValueConverter.cs
+++ src/AutoRest.CSharp/Common/Input/InputTypes/Serialization/TypeSpecInputEnumTypeValueConverter.cs
@@ -57,7 +57,7 @@ namespace AutoRest.CSharp.Common.Input
             InputEnumTypeValue enumValue = valueType.Kind switch
             {
                 InputPrimitiveTypeKind.String => new InputEnumTypeStringValue(name, rawValue.Value.GetString() ?? throw new JsonException(), description) { Decorators = decorators ?? [] },
-                InputPrimitiveTypeKind.Int32 => new InputEnumTypeIntegerValue(name, rawValue.Value.GetInt32(), valueType, description) { Decorators = decorators ?? [] },
+                InputPrimitiveTypeKind.Int32 => new InputEnumTypeIntegerValue(name, rawValue.Value.GetInt32(), description) { Decorators = decorators ?? [] },
                 InputPrimitiveTypeKind.Float32 => new InputEnumTypeFloatValue(name, rawValue.Value.GetSingle(), description) { Decorators = decorators ?? [] },
                 _ => throw new JsonException()
             };
