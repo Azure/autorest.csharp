2ebc3edbcd5fda16f51b6a7152f3c0463c6d600c
mcgallan <88413158+mcgallan@users.noreply.github.com>
2024-09-27 13:54:37 +0800

    generate (#5090)

---
+++ test/CadlRanchProjects.Tests/parameter-basic.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoRest.TestServer.Tests.Infrastructure;
+using Azure;
+using Azure.Core;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using Parameters.Basic;
+using Parameters.Basic.Models;
+
+namespace CadlRanchProjects.Tests
+{
+    public class parameter_basic : CadlRanchTestBase
+    {
+        [Test]
+        public Task Parameters_Basic_ExplicitBody() => Test(async (host) =>
+        {
+            Response response = await new BasicClient(host, null).GetExplicitBodyClient().SimpleAsync(new User("foo"));
+            Assert.AreEqual(204, response.Status);
+        });
+
+        [Test]
+        public Task Parameters_Basic_implicitBody() => Test(async (host) =>
+        {
+            Response response = await new BasicClient(host, null).GetImplicitBodyClient().SimpleAsync("foo");
+            Assert.AreEqual(204, response.Status);
+        });
+    }
+}
