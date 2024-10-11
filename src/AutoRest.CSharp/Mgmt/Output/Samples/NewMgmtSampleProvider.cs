066cd2d14161efa7a4c0cca0aae53355a3024c7f
Arcturus Zhang <dapzhang@microsoft.com>
2024-10-11 17:11:53 +0800

    follow up

--- src/AutoRest.CSharp/Mgmt/Output/Samples/NewMgmtSampleProvider.cs
+++ src/AutoRest.CSharp/Mgmt/Output/Samples/NewMgmtSampleProvider.cs
@@ -1,14 +1,22 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using AutoRest.CSharp.Common.Input;
+using AutoRest.CSharp.Common.Output.Expressions.Statements;
+using AutoRest.CSharp.Common.Output.Expressions.ValueExpressions;
 using AutoRest.CSharp.Common.Output.Models;
+using AutoRest.CSharp.Generation.Types;
 using AutoRest.CSharp.Input.Source;
 using AutoRest.CSharp.MgmtTest.Models;
 using AutoRest.CSharp.Output.Models;
 using AutoRest.CSharp.Output.Models.Shared;
 using AutoRest.CSharp.Output.Models.Types;
+using Azure.Core;
+using Azure.ResourceManager;
+using Azure.ResourceManager.Resources;
 using NUnit.Framework;
 using static AutoRest.CSharp.Common.Output.Models.Snippets;
 
@@ -29,12 +37,84 @@ namespace AutoRest.CSharp.Mgmt.Output.Samples
 
         protected override string DefaultName { get; }
 
+        protected override IEnumerable<string> BuildUsings()
+        {
+            yield return "Azure.Identity"; // we need this using because we might need to call `new DefaultAzureCredential` from `Azure.Identity` package, but Azure.Identity package is not a dependency of the generator project.
+        }
+
         private Method BuildSampleMethod(MgmtOperationSample sample, bool isAsync)
         {
             var signature = sample.GetMethodSignature(false);
 
-            return new Method(signature, EmptyStatement);
+            return new Method(signature, BuildSampleMethodBody(sample, isAsync));
         }
+
+        private MethodBodyStatement BuildSampleMethodBody(MgmtOperationSample sample, bool isAsync)
+        {
+            var statements = new List<MethodBodyStatement>()
+            {
+                new SingleLineCommentStatement($"Generated from example definition: {sample.ExampleFilepath}"),
+                new SingleLineCommentStatement($"this example is just showing the usage of \"{sample.OperationId}\" operation, for the dependent resource, they will have to be created separately."),
+                EmptyLine,
+                new SingleLineCommentStatement("get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line"),
+                Declare(typeof(TokenCredential), "cred", new FormattableStringToExpression($"new DefaultAzureCredential()"), out var cred), // this project does not use Azure.Identity as a dependency, therefore this is the only way to write this.
+                new SingleLineCommentStatement("authenticate your client"),
+                Declare(typeof(ArmClient), "client", New.Instance(typeof(ArmClient), cred), out var client),
+                EmptyLine,
+                sample.Carrier switch
+                {
+                    Resource resource => BuildSampleOperationForResource(client, resource, sample),
+                    _ => throw new InvalidOperationException("This should never happen")
+                },
+            };
+
+            return statements;
+        }
+
+        private MethodBodyStatement BuildSampleOperationForResource(ValueExpression client, Resource resource, MgmtOperationSample sample)
+        {
+            var resourceName = GetResourceName(resource);
+            var statements = new List<MethodBodyStatement>()
+            {
+                new SingleLineCommentStatement($"this example assumes you already have this {resourceName} created on azure"),
+                new SingleLineCommentStatement($"for more information of creating {resourceName}, please refer to the document of {resourceName}"),
+                BuildGetResourceStatement(resource, sample, client)
+            };
+
+            return statements;
+        }
+
+        private MethodBodyStatement BuildGetResourceStatement(MgmtTypeProvider carrierResource, OperationExample example, ValueExpression client)
+            => carrierResource switch
+            {
+                ResourceCollection => throw new InvalidOperationException($"ResourceCollection is not supported here"),
+                Resource parentResource => BuildGetResourceStatementFromResource(parentResource, example, client),
+                //MgmtExtension parentExtension => WriteGetResourceStatementFromExtension(parentExtension, example, client),
+                _ => throw new InvalidOperationException($"Unknown parent {carrierResource.GetType()}"),
+            };
+
+        private MethodBodyStatement BuildGetResourceStatementFromResource(Resource carrierResource, OperationExample example, ValueExpression client)
+        {
+            // Can't use CSharpType.Equals(typeof(...)) because the CSharpType.Equals(Type) would assume itself is a FrameworkType, but here it's generated when IsArmCore=true
+            if (Configuration.MgmtConfiguration.IsArmCore && carrierResource.Type.Name == nameof(TenantResource))
+            {
+                //return BuildGetResourceStatementFromTenantResource(carrierResource, example, client);
+                throw new InvalidOperationException("WIP");
+            }
+            else
+            {
+                var id = new VariableReference(typeof(ResourceIdentifier), "id");
+                // WIP here
+                return EmptyLine;
+            }
+        }
+
+        private string GetResourceName(MgmtTypeProvider provider) => provider switch
+        {
+            Resource resource => resource.Type.Name,
+            MgmtExtension extension => extension.ArmCoreType.Name,
+            _ => throw new InvalidOperationException("Should never happen")
+        };
 
         protected override IEnumerable<Method> BuildMethods()
         {
--- test/TestProjects/MgmtTypeSpec/samples/Generated/Samples/Sample_FooResource.cs
+++ test/TestProjects/MgmtTypeSpec/samples/Generated/Samples/Sample_FooResource.cs
@@ -6,6 +6,9 @@
 #nullable disable
 
 using System.Threading.Tasks;
+using Azure.Core;
+using Azure.Identity;
+using Azure.ResourceManager;
 using NUnit.Framework;
 
 namespace MgmtTypeSpec.Samples
@@ -16,30 +19,80 @@ namespace MgmtTypeSpec.Samples
         [Ignore("Only validating compilation of examples")]
         public async Task Get_GetAFoo()
         {
+            // Generated from example definition: 2024-05-01/Foos_Get.json
+            // this example is just showing the usage of "Foos_Get" operation, for the dependent resource, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this FooResource created on azure
+            // for more information of creating FooResource, please refer to the document of FooResource
         }
 
         [Test]
         [Ignore("Only validating compilation of examples")]
         public async Task Update_CreateAFoo()
         {
+            // Generated from example definition: 2024-05-01/Foos_CreateOrUpdate.json
+            // this example is just showing the usage of "Foos_CreateOrUpdate" operation, for the dependent resource, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this FooResource created on azure
+            // for more information of creating FooResource, please refer to the document of FooResource
         }
 
         [Test]
         [Ignore("Only validating compilation of examples")]
         public async Task AddTag_GetAFoo()
         {
+            // Generated from example definition: 2024-05-01/Foos_Get.json
+            // this example is just showing the usage of "Foos_Get" operation, for the dependent resource, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this FooResource created on azure
+            // for more information of creating FooResource, please refer to the document of FooResource
         }
 
         [Test]
         [Ignore("Only validating compilation of examples")]
         public async Task SetTags_GetAFoo()
         {
+            // Generated from example definition: 2024-05-01/Foos_Get.json
+            // this example is just showing the usage of "Foos_Get" operation, for the dependent resource, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this FooResource created on azure
+            // for more information of creating FooResource, please refer to the document of FooResource
         }
 
         [Test]
         [Ignore("Only validating compilation of examples")]
         public async Task RemoveTag_GetAFoo()
         {
+            // Generated from example definition: 2024-05-01/Foos_Get.json
+            // this example is just showing the usage of "Foos_Get" operation, for the dependent resource, they will have to be created separately.
+
+            // get your azure access token, for more details of how Azure SDK get your access token, please refer to https://learn.microsoft.com/en-us/dotnet/azure/sdk/authentication?tabs=command-line
+            TokenCredential cred = new DefaultAzureCredential();
+            // authenticate your client
+            ArmClient client = new ArmClient(cred);
+
+            // this example assumes you already have this FooResource created on azure
+            // for more information of creating FooResource, please refer to the document of FooResource
         }
     }
 }
