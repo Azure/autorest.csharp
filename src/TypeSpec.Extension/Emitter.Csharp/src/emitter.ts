1ad71449faea166b1760db0fd762b666c60d6349
FAREAST\chunyu <chunyu@microsoft.com>
2024-10-17 17:13:30 +0800

    invert if

--- src/TypeSpec.Extension/Emitter.Csharp/src/emitter.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/emitter.ts
@@ -24,124 +24,114 @@ import { azureSDKContextOptions } from "./sdk-context-options.js";
 
 export async function $onEmit(context: EmitContext<AzureNetEmitterOptions>) {
     const program: Program = context.program;
+
+    if (program.compilerOptions.noEmit || program.hasError()) return;
+
     const options = resolveAzureEmitterOptions(context);
     /* set the loglevel. */
     Logger.initialize(program, options.logLevel ?? LoggerLevel.INFO);
 
-    if (!program.compilerOptions.noEmit && !program.hasError()) {
-        context.options.skipSDKGeneration = true;
-        if (
-            context.emitterOutputDir &&
-            path.basename(context.emitterOutputDir) === "src"
-        ) {
-            context.emitterOutputDir = path.dirname(context.emitterOutputDir);
-        }
-        Logger.getInstance().info(
-            "Starting Microsoft Generator Csharp emitter."
+    context.options.skipSDKGeneration = true;
+    if (
+        context.emitterOutputDir &&
+        path.basename(context.emitterOutputDir) === "src"
+    ) {
+        context.emitterOutputDir = path.dirname(context.emitterOutputDir);
+    }
+    Logger.getInstance().info("Starting Microsoft Generator Csharp emitter.");
+    setSDKContextOptions(azureSDKContextOptions);
+    await $OnMGCEmit(context);
+    const outputFolder = resolveOutputFolder(context);
+    const isSrcFolder = path.basename(outputFolder) === "src";
+    const generatedFolder = isSrcFolder
+        ? resolvePath(outputFolder, "Generated")
+        : resolvePath(outputFolder, "src", "Generated");
+    if (options.skipSDKGeneration !== true) {
+        const configurationFilePath = resolvePath(
+            outputFolder,
+            configurationFileName
         );
-        setSDKContextOptions(azureSDKContextOptions);
-        await $OnMGCEmit(context);
-        const outputFolder = resolveOutputFolder(context);
-        const isSrcFolder = path.basename(outputFolder) === "src";
-        const generatedFolder = isSrcFolder
-            ? resolvePath(outputFolder, "Generated")
-            : resolvePath(outputFolder, "src", "Generated");
-        if (options.skipSDKGeneration !== true) {
-            const configurationFilePath = resolvePath(
+        const configurations = JSON.parse(
+            fs.readFileSync(configurationFilePath, "utf-8")
+        );
+        //resolve shared folders based on generator path override
+        const resolvedSharedFolders: string[] = [];
+        const sharedFolders = [
+            resolvePath(options.csharpGeneratorPath, "..", "Generator.Shared"),
+            resolvePath(options.csharpGeneratorPath, "..", "Azure.Core.Shared")
+        ];
+        for (const sharedFolder of sharedFolders) {
+            resolvedSharedFolders.push(
+                path
+                    .relative(generatedFolder, sharedFolder)
+                    .replaceAll("\\", "/")
+            );
+        }
+        configurations["shared-source-folders"] = resolvedSharedFolders ?? [];
+        configurations["flavor"] =
+            options["flavor"] ??
+            (configurations.namespace.toLowerCase().startsWith("azure.")
+                ? "azure"
+                : undefined);
+        configurations["enable-internal-raw-data"] =
+            options["enable-internal-raw-data"];
+        const examplesDir =
+            options["examples-dir"] ?? options["examples-directory"];
+        if (examplesDir) {
+            configurations["examples-dir"] = path.relative(
                 outputFolder,
-                configurationFileName
+                examplesDir
             );
-            const configurations = JSON.parse(
-                fs.readFileSync(configurationFilePath, "utf-8")
-            );
-            //resolve shared folders based on generator path override
-            const resolvedSharedFolders: string[] = [];
-            const sharedFolders = [
-                resolvePath(
-                    options.csharpGeneratorPath,
-                    "..",
-                    "Generator.Shared"
-                ),
-                resolvePath(
-                    options.csharpGeneratorPath,
-                    "..",
-                    "Azure.Core.Shared"
-                )
-            ];
-            for (const sharedFolder of sharedFolders) {
-                resolvedSharedFolders.push(
-                    path
-                        .relative(generatedFolder, sharedFolder)
-                        .replaceAll("\\", "/")
-                );
-            }
-            configurations["shared-source-folders"] =
-                resolvedSharedFolders ?? [];
-            configurations["flavor"] =
-                options["flavor"] ??
-                (configurations.namespace.toLowerCase().startsWith("azure.")
-                    ? "azure"
-                    : undefined);
-            configurations["enable-internal-raw-data"] =
-                options["enable-internal-raw-data"];
-            const examplesDir =
-                options["examples-dir"] ?? options["examples-directory"];
-            if (examplesDir) {
-                configurations["examples-dir"] = path.relative(
-                    outputFolder,
-                    examplesDir
-                );
-            }
-            /* TODO: when we support to emit decorator list https://github.com/Azure/autorest.csharp/issues/4887, we will update to use emitted decorator to identify if it is azure-arm */
-            /* set azure-arm */
-            const sdkContext = await createSdkContext(
-                context,
-                "@azure-tools/typespec-csharp"
-            );
-            configurations["azure-arm"] =
-                sdkContext.arm === false ? undefined : sdkContext.arm;
-            configurations["use-write-core"] =
-                options["use-write-core"] ??
-                (configurations["azure-arm"] ? true : undefined);
-            await program.host.writeFile(
-                resolvePath(outputFolder, configurationFileName),
-                prettierOutput(JSON.stringify(configurations, null, 2))
-            );
+        }
+        /* TODO: when we support to emit decorator list https://github.com/Azure/autorest.csharp/issues/4887, we will update to use emitted decorator to identify if it is azure-arm */
+        /* set azure-arm */
+        const sdkContext = await createSdkContext(
+            context,
+            "@azure-tools/typespec-csharp"
+        );
+        configurations["azure-arm"] =
+            sdkContext.arm === false ? undefined : sdkContext.arm;
+        configurations["use-write-core"] =
+            options["use-write-core"] ??
+            (configurations["azure-arm"] ? true : undefined);
+        await program.host.writeFile(
+            resolvePath(outputFolder, configurationFileName),
+            prettierOutput(JSON.stringify(configurations, null, 2))
+        );
 
-            const csProjFile = resolvePath(
-                isSrcFolder ? outputFolder : resolvePath(outputFolder, "src"),
-                `${configurations["library-name"]}.csproj`
-            );
-            Logger.getInstance().info(`Checking if ${csProjFile} exists`);
-            const newProjectOption =
-                options["new-project"] || !existsSync(csProjFile)
-                    ? "--new-project"
-                    : "";
-            const existingProjectOption = options["existing-project-folder"]
-                ? `--existing-project-folder ${options["existing-project-folder"]}`
+        const csProjFile = resolvePath(
+            isSrcFolder ? outputFolder : resolvePath(outputFolder, "src"),
+            `${configurations["library-name"]}.csproj`
+        );
+        Logger.getInstance().info(`Checking if ${csProjFile} exists`);
+        const newProjectOption =
+            options["new-project"] || !existsSync(csProjFile)
+                ? "--new-project"
                 : "";
-            const debugFlag = options.debug ?? false ? " --debug" : "";
+        const existingProjectOption = options["existing-project-folder"]
+            ? `--existing-project-folder ${options["existing-project-folder"]}`
+            : "";
+        const debugFlag = options.debug ?? false ? " --debug" : "";
 
-            const command = `dotnet --roll-forward Major ${resolvePath(
-                options.csharpGeneratorPath
-            )} --project-path ${outputFolder} ${newProjectOption} ${existingProjectOption} --clear-output-folder ${
-                options["clear-output-folder"]
-            }${debugFlag}`;
-            Logger.getInstance().info(command);
+        const command = `dotnet --roll-forward Major ${resolvePath(
+            options.csharpGeneratorPath
+        )} --project-path ${outputFolder} ${newProjectOption} ${existingProjectOption} --clear-output-folder ${
+            options["clear-output-folder"]
+        }${debugFlag}`;
+        Logger.getInstance().info(command);
 
-            try {
-                execSync(command, { stdio: "inherit" });
-            } catch (error: any) {
-                if (error.message) Logger.getInstance().info(error.message);
-                if (error.stderr) Logger.getInstance().error(error.stderr);
-                if (error.stdout) Logger.getInstance().verbose(error.stdout);
-                throw error;
-            }
-            if (!options["save-inputs"]) {
-                // delete
-                deleteFile(resolvePath(outputFolder, tspOutputFileName));
-                deleteFile(resolvePath(outputFolder, configurationFileName));
-            }
+        try {
+            execSync(command, { stdio: "inherit" });
+        } catch (error: any) {
+            if (error.message) Logger.getInstance().info(error.message);
+            if (error.stderr) Logger.getInstance().error(error.stderr);
+            if (error.stdout) Logger.getInstance().verbose(error.stdout);
+            throw error;
+        }
+        if (!options["save-inputs"]) {
+            // delete
+            deleteFile(resolvePath(outputFolder, tspOutputFileName));
+            deleteFile(resolvePath(outputFolder, configurationFileName));
         }
     }
 }
