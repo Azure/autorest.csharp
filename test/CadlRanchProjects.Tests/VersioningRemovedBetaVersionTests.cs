9d216e52e1bc37a230ccbc0ed4514e8662b3470c
mcgallan <1186003288@qq.com>
2024-11-18 11:31:51 +0800

    add test

--- test/CadlRanchProjects.Tests/VersioningRemovedBetaVersionTests.cs
+++ test/CadlRanchProjects.Tests/VersioningRemovedBetaVersionTests.cs
@@ -14,6 +14,69 @@ namespace CadlRanchProjects.Tests
     public class VersioningRemovedBetaVersionTests : CadlRanchTestBase
     {
         [Test]
+        public void TestRemovedMembers()
+        {
+            /* verify ModelV1. */
+            var propertiesV1 = typeof(ModelV1).GetProperties();
+            Assert.IsNotNull(propertiesV1);
+            Assert.AreEqual(3, propertiesV1.Length);
+
+            /* verify EnumV1 */
+            Assert.IsTrue(typeof(EnumV1).IsEnum);
+            var enumValuesV1 = typeof(EnumV1).GetEnumNames();
+            Assert.IsNotNull(enumValuesV1);
+            Assert.AreEqual(1, enumValuesV1.Length);
+
+            /* verify ModelV2. */
+            var properties = typeof(ModelV2).GetProperties();
+            Assert.IsNotNull(properties);
+            Assert.AreEqual(4, properties.Length);
+            /* verify property RemovedProp in V2.*/
+            Assert.IsNotNull(typeof(ModelV2).GetProperty("RemovedProp"));
+
+            /* verify EnumV2 */
+            Assert.IsTrue(typeof(EnumV2).IsEnum);
+            var enumValues = typeof(EnumV2).GetEnumNames();
+            Assert.IsNotNull(enumValues);
+            Assert.AreEqual(2, enumValues.Length);
+
+            /* verify ModelV3. */
+            var propertiesv3 = typeof(ModelV3).GetProperties();
+            Assert.IsNotNull(propertiesv3);
+            Assert.AreEqual(1, propertiesv3.Length);
+            /* verify property id in ModelV3.*/
+            Assert.IsNotNull(typeof(ModelV3).GetProperty("Id"));
+
+            /* check existence of method V1 */
+            var v1Methods = typeof(RemovedClient).GetMethods().Where(m => m.Name == "V1" || m.Name == "V1Async");
+            Assert.IsNotNull(v1Methods);
+            Assert.AreEqual(4, v1Methods.Count());
+            foreach (var method in v1Methods)
+            {
+                Assert.False(method.GetParameters().Any(p => p.Name == "param"));
+            }
+
+            /* check existence of method V2. */
+            var v2Methods = typeof(RemovedClient).GetMethods().Where(m => m.Name == "V2" || m.Name == "V2Async");
+            Assert.IsNotNull(v2Methods);
+            Assert.AreEqual(4, v2Methods.Count());
+            foreach (var method in v2Methods)
+            {
+                Assert.True(method.GetParameters().Any(p => p.Name == "param"));
+            }
+
+            /* check existence of method ModelV3. */
+            var v3Methods = typeof(RemovedClient).GetMethods().Where(m => m.Name == "ModelV3" || m.Name == "ModelV3Async");
+            Assert.IsNotNull(v3Methods);
+            Assert.AreEqual(4, v3Methods.Count());
+            foreach (var method in v3Methods)
+            {
+                Assert.False(method.GetParameters().Any(p => p.Name == "param"));
+            }
+
+        }
+
+        [Test]
         public Task Versioning_Removed_BetaVersion_V3Model() => Test(async (host) =>
         {
             var model = new ModelV3("123");
--- test/CadlRanchProjects.Tests/VersioningRemovedOldVersionTests.cs
+++ test/CadlRanchProjects.Tests/VersioningRemovedOldVersionTests.cs
@@ -14,6 +14,70 @@ namespace CadlRanchProjects.Tests
     public class VersioningRemovedOldVersionTests : CadlRanchTestBase
     {
         [Test]
+        public void TestRemovedMembers()
+        {
+            /* verify ModelV1. */
+            var properties = typeof(ModelV1).GetProperties();
+            Assert.IsNotNull(properties);
+            Assert.AreEqual(3, properties.Length);
+
+            /* verify EnumV1 */
+            Assert.IsTrue(typeof(EnumV1).IsEnum);
+            var enumValues = typeof(EnumV1).GetEnumNames();
+            Assert.IsNotNull(enumValues);
+            Assert.AreEqual(1, enumValues.Length);
+
+            /* verify ModelV2. */
+            var propertiesV2 = typeof(ModelV2).GetProperties();
+            Assert.IsNotNull(propertiesV2);
+            Assert.AreEqual(4, propertiesV2.Length);
+
+            /* verify EnumV2 */
+            Assert.IsTrue(typeof(EnumV2).IsEnum);
+            var enumValuesV2 = typeof(EnumV2).GetEnumNames();
+            Assert.IsNotNull(enumValuesV2);
+            Assert.AreEqual(2, enumValuesV2.Length);
+
+            /* verify ModelV3. */
+            var propertiesV3 = typeof(ModelV3).GetProperties();
+            Assert.IsNotNull(propertiesV3);
+            Assert.AreEqual(2, propertiesV3.Length);
+
+            /* verify EnumV3 */
+            Assert.IsTrue(typeof(EnumV3).IsEnum);
+            var enumValuesV3 = typeof(EnumV3).GetEnumNames();
+            Assert.IsNotNull(enumValuesV3);
+            Assert.AreEqual(2, enumValuesV3.Length);
+
+            /* check existence of method v2. */
+            var v1Methods = typeof(RemovedClient).GetMethods().Where(m => m.Name == "V1" || m.Name == "V1Async");
+            Assert.IsNotNull(v1Methods);
+            Assert.AreEqual(4, v1Methods.Count());
+            foreach (var method in v1Methods)
+            {
+                Assert.False(method.GetParameters().Any(p => p.Name == "param"));
+            }
+
+            /* check existence of method v2. */
+            var v2Methods = typeof(RemovedClient).GetMethods().Where(m => m.Name == "V2" || m.Name == "V2Async");
+            Assert.IsNotNull(v2Methods);
+            Assert.AreEqual(4, v2Methods.Count());
+            foreach (var method in v2Methods)
+            {
+                Assert.True(method.GetParameters().Any(p => p.Name == "param"));
+            }
+
+            /* check existence of method v2. */
+            var v3Methods = typeof(RemovedClient).GetMethods().Where(m => m.Name == "ModelV3" || m.Name == "ModelV3Async");
+            Assert.IsNotNull(v3Methods);
+            Assert.AreEqual(4, v3Methods.Count());
+            foreach (var method in v3Methods)
+            {
+                Assert.False(method.GetParameters().Any(p => p.Name == "param"));
+            }
+        }
+
+        [Test]
         public Task Versioning_Removed_OldVersion_V3Model() => Test(async (host) =>
         {
             var model = new ModelV3("123", EnumV3.EnumMemberV1);
