78c1132bcf2cb5af62bb7df74be8c0afe9ed96cf
Pan Shao <pashao@microsoft.com>
2024-11-20 16:49:18 +0800

    Add some comment for test customization

--- test/CadlRanchProjects/authentication/union/src/UnionClient.cs
+++ test/CadlRanchProjects/authentication/union/src/UnionClient.cs
@@ -6,7 +6,7 @@ namespace Authentication.Union
     public partial class UnionClient
     {
         /// <summary>
-        /// Gets the scopes required for authentication.
+        /// Gets the scopes required for authentication. We add this for test purpose, because the original AuthorizationScopes is private. We add this public property so that we could refer to in the test case.
         /// </summary>
         public static string[] TokenScopes => AuthorizationScopes;
     }
