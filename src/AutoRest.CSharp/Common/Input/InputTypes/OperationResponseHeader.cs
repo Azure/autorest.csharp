cef5f01847ed25c5e3926f339918a945f7511409
Mingzhe Huang (from Dev Box) <mingzhehuang@microsoft.com>
2024-11-26 14:33:15 +0800

    update `OperationResponseHeader`

--- src/AutoRest.CSharp/Common/Input/InputTypes/OperationResponseHeader.cs
+++ src/AutoRest.CSharp/Common/Input/InputTypes/OperationResponseHeader.cs
@@ -6,9 +6,7 @@ using System.Collections.Generic;
 
 namespace AutoRest.CSharp.Common.Input;
 
-internal record OperationResponseHeader(string Name, string NameInResponse, string Summary, string Doc, InputType Type)
+internal record OperationResponseHeader(string Name, string NameInResponse, string? Summary, string? Doc, InputType Type)
 {
     public OperationResponseHeader() : this(string.Empty, string.Empty, string.Empty, string.Empty, InputPrimitiveType.String) { }
-
-    public string Description { get; init; } = string.IsNullOrEmpty(Summary) ? Doc : Summary;
 }
