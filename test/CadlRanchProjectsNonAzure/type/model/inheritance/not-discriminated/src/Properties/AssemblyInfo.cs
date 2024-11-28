451545b10ac00dc9ef10dd6e3a9cb3262ee43e29
Wenming Liu <v-wenmingliu@microsoft.com>
2024-11-28 17:14:05 +0800

    regen2

--- test/CadlRanchProjectsNonAzure/type/model/inheritance/not-discriminated/src/Properties/AssemblyInfo.cs
+++ test/CadlRanchProjectsNonAzure/type/model/inheritance/not-discriminated/src/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
-
 using System.Runtime.CompilerServices;
-
 [assembly: InternalsVisibleTo("Scm._Type.Model.Inheritance.NotDiscriminated.Tests")]
