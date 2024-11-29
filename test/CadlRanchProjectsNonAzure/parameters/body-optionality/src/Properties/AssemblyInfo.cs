9ddd53ccd2a98a03f484b2d92fcf91ee871c825e
Wenming Liu <v-wenmingliu@microsoft.com>
2024-11-29 09:19:03 +0800

    gen

--- test/CadlRanchProjectsNonAzure/parameters/body-optionality/src/Properties/AssemblyInfo.cs
+++ test/CadlRanchProjectsNonAzure/parameters/body-optionality/src/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
+
 using System.Runtime.CompilerServices;
+
 [assembly: InternalsVisibleTo("Scm.Parameters.BodyOptionality.Tests")]
