f6e8b6117fc96c66931e7ddbb4e32693619cb756
Jorge Rangel <102122018+jorgerangel-msft@users.noreply.github.com>
2025-02-04 14:29:37 -0600

    add autorest emitter options (#5227)
    * add custom emitter options
    
    * set default values for all options
    
    * lint fixes

--- src/TypeSpec.Extension/Emitter.Csharp/src/options.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/options.ts
@@ -10,6 +10,19 @@ import { dllFilePath } from "@autorest/csharp";
 export interface AzureNetEmitterOptions extends NetEmitterOptions {
     csharpGeneratorPath?: string;
     "enable-internal-raw-data"?: boolean;
+    "single-top-level-client"?: boolean;
+    "existing-project-folder"?: string;
+    "keep-non-overloadable-protocol-signature"?: boolean;
+    "models-to-treat-empty-string-as-null"?: string[];
+    "additional-intrinsic-types-to-treat-empty-string-as-null"?: string[];
+    "methods-to-keep-client-default-value"?: string[];
+    "deserialize-null-collection-as-null-value"?: boolean;
+    "package-dir"?: string;
+    "head-as-boolean"?: boolean;
+    flavor?: string;
+    "generate-sample-project"?: boolean;
+    "generate-test-project"?: boolean;
+    "use-model-reader-writer"?: boolean;
 }
 
 export const AzureNetEmitterOptionsSchema: JSONSchemaType<AzureNetEmitterOptions> =
@@ -26,7 +39,46 @@ export const AzureNetEmitterOptionsSchema: JSONSchemaType<AzureNetEmitterOptions
             "enable-internal-raw-data": {
                 type: "boolean",
                 default: false
-            }
+            },
+            "single-top-level-client": { type: "boolean", nullable: true },
+            "existing-project-folder": { type: "string", nullable: true },
+            "keep-non-overloadable-protocol-signature": {
+                type: "boolean",
+                nullable: true
+            },
+            "models-to-treat-empty-string-as-null": {
+                type: "array",
+                nullable: true,
+                items: { type: "string" }
+            },
+            "additional-intrinsic-types-to-treat-empty-string-as-null": {
+                type: "array",
+                nullable: true,
+                items: { type: "string" }
+            },
+            "methods-to-keep-client-default-value": {
+                type: "array",
+                nullable: true,
+                items: { type: "string" }
+            },
+            "deserialize-null-collection-as-null-value": {
+                type: "boolean",
+                nullable: true
+            },
+            "package-dir": { type: "string", nullable: true },
+            "head-as-boolean": { type: "boolean", nullable: true },
+            flavor: { type: "string", nullable: true },
+            "generate-sample-project": {
+                type: "boolean",
+                nullable: true,
+                default: true
+            },
+            "generate-test-project": {
+                type: "boolean",
+                nullable: true,
+                default: false
+            },
+            "use-model-reader-writer": { type: "boolean", nullable: true }
         },
         required: []
     };
@@ -34,7 +86,19 @@ export const AzureNetEmitterOptionsSchema: JSONSchemaType<AzureNetEmitterOptions
 const defaultAzureEmitterOptions = {
     ...defaultOptions,
     csharpGeneratorPath: dllFilePath,
-    "enable-internal-raw-data": undefined
+    "enable-internal-raw-data": undefined,
+    "models-to-treat-empty-string-as-null": undefined,
+    "additional-intrinsic-types-to-treat-empty-string-as-null": [],
+    "methods-to-keep-client-default-value": undefined,
+    "deserialize-null-collection-as-null-value": undefined,
+    flavor: undefined,
+    "generate-test-project": false,
+    "existing-project-folder": undefined,
+    "head-as-boolean": undefined,
+    "generate-sample-project": true,
+    "use-model-reader-writer": true,
+    "single-top-level-client": undefined,
+    "keep-non-overloadable-protocol-signature": undefined
 };
 
 export function resolveAzureEmitterOptions(
@@ -47,6 +111,48 @@ export function resolveAzureEmitterOptions(
             defaultAzureEmitterOptions.csharpGeneratorPath,
         "enable-internal-raw-data":
             context.options["enable-internal-raw-data"] ??
-            defaultAzureEmitterOptions["enable-internal-raw-data"]
+            defaultAzureEmitterOptions["enable-internal-raw-data"],
+        "models-to-treat-empty-string-as-null":
+            context.options["models-to-treat-empty-string-as-null"] ??
+            defaultAzureEmitterOptions["models-to-treat-empty-string-as-null"],
+        "additional-intrinsic-types-to-treat-empty-string-as-null":
+            context.options[
+                "additional-intrinsic-types-to-treat-empty-string-as-null"
+            ] ??
+            defaultAzureEmitterOptions[
+                "additional-intrinsic-types-to-treat-empty-string-as-null"
+            ],
+        "methods-to-keep-client-default-value":
+            context.options["methods-to-keep-client-default-value"] ??
+            defaultAzureEmitterOptions["methods-to-keep-client-default-value"],
+        "deserialize-null-collection-as-null-value":
+            context.options["deserialize-null-collection-as-null-value"] ??
+            defaultAzureEmitterOptions[
+                "deserialize-null-collection-as-null-value"
+            ],
+        flavor: context.options.flavor ?? defaultAzureEmitterOptions.flavor,
+        "existing-project-folder":
+            context.options["existing-project-folder"] ??
+            defaultAzureEmitterOptions["existing-project-folder"],
+        "head-as-boolean":
+            context.options["head-as-boolean"] ??
+            defaultAzureEmitterOptions["head-as-boolean"],
+        "generate-sample-project":
+            context.options["generate-sample-project"] ??
+            defaultAzureEmitterOptions["generate-sample-project"],
+        "generate-test-project":
+            context.options["generate-test-project"] ??
+            defaultAzureEmitterOptions["generate-test-project"],
+        "use-model-reader-writer":
+            context.options["use-model-reader-writer"] ??
+            defaultAzureEmitterOptions["use-model-reader-writer"],
+        "single-top-level-client":
+            context.options["single-top-level-client"] ??
+            defaultAzureEmitterOptions["single-top-level-client"],
+        "keep-non-overloadable-protocol-signature":
+            context.options["keep-non-overloadable-protocol-signature"] ??
+            defaultAzureEmitterOptions[
+                "keep-non-overloadable-protocol-signature"
+            ]
     };
 }
