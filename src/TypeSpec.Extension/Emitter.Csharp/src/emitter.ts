59faeafa8c3b799691c9943ff537aeb0e0b8b1e5
jolov <jolov@microsoft.com>
2025-02-17 18:46:16 -0800

    format

--- src/TypeSpec.Extension/Emitter.Csharp/src/emitter.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/emitter.ts
@@ -89,7 +89,10 @@ export async function $onEmit(context: EmitContext<AzureCSharpEmitterOptions>) {
         const namespace = options["namespace"] ?? root.Name;
         configurations["namespace"] = namespace;
         configurations["library-name"] = options["library-name"] ?? namespace;
-        configurations["disable-xml-docs"] = options["disable-xml-docs"] === false ? undefined : options["disable-xml-docs"]
+        configurations["disable-xml-docs"] =
+            options["disable-xml-docs"] === false
+                ? undefined
+                : options["disable-xml-docs"];
 
         configurations["head-as-boolean"] = options["head-as-boolean"];
         configurations["deserialize-null-collection-as-null-value"] =
