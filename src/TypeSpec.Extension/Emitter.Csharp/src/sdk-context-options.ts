30b81078102122560a097b0cfd5c67df9dee5297
Pan Shao <pashao@microsoft.com>
2025-03-17 16:20:15 +0800

    Support request-path-is-non-resource for TypeSpec input

--- src/TypeSpec.Extension/Emitter.Csharp/src/sdk-context-options.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/sdk-context-options.ts
@@ -6,6 +6,7 @@ import { CreateSdkContextOptions } from "@azure-tools/typespec-client-generator-
 export const azureSDKContextOptions: CreateSdkContextOptions = {
     versioning: {},
     additionalDecorators: [
-        "Azure\\.ClientGenerator\\.Core\\.@useSystemTextJsonConverter"
+        "Azure\\.ClientGenerator\\.Core\\.@useSystemTextJsonConverter",
+        "Azure\\.ResourceManager\\.@armResourceOperations"
     ]
 };
