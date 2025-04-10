43a6e7d07d21ee903fbb673039a177096cf5afa2
Wei Hu <huwe@microsoft.com>
2025-04-10 08:00:01 +0800

    Add skeleton of AzurePlugin support for swagger deprecation (#5288)

---
+++ src/AutoRest.CSharp/AzurePlugin/AzurePluginTarget.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using AutoRest.CSharp.Common.Input;
+using System.Threading.Tasks;
+
+namespace AutoRest.CSharp.AutoRest.Plugins
+{
+    internal class AzurePluginTarget
+    {
+        public static async Task ExecuteAsync(GeneratedCodeWorkspace project, InputNamespace inputNamespace)
+        {
+            // TODO: serialize configuration.json and serialize inputNamespace to tspCodeModel.json
+            // TODO: spawn a child process to invoke MTG or Azure plugin
+            await Task.CompletedTask;
+        }
+    }
+}
