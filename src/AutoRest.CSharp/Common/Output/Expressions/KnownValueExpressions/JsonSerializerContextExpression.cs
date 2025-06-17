fd8167aee7990af15184ea736f30757007f70d35
Wei Hu <huwe@microsoft.com>
2025-06-17 11:11:27 +0800

    writer jsonSerializerContext

---
+++ src/AutoRest.CSharp/Common/Output/Expressions/KnownValueExpressions/JsonSerializerContextExpression.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using AutoRest.CSharp.Common.Generation.Writers;
+using AutoRest.CSharp.Common.Output.Expressions.ValueExpressions;
+
+namespace AutoRest.CSharp.Common.Output.Expressions.KnownValueExpressions
+{
+    internal class JsonSerializerContextExpression
+    {
+        public static readonly ValueExpression Default = new MemberExpression(new MemberExpression(null, JsonSerializerContextWriter.Name), "Default");
+    }
+}
