c0ec31d8504a5e75677de7b989a2db60ff5c4790
Sven Boemer <sbomer@gmail.com>
2025-07-28 09:58:19 -0700

    Update generated code

--- test/TestProjects/Customizations-TypeSpec/src/Generated/Models/CustomizationsInTspContext.cs
+++ test/TestProjects/Customizations-TypeSpec/src/Generated/Models/CustomizationsInTspContext.cs
@@ -14,7 +14,7 @@ namespace CustomizationsInTsp
     /// Context class which will be filled in by the System.ClientModel.SourceGeneration.
     /// For more information see 'https://github.com/Azure/azure-sdk-for-net/blob/main/sdk/core/System.ClientModel/src/docs/ModelReaderWriterContext.md'
     /// </summary>
-    [ModelReaderWriterBuildable(typeof(ModelStruct?))]
+    [ModelReaderWriterBuildable(typeof(ModelStruct))]
     [ModelReaderWriterBuildable(typeof(ModelToAddAdditionalSerializableProperty))]
     [ModelReaderWriterBuildable(typeof(ModelToChangeNamespace))]
     [ModelReaderWriterBuildable(typeof(ModelToMakeInternal))]
