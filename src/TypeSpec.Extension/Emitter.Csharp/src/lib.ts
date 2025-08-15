638f5b6fffb84526a2828de670a5b1a463d48f64
Arcturus Zhang <dapzhang@microsoft.com>
2025-08-15 16:23:39 +0800

    format

--- src/TypeSpec.Extension/Emitter.Csharp/src/lib.ts
+++ src/TypeSpec.Extension/Emitter.Csharp/src/lib.ts
@@ -1,7 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-import { createTypeSpecLibrary, DiagnosticDefinition, DiagnosticMessages, paramMessage } from "@typespec/compiler";
+import {
+    createTypeSpecLibrary,
+    DiagnosticDefinition,
+    DiagnosticMessages,
+    paramMessage
+} from "@typespec/compiler";
 import { AzureCSharpEmitterOptionsSchema } from "./options.js";
 
 export type DiagnosticMessagesMap = {
